64e16f0b3e6f2b3427020a0bb1a976d126d0434b
dlemstra <dirk@git.imagemagick.org>
2014-01-28 13:59:27 -0800

    Added ResourceLimits class.

---
+++ Magick.NET.Tests/ResourceLimitsTests.cs
@@ -0,0 +1,49 @@
+﻿//=================================================================================================
+// Copyright 2013-2014 Dirk Lemstra <https://magick.codeplex.com/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+//
+//   http://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+//=================================================================================================
+
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+	//==============================================================================================
+	[TestClass]
+	public class ResourceLimitsTests
+	{
+		//===========================================================================================
+		private const string _Category = "ResourceLimits";
+		//===========================================================================================
+		[TestMethod, TestCategory(_Category)]
+		public void Test_Values()
+		{
+			Assert.AreEqual(ulong.MaxValue, ResourceLimits.Disk);
+			Assert.AreEqual(8585838592U, ResourceLimits.Memory);
+			Assert.AreEqual(4U, ResourceLimits.Thread);
+
+			ResourceLimits.Disk = 400U;
+			Assert.AreEqual(400U, ResourceLimits.Disk);
+			ResourceLimits.Disk = ulong.MaxValue;
+
+			ResourceLimits.Memory = 858U;
+			Assert.AreEqual(858U, ResourceLimits.Memory);
+			ResourceLimits.Memory = 8585838592U;
+
+			ResourceLimits.Thread = 1U;
+			Assert.AreEqual(1U, ResourceLimits.Thread);
+			ResourceLimits.Thread = 4U;
+		}
+		//===========================================================================================
+	}
+	//==============================================================================================
+}
---
+++ Magick.NET/ResourceLimits.h
@@ -0,0 +1,58 @@
+//=================================================================================================
+// Copyright 2013-2014 Dirk Lemstra <https://magick.codeplex.com/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+//
+//   http://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+//=================================================================================================
+#pragma once
+
+#include "Stdafx.h"
+
+namespace ImageMagick
+{
+	///=============================================================================================
+	///<summary>
+	/// Class that can be used to set the limits to the resources that are being used.
+	///</summary>
+	public ref class ResourceLimits abstract sealed
+	{
+	public:
+		///==========================================================================================
+		///<summary>
+		/// Pixel cache limit in bytes. Requests for memory above this limit will fail.
+		///</summary>
+		static property Magick::MagickSizeType Disk
+		{
+			Magick::MagickSizeType get();
+			void set(Magick::MagickSizeType limit);
+		}
+		///==========================================================================================
+		///<summary>
+		/// Pixel cache limit in bytes. Once this memory limit is exceeded, all subsequent pixels cache
+		/// operations are to/from disk.
+		///</summary>
+		static property Magick::MagickSizeType Memory
+		{
+			Magick::MagickSizeType get();
+			void set(Magick::MagickSizeType limit);
+		}
+		///==========================================================================================
+		///<summary>
+		/// Limits the number of threads used in multithreaded operations.
+		///</summary>
+		static property Magick::MagickSizeType Thread
+		{
+			Magick::MagickSizeType get();
+			void set(Magick::MagickSizeType limit);
+		}
+		//===========================================================================================
+	};
+	//==============================================================================================
+}
