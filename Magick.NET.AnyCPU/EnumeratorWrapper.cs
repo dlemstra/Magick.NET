c00966dbfc3ba9ea4c50d258015bff9839349b09
dlemstra <dirk@git.imagemagick.org>
2014-03-05 01:52:58 -0800

    Forgot to save file.

--- Magick.NET.AnyCPU/EnumeratorWrapper.cs
+++ Magick.NET.AnyCPU/EnumeratorWrapper.cs
@@ -14,9 +14,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
-using System.Reflection;
 using Fasterflect;
 
 namespace ImageMagick
