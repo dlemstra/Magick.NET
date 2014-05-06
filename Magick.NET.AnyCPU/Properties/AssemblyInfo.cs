5588e8f18c4fdf647959c850efb192c57fe1cd6f
dlemstra <dirk@git.imagemagick.org>
2014-05-05 22:58:41 -0700

    Published Magick.NET 6.8.9.002.

--- Magick.NET.AnyCPU/Properties/AssemblyInfo.cs
+++ Magick.NET.AnyCPU/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Web;
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.8.9.001")]
+[assembly: AssemblyFileVersion("6.8.9.002")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 //=================================================================================================
