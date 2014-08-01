2f6b73222ee0844eb6bb37fbd706ea2c5a004039
dlemstra <dirk@git.imagemagick.org>
2014-08-01 13:11:53 -0700

    Published Magick.NET 6.8.9.601.

--- Publish/Publish.cmd
+++ Publish/Publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "%vs110comntools%vsvars32.bat"
-powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "6.8.9.5" "6.8.9.501"
+powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "6.8.9.6" "6.8.9.601"
 pause
