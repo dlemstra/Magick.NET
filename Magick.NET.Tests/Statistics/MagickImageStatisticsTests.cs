b9642de21eda16b2305cc632cbb46552f87bba36
dlemstra <dirk@git.imagemagick.org>
2014-08-17 05:17:25 -0700

    Fixed unit test for 8 bit.

--- Magick.NET.Tests/Statistics/MagickImageStatisticsTests.cs
+++ Magick.NET.Tests/Statistics/MagickImageStatisticsTests.cs
@@ -61,6 +61,19 @@ namespace Magick.NET.Tests
 			ChannelStatistics red = statistics.GetChannel(PixelChannel.Red);
 
 			Assert.IsNotNull(red);
+#if Q8
+			Assert.AreEqual(7, red.Depth);
+			Assert.AreEqual(-1.89, red.Kurtosis, 0.01);
+			Assert.AreEqual(2, red.Maximum);
+			Assert.AreEqual(0.83, red.Mean, 0.01);
+			Assert.AreEqual(0, red.Minimum);
+			Assert.AreEqual(0.32, red.Skewness, 0.01);
+			Assert.AreEqual(0.98, red.StandardDeviation, 0.01);
+			Assert.AreEqual(0.83, red.Sum, 0.01);
+			Assert.AreEqual(3.35, red.SumCubed, 0.01);
+			Assert.AreEqual(6.71, red.SumFourthPower, 0.01);
+			Assert.AreEqual(1.67, red.SumSquared, 0.01);
+#elif Q16 || Q16HDRI
 			Assert.AreEqual(8, red.Depth);
 			Assert.AreEqual(-1.89, red.Kurtosis, 0.01);
 			Assert.AreEqual(514, red.Maximum);
@@ -72,6 +85,9 @@ namespace Magick.NET.Tests
 			Assert.AreEqual(57013088.69, red.SumCubed, 0.01);
 			Assert.AreEqual(29304727586.71, red.SumFourthPower, 0.01);
 			Assert.AreEqual(110920.40, red.SumSquared, 0.01);
+#else
+#error Not implemented!
+#endif
 
 			Assert.IsNotNull(statistics.GetChannel(PixelChannel.Green));
 			Assert.IsNotNull(statistics.GetChannel(PixelChannel.Blue));
