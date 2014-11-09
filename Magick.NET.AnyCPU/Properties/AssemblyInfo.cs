7de74074ccaeca432141ae123d8da6063c39db3d
dlemstra <dirk@git.imagemagick.org>
2014-11-09 09:55:46 -0800

    Published Magick.NET 7.0.0.0007.

--- Magick.NET.AnyCPU/Properties/AssemblyInfo.cs
+++ Magick.NET.AnyCPU/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Web;
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.0.0006")]
+[assembly: AssemblyFileVersion("7.0.0.0007")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 //=================================================================================================
