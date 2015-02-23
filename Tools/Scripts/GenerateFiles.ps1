40d6aea9ee92698877ab395a9e9488928a430eaf
dlemstra <dirk@git.imagemagick.org>
2015-02-23 10:50:53 -0800

    Fixed creation of MagickScript XSD for HDRI build.

--- Tools/Scripts/GenerateFiles.ps1
+++ Tools/Scripts/GenerateFiles.ps1
@@ -9,6 +9,7 @@ function BuildMagickNET()
 {
 	BuildSolution "Magick.NET.sln" "Configuration=ReleaseQ8,RunCodeAnalysis=false,Platform=Win32"
 	BuildSolution "Magick.NET.sln" "Configuration=ReleaseQ16,RunCodeAnalysis=false,Platform=Win32"
+	BuildSolution "Magick.NET.sln" "Configuration=ReleaseQ16-HDRI,RunCodeAnalysis=false,Platform=Win32"
 }
 #==================================================================================================
 BuildMagickNET
