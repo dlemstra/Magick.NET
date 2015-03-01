515d58d0e7e8bf9582e77caaa14f61bafa3e6d7e
dlemstra <dirk@git.imagemagick.org>
2015-03-01 03:51:38 -0800

    Added new test for iptc profile in tiff coder.

---
+++ Magick.NET.Tests/Coders/TiffTests.cs
@@ -0,0 +1,68 @@
+﻿//=================================================================================================
+// Copyright 2013-2015 Dirk Lemstra <https://magick.codeplex.com/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+//
+//   http://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+//=================================================================================================
+
+using System.IO;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests.Coders
+{
+	//==============================================================================================
+	[TestClass]
+	public class TiffTests
+	{
+		//===========================================================================================
+		private const string _Category = "TiffTests";
+		//===========================================================================================
+		private static void TestValue(IptcProfile profile, IptcTag tag, string expectedValue)
+		{
+			IptcValue value = profile.GetValue(tag);
+			Assert.IsNotNull(value);
+			Assert.AreEqual(expectedValue, value.Value);
+		}
+		//===========================================================================================
+		[TestMethod, TestCategory(_Category)]
+		public void Test_IptcProfile()
+		{
+			using (MagickImage input = new MagickImage(Files.MagickNETIconPNG))
+			{
+				IptcProfile profile = input.GetIptcProfile();
+				Assert.IsNull(profile);
+
+				profile = new IptcProfile();
+				profile.SetValue(IptcTag.Headline, "Magick.NET");
+				profile.SetValue(IptcTag.CopyrightNotice, "Copyright.NET");
+
+				input.AddProfile(profile);
+
+				using (MemoryStream memStream = new MemoryStream())
+				{
+					input.Format = MagickFormat.Tiff;
+					input.Write(memStream);
+
+					memStream.Position = 0;
+					using (MagickImage output = new MagickImage(memStream))
+					{
+						profile = output.GetIptcProfile();
+						Assert.IsNotNull(profile);
+						TestValue(profile, IptcTag.Headline, "Magick.NET");
+						TestValue(profile, IptcTag.CopyrightNotice, "Copyright.NET");
+					}
+				}
+			}
+		}
+		//===========================================================================================
+	}
+	//==============================================================================================
+}
