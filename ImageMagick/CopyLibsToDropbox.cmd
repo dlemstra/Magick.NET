2babde7c0feed44adcd50c6b4dafaac8596621b0
dlemstra <dirk@git.imagemagick.org>
2015-04-29 09:19:00 +0200

    Fixed pause in batch file.

--- ImageMagick/CopyLibsToDropbox.cmd
+++ ImageMagick/CopyLibsToDropbox.cmd
@@ -6,10 +6,11 @@ if exist %DROPBOX% goto copy
 echo Unable to find Dropbox folder
 goto done
 
+:copy
+
 echo Are you sure?
 pause
 
-:copy
 xcopy lib %DROPBOX%\lib /Y /S /I
 xcopy Q8\lib %DROPBOX%\Q8\lib /Y /S /I
 xcopy Q16\lib %DROPBOX%\Q16\lib /Y /S /I
