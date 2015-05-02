7dad51982096f9c91923d53095bd5c0dc5fbd3d1
dlemstra <dirk@git.imagemagick.org>
2015-05-02 12:26:58 +0200

    Published Magick.NET 7.0.0.0014.

--- Magick.NET.Core/Properties/AssemblyInfo.cs
+++ Magick.NET.Core/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("7.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.0.0013")]
+[assembly: AssemblyFileVersion("7.0.0.0014")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 //=================================================================================================
--- Magick.NET.Wrapper/AssemblyInfo.cpp
+++ Magick.NET.Wrapper/AssemblyInfo.cpp
@@ -66,7 +66,7 @@ using namespace System::Security::Permissions;
 [assembly: AssemblyConfiguration("Release")];
 [assembly: AssemblyCulture("")];
 [assembly: AssemblyVersion("7.0.0.0")];
-[assembly: AssemblyFileVersion("7.0.0.0013")]
+[assembly: AssemblyFileVersion("7.0.0.0014")]
 [assembly: ComVisible(false)];
 [assembly: CLSCompliant(true)];
 [assembly: SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
