5aca25310f3cd1daa05ec973d120e8264789f46d
dlemstra <dirk@git.imagemagick.org>
2015-08-01 12:47:08 +0200

    Added watermark sample.

---
+++ Magick.NET.Samples/Samples/Magick.NET/Watermark.cs
@@ -0,0 +1,45 @@
+﻿//=================================================================================================
+// Copyright 2013-2015 Dirk Lemstra <https://magick.codeplex.com/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+//
+//   http://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+//=================================================================================================
+
+using System;
+using ImageMagick;
+
+namespace RootNamespace.Samples.MagickNET
+{
+	public static class WatermarkSamples
+	{
+		public static void CreateWatermark()
+		{
+			// Read image that needs a watermark
+			using (MagickImage image = new MagickImage(SampleFiles.FujiFilmFinePixS1ProJpg))
+			{
+				// Read the watermark that will be put on top of the image
+				using (MagickImage watermark = new MagickImage(SampleFiles.SnakewarePng))
+				{
+					// Draw the watermark in the bottom right corner
+					image.Composite(watermark, Gravity.Southeast, CompositeOperator.Over);
+
+					// Optionally make the watermark more transparent
+					watermark.Evaluate(Channels.Alpha, EvaluateOperator.Divide, 4);
+
+					// Or draw the watermark at a specific location
+					image.Composite(watermark, 200, 50, CompositeOperator.Over);
+				}
+
+				// Save the result
+				image.Write(SampleFiles.OutputDirectory + "FujiFilmFinePixS1Pro.watermark.jpg");
+			}
+		}
+	}
+}
---
+++ Magick.NET.Samples/Samples/Magick.NET/Watermark.vb
@@ -0,0 +1,44 @@
+﻿'==================================================================================================
+'  Copyright 2013-2015 Dirk Lemstra <https://magick.codeplex.com/>
+' 
+'  Licensed under the ImageMagick License (the "License"); you may not use this file except in 
+'  compliance with the License. You may obtain a copy of the License at
+' 
+'    http://www.imagemagick.org/script/license.php
+' 
+'  Unless required by applicable law or agreed to in writing, software distributed under the
+'  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+'  express or implied. See the License for the specific language governing permissions and
+'  limitations under the License.
+'==================================================================================================
+
+Imports System
+Imports ImageMagick
+
+Namespace RootNamespace.Samples.MagickNET
+
+	Public NotInheritable Class WatermarkSamples
+
+		Public Shared Sub CreateWatermark()
+			' Read image that needs a watermark
+			Using image As New MagickImage(SampleFiles.FujiFilmFinePixS1ProJpg)
+				' Read the watermark that will be put on top of the image
+				Using watermark As New MagickImage(SampleFiles.SnakewarePng)
+					' Draw the watermark in the bottom right corner
+					image.Composite(watermark, Gravity.Southeast, CompositeOperator.Over)
+
+					' Optionally make the watermark more transparent
+					watermark.Evaluate(Channels.Alpha, EvaluateOperator.Divide, 4)
+
+					' Or draw the watermark at a specific location
+					image.Composite(watermark, 200, 50, CompositeOperator.Over)
+				End Using
+
+				' Save the result
+				image.Write(SampleFiles.OutputDirectory + "FujiFilmFinePixS1Pro.watermark.jpg")
+			End Using
+		End Sub
+
+	End Class
+
+End Namespace
