cf5ea62fe2041d65f900c0364c86f05da6d676a1
dlemstra <dirk@git.imagemagick.org>
2015-10-06 22:58:11 +0200

    Added unit test for jp2 issue.

---
+++ Magick.NET.Tests/Coders/Jp2Tests.cs
@@ -0,0 +1,47 @@
+﻿//=================================================================================================
+// Copyright 2013-2015 Dirk Lemstra <https://magick.codeplex.com/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+//
+//   http://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+//=================================================================================================
+
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests.Coders
+{
+  [TestClass]
+  public class Jp2Tests
+  {
+    private const string _Category = "Jp2Tests";
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Dimensions()
+    {
+      using (MagickImage image = new MagickImage(Files.Coders.GrimJp2))
+      {
+        Assert.AreEqual(2155, image.Width);
+        Assert.AreEqual(2687, image.Height);
+      }
+
+      using (MagickImage image = new MagickImage(Files.Coders.GrimJp2 + "[0]"))
+      {
+        Assert.AreEqual(2155, image.Width);
+        Assert.AreEqual(2687, image.Height);
+      }
+
+      using (MagickImage image = new MagickImage(Files.Coders.GrimJp2 + "[1]"))
+      {
+        Assert.AreEqual(256, image.Width);
+        Assert.AreEqual(256, image.Height);
+      }
+    }
+  }
+}
