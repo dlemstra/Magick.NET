b8d5194b7d94548142335f165f6884aef844c083
dlemstra <dirk@git.imagemagick.org>
2016-01-30 13:34:57 +0100

    Disabled test build for .NET 2.0 x64. Setting correct platform during unit test.

--- Tools/AppVeyor/Test.cmd
+++ Tools/AppVeyor/Test.cmd
@@ -1,10 +1,15 @@
 @echo off
 call "%vs140comntools%vsvars32.bat"
 
+set platform="%1"
+if "%1" == "AnyCPU" (
+  set platform="x86"
+)
+
 for /r ..\..\Magick.NET.Tests\bin %%a in (*.dll) do (
   if "%%~nxa"=="Magick.NET.Tests.dll" (
     echo "Running tests from: %%~dpnxa"
-    vstest.console /inIsolation /logger:AppVeyor %%~dpnxa
+    vstest.console /inIsolation /platform:%platform% /logger:AppVeyor %%~dpnxa
     if %ERRORLEVEL% neq 0 goto done
   )
 )
