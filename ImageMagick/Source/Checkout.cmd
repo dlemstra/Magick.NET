56d880653c6e43a06638b9bed8726b37c538ad78
dlemstra <dirk@git.imagemagick.org>
2016-02-04 20:53:23 +0100

    Recompiled libraries with ImageMagick 7.

--- ImageMagick/Source/Checkout.cmd
+++ ImageMagick/Source/Checkout.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set REPOS=http://git.imagemagick.org/repos
-set DATE=2016-01-30 16:22
+set DATE=2016-02-04 13:46
 
 set BASH="%PROGRAMFILES%\Git\bin\bash.exe"
 if exist %BASH% goto EXECUTE
--- ImageMagick/include/MagickCore/version.h
+++ ImageMagick/include/MagickCore/version.h
@@ -41,7 +41,7 @@ extern "C" {
 #else
 #  define MagickPlatform "x86"
 #endif
-#define MagickReleaseDate  "2016-01-31"
+#define MagickReleaseDate  "2016-02-04"
 #define MagickAuthoritativeLicense  \
   "http://www.imagemagick.org/script/license.php"
 #define MagickAuthoritativeURL  "http://www.imagemagick.org"
