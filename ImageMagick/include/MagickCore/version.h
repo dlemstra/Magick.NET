4440395067a1ecc893e15cd695bdb11366cb5802
dlemstra <dirk@git.imagemagick.org>
2016-03-23 07:57:10 +0100

    Recompiled libraries with ImageMagick 7.

--- ImageMagick/include/MagickCore/version.h
+++ ImageMagick/include/MagickCore/version.h
@@ -41,7 +41,7 @@ extern "C" {
 #else
 #  define MagickPlatform "x86"
 #endif
-#define MagickReleaseDate  "2016-03-20"
+#define MagickReleaseDate  "2016-03-23"
 #define MagickAuthoritativeLicense  \
   "http://www.imagemagick.org/script/license.php"
 #define MagickAuthoritativeURL  "http://www.imagemagick.org"
