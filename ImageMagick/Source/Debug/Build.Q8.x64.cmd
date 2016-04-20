1f96a341cbc0f9572841b6ded115796c4f1404d4
dlemstra <dirk@git.imagemagick.org>
2016-04-20 07:05:48 +0200

    Fixed path.

--- ImageMagick/Source/Debug/Build.Q8.x64.cmd
+++ ImageMagick/Source/Debug/Build.Q8.x64.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 call "%vs140comntools%vsvars32.bat"
-powershell -ExecutionPolicy Unrestricted ..\..\..\..\Tools\Scripts\BuildLibraries.ps1 -dev Q8.x64 -configuration Debug
+powershell -ExecutionPolicy Unrestricted ..\..\..\Tools\Scripts\BuildLibraries.ps1 -dev Q8.x64 -configuration Debug
 
 pause
