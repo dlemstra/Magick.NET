98f22083e499be597f96a40189c2e7f8fb978694
dlemstra <dirk@git.imagemagick.org>
2016-04-27 12:26:42 +0200

    Restored methods that were removed from the DrawableSettings class.

--- Magick.NET/Core/Drawables/DrawableAffine.cs
+++ Magick.NET/Core/Drawables/DrawableAffine.cs
@@ -12,6 +12,7 @@
 // limitations under the License.
 //=================================================================================================
 
+using System;
 using ImageMagick.Drawables;
 
 namespace ImageMagick
@@ -26,6 +27,36 @@ namespace ImageMagick
     {
       if (wand != null)
         wand.Affine(ScaleX, ScaleY, ShearX, ShearY, TranslateX, TranslateY);
+    }
+
+    private static double DegreesToRadians(double x)
+    {
+      return Math.PI * x / 180.0;
+    }
+
+    private void Transform(DrawableAffine affine)
+    {
+      double scaleX = ScaleX;
+      double scaleY = ScaleY;
+      double shearX = ShearX;
+      double shearY = ShearY;
+      double translateX = TranslateX;
+      double translateY = TranslateY;
+
+      ScaleX = scaleX * affine.ScaleX + shearY * affine.ShearX;
+      ScaleY = shearX * affine.ShearY + scaleY * affine.ScaleY;
+      ShearX = shearX * affine.ScaleX + scaleY * affine.ShearX;
+      ShearY = scaleX * affine.ShearY + shearY * affine.ScaleY;
+      TranslateX = scaleX * affine.TranslateX + shearY * affine.TranslateY + translateX;
+      TranslateY = shearX * affine.TranslateX + scaleY * affine.TranslateY + translateY;
+    }
+
+    ///<summary>
+    /// Creates a new DrawableAffine instance.
+    ///</summary>
+    public DrawableAffine()
+    {
+      Reset();
     }
 
     ///<summary>
@@ -99,6 +130,91 @@ namespace ImageMagick
     {
       get;
       set;
+    }
+
+    /// <summary>
+    /// Reset to default
+    /// </summary>
+    public void Reset()
+    {
+      ScaleX = 1.0;
+      ScaleY = 1.0;
+      ShearX = 0.0;
+      ShearY = 0.0;
+      TranslateX = 0.0;
+      TranslateY = 0.0;
+    }
+
+    /// <summary>
+    /// Sets the origin of coordinate system.
+    /// </summary>
+    ///<param name="translateX">The X coordinate of the translation element.</param>
+    ///<param name="translateY">The Y coordinate of the translation element.</param>
+    public void TransformOrigin(double translateX, double translateY)
+    {
+      DrawableAffine affine = new DrawableAffine();
+
+      affine.TranslateX = translateX;
+      affine.TranslateY = translateY;
+
+      Transform(affine);
+    }
+
+    /// <summary>
+    /// Rotation to use.
+    /// </summary>
+    /// <param name="angle">The angle of the rotation.</param>
+    public void TransformRotation(double angle)
+    {
+      DrawableAffine affine = new DrawableAffine();
+
+      affine.ScaleX = Math.Cos(DegreesToRadians(Math.IEEERemainder(angle, 360.0)));
+      affine.ScaleY = Math.Cos(DegreesToRadians(Math.IEEERemainder(angle, 360.0)));
+      affine.ShearX = (-Math.Sin(DegreesToRadians(Math.IEEERemainder(angle, 360.0))));
+      affine.ShearY = Math.Sin(DegreesToRadians(Math.IEEERemainder(angle, 360.0)));
+
+      Transform(affine);
+    }
+
+    /// <summary>
+    /// Sets the scale to use.
+    /// </summary>
+    ///<param name="scaleX">The X coordinate scaling element.</param>
+    ///<param name="scaleY">The Y coordinate scaling element.</param>
+    public void TransformScale(double scaleX, double scaleY)
+    {
+      DrawableAffine affine = new DrawableAffine();
+
+      affine.ScaleX = scaleX;
+      affine.ScaleY = scaleY;
+
+      Transform(affine);
+    }
+
+    /// <summary>
+    /// Skew to use in X axis
+    /// </summary>
+    ///<param name="skewX">The X skewing element.</param>
+    public void TransformSkewX(double skewX)
+    {
+      DrawableAffine affine = new DrawableAffine();
+
+      affine.ShearX = Math.Tan(DegreesToRadians(Math.IEEERemainder(skewX, 360.0)));
+
+      Transform(affine);
+    }
+
+    /// <summary>
+    /// Skew to use in Y axis
+    /// </summary>
+    ///<param name="skewY">The Y skewing element.</param>
+    public void TransformSkewY(double skewY)
+    {
+      DrawableAffine affine = new DrawableAffine();
+
+      affine.ShearY = Math.Tan(DegreesToRadians(Math.IEEERemainder(skewY, 360.0)));
+
+      Transform(affine);
     }
   }
 }
