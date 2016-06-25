a900eca24bf06222370a4c8f0c11d7e5504f57b1
dlemstra <dirk@git.imagemagick.org>
2016-06-25 22:31:27 +0200

    Fixed check for error level during tests.

--- Tools/AppVeyor/Test.cmd
+++ Tools/AppVeyor/Test.cmd
@@ -10,8 +10,6 @@ for /r ..\..\Magick.NET.Tests\bin %%a in (*.dll) do (
   if "%%~nxa"=="Magick.NET.Tests.dll" (
     echo "Running tests from: %%~dpnxa"
     vstest.console /inIsolation /platform:%platform% /logger:AppVeyor %%~dpnxa
-    if %ERRORLEVEL% neq 0 goto done
+    if %errorlevel% neq 0 exit /b %errorlevel%
   )
 )
-
-:done
