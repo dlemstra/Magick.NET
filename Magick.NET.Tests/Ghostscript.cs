1fd2f3069d53e9ab0617cc6bfeee6497cdf9f908
dlemstra <dirk@git.imagemagick.org>
2016-06-26 16:23:32 +0200

    Made class public.

--- Magick.NET.Tests/Ghostscript.cs
+++ Magick.NET.Tests/Ghostscript.cs
@@ -18,7 +18,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Magick.NET.Tests
 {
   [TestClass]
-  internal sealed class Ghostscript
+  public sealed class Ghostscript
   {
     [AssemblyInitialize]
     public static void Initialize(TestContext context)
