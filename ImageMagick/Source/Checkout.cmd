15f90a780b14533746776091695e9757695cb53c
dlemstra <dirk@git.imagemagick.org>
2016-07-23 14:08:24 +0200

    Recompiled libraries with ImageMagick 7.0.2-6 beta.

--- ImageMagick/Source/Checkout.cmd
+++ ImageMagick/Source/Checkout.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set REPOS=http://git.imagemagick.org/repos
-set DATE=2016-07-22 00:17
+set DATE=2016-07-23 13:28
 
 set BASH="%PROGRAMFILES%\Git\bin\bash.exe"
 if exist %BASH% goto EXECUTE
