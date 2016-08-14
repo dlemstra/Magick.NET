26af90129dd296b7758fe1017a805735fad8c439
dlemstra <dirk@git.imagemagick.org>
2016-08-14 13:28:22 +0200

    Adjusted test results due to use of OpenCL.

--- Magick.NET.Tests/Core/MagickImageTests.cs
+++ Magick.NET.Tests/Core/MagickImageTests.cs
@@ -3015,10 +3015,14 @@ namespace Magick.NET.Tests
         ColorAssert.AreEqual(new MagickColor("#472400"), image, 243, 45);
         ColorAssert.AreEqual(new MagickColor("#522e00"), image, 394, 394);
         ColorAssert.AreEqual(new MagickColor("#e4bb7c"), image, 477, 373);
-#elif Q16 || Q16HDRI
-        ColorAssert.AreEqual(new MagickColor("#475f24bf0000"), image, 243, 45);
-        ColorAssert.AreEqual(new MagickColor("#52672e770000"), image, 394, 394);
-        ColorAssert.AreEqual(new MagickColor("#e5adbb627bf2"), image, 477, 373);
+#elif Q16
+        ColorAssert.AreEqual(new MagickColor(OpenCLValue.Get("#45be23e80000", "#475f24bf0000")), image, 243, 45);
+        ColorAssert.AreEqual(new MagickColor(OpenCLValue.Get("#50852d680000", "#52672e770000")), image, 394, 394);
+        ColorAssert.AreEqual(new MagickColor(OpenCLValue.Get("#e273b8c17a35", "#e5adbb627bf2")), image, 477, 373);
+#elif Q16HDRI
+        ColorAssert.AreEqual(new MagickColor(OpenCLValue.Get("#45be23e90001", "#475f24bf0000")), image, 243, 45);
+        ColorAssert.AreEqual(new MagickColor(OpenCLValue.Get("#50862d690001", "#52672e770000")), image, 394, 394);
+        ColorAssert.AreEqual(new MagickColor(OpenCLValue.Get("#e274b8c17a35", "#e5adbb627bf2")), image, 477, 373);
 #else
 #error Not implemented!
 #endif
