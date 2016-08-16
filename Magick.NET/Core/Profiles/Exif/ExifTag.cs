a2d15174588230114de5f58314405eab5533cbc1
dlemstra <dirk@git.imagemagick.org>
2016-08-16 20:42:22 +0200

    Added descriptions to exif tags (patch provided by Oliver Schneider).

--- Magick.NET/Core/Profiles/Exif/ExifTag.cs
+++ Magick.NET/Core/Profiles/Exif/ExifTag.cs
@@ -12,6 +12,8 @@
 // limitations under the License.
 //=================================================================================================
 
+// Descriptions from: http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/EXIF.html
+
 namespace ImageMagick
 {
   /// <summary>
@@ -53,16 +55,74 @@ namespace ImageMagick
     /// <summary>
     /// Compression
     /// </summary>
+    [ExifTagDescription((ushort)1, "Uncompressed")]
+    [ExifTagDescription((ushort)2, "CCITT 1D")]
+    [ExifTagDescription((ushort)3, "T4/Group 3 Fax")]
+    [ExifTagDescription((ushort)4, "T6/Group 4 Fax")]
+    [ExifTagDescription((ushort)5, "LZW")]
+    [ExifTagDescription((ushort)6, "JPEG (old-style)")]
+    [ExifTagDescription((ushort)7, "JPEG")]
+    [ExifTagDescription((ushort)8, "Adobe Deflate")]
+    [ExifTagDescription((ushort)9, "JBIG B&W")]
+    [ExifTagDescription((ushort)10, "JBIG Color")]
+    [ExifTagDescription((ushort)99, "JPEG")]
+    [ExifTagDescription((ushort)262, "Kodak 262")]
+    [ExifTagDescription((ushort)32766, "Next")]
+    [ExifTagDescription((ushort)32767, "Sony ARW Compressed")]
+    [ExifTagDescription((ushort)32769, "Packed RAW")]
+    [ExifTagDescription((ushort)32770, "Samsung SRW Compressed")]
+    [ExifTagDescription((ushort)32771, "CCIRLEW")]
+    [ExifTagDescription((ushort)32772, "Samsung SRW Compressed 2")]
+    [ExifTagDescription((ushort)32773, "PackBits")]
+    [ExifTagDescription((ushort)32809, "Thunderscan")]
+    [ExifTagDescription((ushort)32867, "Kodak KDC Compressed")]
+    [ExifTagDescription((ushort)32895, "IT8CTPAD")]
+    [ExifTagDescription((ushort)32896, "IT8LW")]
+    [ExifTagDescription((ushort)32897, "IT8MP")]
+    [ExifTagDescription((ushort)32898, "IT8BL")]
+    [ExifTagDescription((ushort)32908, "PixarFilm")]
+    [ExifTagDescription((ushort)32909, "PixarLog")]
+    [ExifTagDescription((ushort)32946, "Deflate")]
+    [ExifTagDescription((ushort)32947, "DCS")]
+    [ExifTagDescription((ushort)34661, "JBIG")]
+    [ExifTagDescription((ushort)34676, "SGILog")]
+    [ExifTagDescription((ushort)34677, "SGILog24")]
+    [ExifTagDescription((ushort)34712, "JPEG 2000")]
+    [ExifTagDescription((ushort)34713, "Nikon NEF Compressed")]
+    [ExifTagDescription((ushort)34715, "JBIG2 TIFF FX")]
+    [ExifTagDescription((ushort)34718, "Microsoft Document Imaging (MDI) Binary Level Codec")]
+    [ExifTagDescription((ushort)34719, "Microsoft Document Imaging (MDI) Progressive Transform Codec")]
+    [ExifTagDescription((ushort)34720, "Microsoft Document Imaging (MDI) Vector")]
+    [ExifTagDescription((ushort)34892, "Lossy JPEG")]
+    [ExifTagDescription((ushort)65000, "Kodak DCR Compressed")]
+    [ExifTagDescription((ushort)65535, "Pentax PEF Compressed")]
     Compression = 0x0103,
 
     /// <summary>
     /// PhotometricInterpretation
     /// </summary>
+    [ExifTagDescription((ushort)0, "WhiteIsZero")]
+    [ExifTagDescription((ushort)1, "BlackIsZero")]
+    [ExifTagDescription((ushort)2, "RGB")]
+    [ExifTagDescription((ushort)3, "RGB Palette")]
+    [ExifTagDescription((ushort)4, "Transparency Mask")]
+    [ExifTagDescription((ushort)5, "CMYK")]
+    [ExifTagDescription((ushort)6, "YCbCr")]
+    [ExifTagDescription((ushort)8, "CIELab")]
+    [ExifTagDescription((ushort)9, "ICCLab")]
+    [ExifTagDescription((ushort)10, "TULab")]
+    [ExifTagDescription((ushort)32803, "Color Filter Array")]
+    [ExifTagDescription((ushort)32844, "Pixar LogL")]
+    [ExifTagDescription((ushort)32845, "Pixar LogLuv")]
+    [ExifTagDescription((ushort)34892, "Linear Raw")]
     PhotometricInterpretation = 0x0106,
 
     /// <summary>
     /// Thresholding
     /// </summary>
+    [ExifTagDescription((ushort)1, "No dithering or halftoning")]
+    [ExifTagDescription((ushort)2, "Ordered dither or halftone")]
+    [ExifTagDescription((ushort)3, "Randomized dither")]
     Thresholding = 0x0107,
 
     /// <summary>
@@ -78,6 +138,8 @@ namespace ImageMagick
     /// <summary>
     /// FillOrder
     /// </summary>
+    [ExifTagDescription((ushort)1, "Normal")]
+    [ExifTagDescription((ushort)2, "Reversed")]
     FillOrder = 0x010A,
 
     /// <summary>
@@ -103,6 +165,14 @@ namespace ImageMagick
     /// <summary>
     /// Orientation
     /// </summary>
+    [ExifTagDescription((ushort)1, "Horizontal (normal)")]
+    [ExifTagDescription((ushort)2, "Mirror horizontal")]
+    [ExifTagDescription((ushort)3, "Rotate 180")]
+    [ExifTagDescription((ushort)4, "Mirror vertical")]
+    [ExifTagDescription((ushort)5, "Mirror horizontal and rotate 270 CW")]
+    [ExifTagDescription((ushort)6, "Rotate 90 CW")]
+    [ExifTagDescription((ushort)7, "Mirror horizontal and rotate 90 CW")]
+    [ExifTagDescription((ushort)8, "Rotate 270 CW")]
     Orientation = 0x0112,
 
     /// <summary>
@@ -143,6 +213,8 @@ namespace ImageMagick
     /// <summary>
     /// PlanarConfiguration
     /// </summary>
+    [ExifTagDescription((ushort)1, "Chunky")]
+    [ExifTagDescription((ushort)2, "Planar")]
     PlanarConfiguration = 0x011C,
 
     /// <summary>
@@ -158,6 +230,11 @@ namespace ImageMagick
     /// <summary>
     /// GrayResponseUnit
     /// </summary>
+    [ExifTagDescription((ushort)1, "0.1")]
+    [ExifTagDescription((ushort)2, "0.001")]
+    [ExifTagDescription((ushort)3, "0.0001")]
+    [ExifTagDescription((ushort)4, "1e-05")]
+    [ExifTagDescription((ushort)5, "1e-06")]
     GrayResponseUnit = 0x0122,
 
     /// <summary>
@@ -170,7 +247,7 @@ namespace ImageMagick
     /// </summary>
     [ExifTagDescription((ushort)1, "None")]
     [ExifTagDescription((ushort)2, "Inches")]
-    [ExifTagDescription((ushort)3, "Cm")]
+    [ExifTagDescription((ushort)3, "Centimeter")]
     ResolutionUnit = 0x0128,
 
     /// <summary>
@@ -232,11 +309,15 @@ namespace ImageMagick
     /// <summary>
     /// T4Options
     /// </summary>
+    [ExifTagDescription((uint)0, "2-Dimensional encoding")]
+    [ExifTagDescription((uint)1, "Uncompressed")]
+    [ExifTagDescription((uint)2, "Fill bits added")]
     T4Options = 0x0124,
 
     /// <summary>
     /// T6Options
     /// </summary>
+    [ExifTagDescription((uint)1, "Uncompressed")]
     T6Options = 0x0125,
 
     /// <summary>
@@ -297,6 +378,9 @@ namespace ImageMagick
     /// <summary>
     /// CleanFaxData
     /// </summary>
+    [ExifTagDescription((uint)0, "Clean")]
+    [ExifTagDescription((uint)1, "Regenerated")]
+    [ExifTagDescription((uint)2, "Unclean")]
     CleanFaxData = 0x0147,
 
     /// <summary>
@@ -307,6 +391,8 @@ namespace ImageMagick
     /// <summary>
     /// InkSet
     /// </summary>
+    [ExifTagDescription((ushort)1, "CMYK")]
+    [ExifTagDescription((ushort)2, "Not CMYK")]
     InkSet = 0x014C,
 
     /// <summary>
@@ -332,6 +418,12 @@ namespace ImageMagick
     /// <summary>
     /// SampleFormat
     /// </summary>
+    [ExifTagDescription((ushort)1, "Unsigned")]
+    [ExifTagDescription((ushort)2, "Signed")]
+    [ExifTagDescription((ushort)3, "Float")]
+    [ExifTagDescription((ushort)4, "Undefined")]
+    [ExifTagDescription((ushort)5, "Complex")]
+    [ExifTagDescription((ushort)6, "Complex")]
     SampleFormat = 0x0153,
 
     /// <summary>
@@ -367,6 +459,8 @@ namespace ImageMagick
     /// <summary>
     /// Indexed
     /// </summary>
+    [ExifTagDescription((ushort)0, "Not indexed")]
+    [ExifTagDescription((ushort)1, "Indexed")]
     Indexed = 0x015A,
 
     /// <summary>
@@ -377,21 +471,41 @@ namespace ImageMagick
     /// <summary>
     /// OPIProxy
     /// </summary>
+    [ExifTagDescription((ushort)0, "Higher resolution image does not exist")]
+    [ExifTagDescription((ushort)1, "Higher resolution image exists")]
     OPIProxy = 0x015F,
 
     /// <summary>
     /// ProfileType
     /// </summary>
+    [ExifTagDescription((uint)0, "Unspecified")]
+    [ExifTagDescription((uint)1, "Group 3 FAX")]
     ProfileType = 0x0191,
 
     /// <summary>
     /// FaxProfile
     /// </summary>
+    [ExifTagDescription((byte)0, "Unknown")]
+    [ExifTagDescription((byte)1, "Minimal B&W lossless, S")]
+    [ExifTagDescription((byte)2, "Extended B&W lossless, F")]
+    [ExifTagDescription((byte)3, "Lossless JBIG B&W, J")]
+    [ExifTagDescription((byte)4, "Lossy color and grayscale, C")]
+    [ExifTagDescription((byte)5, "Lossless color and grayscale, L")]
+    [ExifTagDescription((byte)6, "Mixed raster content, M")]
+    [ExifTagDescription((byte)7, "Profile T")]
+    [ExifTagDescription((byte)255, "Multi Profiles")]
     FaxProfile = 0x0192,
 
     /// <summary>
     /// CodingMethods
     /// </summary>
+    [ExifTagDescription((ulong)0, "Unspecified compression")]
+    [ExifTagDescription((ulong)1, "Modified Huffman")]
+    [ExifTagDescription((ulong)2, "Modified Read")]
+    [ExifTagDescription((ulong)4, "Modified MR")]
+    [ExifTagDescription((ulong)8, "JBIG")]
+    [ExifTagDescription((ulong)16, "Baseline JPEG")]
+    [ExifTagDescription((ulong)32, "JBIG color")]
     CodingMethods = 0x0193,
 
     /// <summary>
@@ -417,6 +531,8 @@ namespace ImageMagick
     /// <summary>
     /// JPEGProc
     /// </summary>
+    [ExifTagDescription((ushort)1, "Baseline")]
+    [ExifTagDescription((ushort)14, "Lossless")]
     JPEGProc = 0x0200,
 
     /// <summary>
@@ -472,6 +588,8 @@ namespace ImageMagick
     /// <summary>
     /// YCbCrPositioning
     /// </summary>
+    [ExifTagDescription((ushort)1, "Centered")]
+    [ExifTagDescription((ushort)2, "Co-sited")]
     YCbCrPositioning = 0x0213,
 
     /// <summary>
@@ -513,6 +631,16 @@ namespace ImageMagick
     /// <summary>
     /// ExposureProgram
     /// </summary>
+    [ExifTagDescription((ushort)0, "Not Defined")]
+    [ExifTagDescription((ushort)1, "Manual")]
+    [ExifTagDescription((ushort)2, "Program AE")]
+    [ExifTagDescription((ushort)3, "Aperture-priority AE")]
+    [ExifTagDescription((ushort)4, "Shutter speed priority AE")]
+    [ExifTagDescription((ushort)5, "Creative (Slow speed)")]
+    [ExifTagDescription((ushort)6, "Action (High speed)")]
+    [ExifTagDescription((ushort)7, "Portrait")]
+    [ExifTagDescription((ushort)8, "Landscape")]
+    [ExifTagDescription((ushort)9, "Bulb")]
     ExposureProgram = 0x8822,
 
     /// <summary>
@@ -588,16 +716,73 @@ namespace ImageMagick
     /// <summary>
     /// MeteringMode
     /// </summary>
+    [ExifTagDescription((ushort)0, "Unknown")]
+    [ExifTagDescription((ushort)1, "Average")]
+    [ExifTagDescription((ushort)2, "Center-weighted average")]
+    [ExifTagDescription((ushort)3, "Spot")]
+    [ExifTagDescription((ushort)4, "Multi-spot")]
+    [ExifTagDescription((ushort)5, "Multi-segment")]
+    [ExifTagDescription((ushort)6, "Partial")]
+    [ExifTagDescription((ushort)255, "Other")]
     MeteringMode = 0x9207,
 
     /// <summary>
     /// LightSource
     /// </summary>
+    [ExifTagDescription((ushort)0, "Unknown")]
+    [ExifTagDescription((ushort)1, "Daylight")]
+    [ExifTagDescription((ushort)2, "Fluorescent")]
+    [ExifTagDescription((ushort)3, "Tungsten (Incandescent)")]
+    [ExifTagDescription((ushort)4, "Flash")]
+    [ExifTagDescription((ushort)9, "Fine Weather")]
+    [ExifTagDescription((ushort)10, "Cloudy")]
+    [ExifTagDescription((ushort)11, "Shade")]
+    [ExifTagDescription((ushort)12, "Daylight Fluorescent")]
+    [ExifTagDescription((ushort)13, "Day White Fluorescent")]
+    [ExifTagDescription((ushort)14, "Cool White Fluorescent")]
+    [ExifTagDescription((ushort)15, "White Fluorescent")]
+    [ExifTagDescription((ushort)16, "Warm White Fluorescent")]
+    [ExifTagDescription((ushort)17, "Standard Light A")]
+    [ExifTagDescription((ushort)18, "Standard Light B")]
+    [ExifTagDescription((ushort)19, "Standard Light C")]
+    [ExifTagDescription((ushort)20, "D55")]
+    [ExifTagDescription((ushort)21, "D65")]
+    [ExifTagDescription((ushort)22, "D75")]
+    [ExifTagDescription((ushort)23, "D50")]
+    [ExifTagDescription((ushort)24, "ISO Studio Tungsten")]
+    [ExifTagDescription((ushort)255, "Other")]
     LightSource = 0x9208,
 
     /// <summary>
     /// Flash
     /// </summary>
+    [ExifTagDescription((ushort)0, "No Flash")]
+    [ExifTagDescription((ushort)1, "Fired")]
+    [ExifTagDescription((ushort)5, "Fired, Return not detected")]
+    [ExifTagDescription((ushort)7, "Fired, Return detected")]
+    [ExifTagDescription((ushort)8, "On, Did not fire")]
+    [ExifTagDescription((ushort)9, "On, Fired")]
+    [ExifTagDescription((ushort)13, "On, Return not detected")]
+    [ExifTagDescription((ushort)15, "On, Return detected")]
+    [ExifTagDescription((ushort)16, "Off, Did not fire")]
+    [ExifTagDescription((ushort)20, "Off, Did not fire, Return not detected")]
+    [ExifTagDescription((ushort)24, "Auto, Did not fire")]
+    [ExifTagDescription((ushort)25, "Auto, Fired")]
+    [ExifTagDescription((ushort)29, "Auto, Fired, Return not detected")]
+    [ExifTagDescription((ushort)31, "Auto, Fired, Return detected")]
+    [ExifTagDescription((ushort)32, "No flash function")]
+    [ExifTagDescription((ushort)48, "Off, No flash function")]
+    [ExifTagDescription((ushort)65, "Fired, Red-eye reduction")]
+    [ExifTagDescription((ushort)69, "Fired, Red-eye reduction, Return not detected")]
+    [ExifTagDescription((ushort)71, "Fired, Red-eye reduction, Return detected")]
+    [ExifTagDescription((ushort)73, "On, Red-eye reduction")]
+    [ExifTagDescription((ushort)77, "On, Red-eye reduction, Return not detected")]
+    [ExifTagDescription((ushort)69, "On, Red-eye reduction, Return detected")]
+    [ExifTagDescription((ushort)80, "Off, Red-eye reduction")]
+    [ExifTagDescription((ushort)88, "Auto, Did not fire, Red-eye reduction")]
+    [ExifTagDescription((ushort)89, "Auto, Fired, Red-eye reduction")]
+    [ExifTagDescription((ushort)93, "Auto, Fired, Red-eye reduction, Return not detected")]
+    [ExifTagDescription((ushort)95, "Auto, Fired, Red-eye reduction, Return detected")]
     Flash = 0x9209,
 
     /// <summary>
@@ -643,6 +828,11 @@ namespace ImageMagick
     /// <summary>
     /// ColorSpace
     /// </summary>
+    [ExifTagDescription((ushort)1, "sRGB")]
+    [ExifTagDescription((ushort)2, "Adobe RGB")]
+    [ExifTagDescription((ushort)4093, "Wide Gamut RGB")]
+    [ExifTagDescription((ushort)65534, "ICC Profile")]
+    [ExifTagDescription((ushort)65535, "Uncalibrated")]
     ColorSpace = 0xA001,
 
     /// <summary>
@@ -683,6 +873,11 @@ namespace ImageMagick
     /// <summary>
     /// FocalPlaneResolutionUnit
     /// </summary>
+    [ExifTagDescription((ushort)1, "None")]
+    [ExifTagDescription((ushort)2, "Inches")]
+    [ExifTagDescription((ushort)3, "Centimeter")]
+    [ExifTagDescription((ushort)4, "Millimeter")]
+    [ExifTagDescription((ushort)5, "Micrometer")]
     FocalPlaneResolutionUnit = 0xA210,
 
     /// <summary>
@@ -698,6 +893,13 @@ namespace ImageMagick
     /// <summary>
     /// SensingMethod
     /// </summary>
+    [ExifTagDescription((ushort)1, "Not defined")]
+    [ExifTagDescription((ushort)2, "One-chip color area")]
+    [ExifTagDescription((ushort)3, "Two-chip color area")]
+    [ExifTagDescription((ushort)4, "Three-chip color area")]
+    [ExifTagDescription((ushort)5, "Color sequential area")]
+    [ExifTagDescription((ushort)7, "Trilinear")]
+    [ExifTagDescription((ushort)8, "Color sequential linear")]
     SensingMethod = 0xA217,
 
     /// <summary>
@@ -718,16 +920,23 @@ namespace ImageMagick
     /// <summary>
     /// CustomRendered
     /// </summary>
+    [ExifTagDescription((ushort)1, "Normal")]
+    [ExifTagDescription((ushort)2, "Custom")]
     CustomRendered = 0xA401,
 
     /// <summary>
     /// ExposureMode
     /// </summary>
+    [ExifTagDescription((ushort)0, "Auto")]
+    [ExifTagDescription((ushort)1, "Manual")]
+    [ExifTagDescription((ushort)2, "Auto bracket")]
     ExposureMode = 0xA402,
 
     /// <summary>
     /// WhiteBalance
     /// </summary>
+    [ExifTagDescription((ushort)0, "Auto")]
+    [ExifTagDescription((ushort)1, "Manual")]
     WhiteBalance = 0xA403,
 
     /// <summary>
@@ -743,26 +952,44 @@ namespace ImageMagick
     /// <summary>
     /// SceneCaptureType
     /// </summary>
+    [ExifTagDescription((ushort)0, "Standard")]
+    [ExifTagDescription((ushort)1, "Landscape")]
+    [ExifTagDescription((ushort)2, "Portrait")]
+    [ExifTagDescription((ushort)3, "Night")]
     SceneCaptureType = 0xA406,
 
     /// <summary>
     /// GainControl
     /// </summary>
+    [ExifTagDescription((ushort)0, "None")]
+    [ExifTagDescription((ushort)1, "Low gain up")]
+    [ExifTagDescription((ushort)2, "High gain up")]
+    [ExifTagDescription((ushort)3, "Low gain down")]
+    [ExifTagDescription((ushort)4, "High gain down")]
     GainControl = 0xA407,
 
     /// <summary>
     /// Contrast
     /// </summary>
+    [ExifTagDescription((ushort)0, "Normal")]
+    [ExifTagDescription((ushort)1, "Low")]
+    [ExifTagDescription((ushort)2, "High")]
     Contrast = 0xA408,
 
     /// <summary>
     /// Saturation
     /// </summary>
+    [ExifTagDescription((ushort)0, "Normal")]
+    [ExifTagDescription((ushort)1, "Low")]
+    [ExifTagDescription((ushort)2, "High")]
     Saturation = 0xA409,
 
     /// <summary>
     /// Sharpness
     /// </summary>
+    [ExifTagDescription((ushort)0, "Normal")]
+    [ExifTagDescription((ushort)1, "Soft")]
+    [ExifTagDescription((ushort)2, "Hard")]
     Sharpness = 0xA40A,
 
     /// <summary>
@@ -773,6 +1000,10 @@ namespace ImageMagick
     /// <summary>
     /// SubjectDistanceRange
     /// </summary>
+    [ExifTagDescription((ushort)0, "Unknown")]
+    [ExifTagDescription((ushort)1, "Macro")]
+    [ExifTagDescription((ushort)2, "Close")]
+    [ExifTagDescription((ushort)3, "Distant")]
     SubjectDistanceRange = 0xA40C,
 
     /// <summary>
