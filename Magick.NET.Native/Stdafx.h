bdc3e3bd2f8bade35a0f66cd4315581cf4ed04b2
dlemstra <dirk@git.imagemagick.org>
2016-09-04 21:42:17 +0200

    Minor change in define.

--- Magick.NET.Native/Stdafx.h
+++ Magick.NET.Native/Stdafx.h
@@ -32,98 +32,98 @@
 
 #include <string.h>
 
-#define MAGICKNET_STRINGIFY(s) #s
+#define MAGICK_NET_STRINGIFY(s) #s
 #if defined(_DEBUG)
-#define MAGICKNET_LINK_LIB(name) \
- __pragma(comment(lib,MAGICKNET_STRINGIFY(CORE_DB_##name##_.lib)))
+#define MAGICK_NET_LINK_LIB(name) \
+ __pragma(comment(lib,MAGICK_NET_STRINGIFY(CORE_DB_##name##_.lib)))
 #else
-#define MAGICKNET_LINK_LIB(name) \
- __pragma(comment(lib,MAGICKNET_STRINGIFY(CORE_RL_##name##_.lib)))
+#define MAGICK_NET_LINK_LIB(name) \
+ __pragma(comment(lib,MAGICK_NET_STRINGIFY(CORE_RL_##name##_.lib)))
 #endif
 
 #define MAGICK_NET_EXPORT __declspec(dllexport)
 
 #if defined(MAGICKCORE_BZLIB_DELEGATE)
-MAGICKNET_LINK_LIB("bzlib")
+MAGICK_NET_LINK_LIB("bzlib")
 #endif
 
-MAGICKNET_LINK_LIB("coders")
+MAGICK_NET_LINK_LIB("coders")
 
 #if defined(MAGICKCORE_OPENEXR_DELEGATE)
-MAGICKNET_LINK_LIB("exr")
+MAGICK_NET_LINK_LIB("exr")
 #endif
 
 #if defined(MAGICKCORE_LQR_DELEGATE)
-MAGICKNET_LINK_LIB("ffi")
+MAGICK_NET_LINK_LIB("ffi")
 #endif
 
 #if defined(MAGICKCORE_FLIF_DELEGATE)
-MAGICKNET_LINK_LIB("flif")
+MAGICK_NET_LINK_LIB("flif")
 #endif
 
 #if defined(MAGICKCORE_LQR_DELEGATE)
-MAGICKNET_LINK_LIB("glib")
+MAGICK_NET_LINK_LIB("glib")
 #pragma comment(lib, "winmm.lib")
 #endif
 
 #if defined(MAGICKCORE_JBIG_DELEGATE)
-MAGICKNET_LINK_LIB("jbig")
+MAGICK_NET_LINK_LIB("jbig")
 #endif
 
 #if defined(MAGICKCORE_JP2_DELEGATE)
-MAGICKNET_LINK_LIB("jp2")
+MAGICK_NET_LINK_LIB("jp2")
 #endif
 
 #if defined(MAGICKCORE_JPEG_DELEGATE)
-MAGICKNET_LINK_LIB("jpeg")
+MAGICK_NET_LINK_LIB("jpeg")
 #endif
 
 #if defined(MAGICKCORE_LCMS_DELEGATE)
-MAGICKNET_LINK_LIB("lcms")
+MAGICK_NET_LINK_LIB("lcms")
 #endif
 
 #if defined(MAGICKCORE_LIBOPENJP2_DELEGATE)
-MAGICKNET_LINK_LIB("openjpeg")
+MAGICK_NET_LINK_LIB("openjpeg")
 #endif
 
-MAGICKNET_LINK_LIB("libxml")
+MAGICK_NET_LINK_LIB("libxml")
 
 #if defined(MAGICKCORE_LQR_DELEGATE)
-MAGICKNET_LINK_LIB("lqr")
+MAGICK_NET_LINK_LIB("lqr")
 #endif
 
-MAGICKNET_LINK_LIB("MagickCore")
-MAGICKNET_LINK_LIB("MagickWand")
+MAGICK_NET_LINK_LIB("MagickCore")
+MAGICK_NET_LINK_LIB("MagickWand")
 
 #if defined(MAGICKCORE_PANGOCAIRO_DELEGATE)
-MAGICKNET_LINK_LIB("cairo")
-MAGICKNET_LINK_LIB("pango")
-MAGICKNET_LINK_LIB("pixman")
+MAGICK_NET_LINK_LIB("cairo")
+MAGICK_NET_LINK_LIB("pango")
+MAGICK_NET_LINK_LIB("pixman")
 #endif
 
 #if defined(MAGICKCORE_PNG_DELEGATE)
-MAGICKNET_LINK_LIB("png")
+MAGICK_NET_LINK_LIB("png")
 #endif
 
 #if defined(MAGICKCORE_RSVG_DELEGATE)
-MAGICKNET_LINK_LIB("croco")
-MAGICKNET_LINK_LIB("librsvg")
+MAGICK_NET_LINK_LIB("croco")
+MAGICK_NET_LINK_LIB("librsvg")
 #endif
 
 #if defined(MAGICKCORE_TIFF_DELEGATE)
-MAGICKNET_LINK_LIB("tiff")
+MAGICK_NET_LINK_LIB("tiff")
 #endif
 
 #if defined(MAGICKCORE_FREETYPE_DELEGATE)
-MAGICKNET_LINK_LIB("ttf")
+MAGICK_NET_LINK_LIB("ttf")
 #endif
 
 #if defined(MAGICKCORE_WEBP_DELEGATE)
-MAGICKNET_LINK_LIB("webp")
+MAGICK_NET_LINK_LIB("webp")
 #endif
 
 #if defined(MAGICKCORE_ZLIB_DELEGATE)
-MAGICKNET_LINK_LIB("zlib")
+MAGICK_NET_LINK_LIB("zlib")
 #endif
 
 #pragma comment(lib, "ws2_32.lib")
