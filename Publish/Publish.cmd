8374d6473e4379aacf7ac13beb238f7cdfe365c6
dlemstra <dirk@git.imagemagick.org>
2016-10-09 13:12:57 +0200

    Published Magick.NET 7.0.3.300.

--- Publish/Publish.cmd
+++ Publish/Publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "%vs140comntools%vsvars32.bat"
-powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "7.0.3.1"
+powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "7.0.3.300"
 pause
