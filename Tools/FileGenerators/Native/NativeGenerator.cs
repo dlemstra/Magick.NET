6ed5458e15596fd4e63aa56fb1e007819d8dd82c
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-09 21:13:01 +0100

    Moved Native folder.

--- Tools/FileGenerators/Native/NativeGenerator.cs
+++ Tools/FileGenerators/Native/NativeGenerator.cs
@@ -39,7 +39,7 @@ namespace FileGenerator.Native
 
     private void CreateClasses()
     {
-      var directory = new DirectoryInfo(PathHelper.GetFullPath(@"\Magick.NET\Core"));
+      var directory = new DirectoryInfo(PathHelper.GetFullPath(@"\Magick.NET\Native"));
 
       IEnumerable<MagickClass> classes = CreateClasses(directory.GetFiles("*.json", SearchOption.AllDirectories));
 
