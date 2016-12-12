ef0102b8358fc660ac86ad5b4c81392b4e825a04
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-12 12:22:32 +0100

    Fixed copying the lib files.

--- Tools/AppVeyor/Build.cmd
+++ Tools/AppVeyor/Build.cmd
@@ -3,9 +3,9 @@ call "%vs140comntools%vsvars32.bat"
 
 set LIBDIR=C:\Magick.NET.libs
 set TARGET=C:\Magick.NET\ImageMagick
-xcopy %LIBDIR%\lib %TARGET%\lib\Release /Y /S /I
-xcopy %LIBDIR%\Q8\lib %TARGET%\Q8\lib\Release /Y /S /I
-xcopy %LIBDIR%\Q16\lib %TARGET%\Q16\lib\Release /Y /S /I
-xcopy %LIBDIR%\Q16-HDRI\lib %TARGET%\Q16-HDRI\lib\Release /Y /S /I
+xcopy %LIBDIR%\lib\Release %TARGET%\lib\Release /Y /S /I
+xcopy %LIBDIR%\Q8\lib\Release %TARGET%\Q8\lib\Release /Y /S /I
+xcopy %LIBDIR%\Q16\lib\Release %TARGET%\Q16\lib\Release /Y /S /I
+xcopy %LIBDIR%\Q16-HDRI\lib\Release %TARGET%\Q16-HDRI\lib\Release /Y /S /I
 
 powershell -ExecutionPolicy Unrestricted ..\Scripts\AppVeyor\Build.ps1 %1 %2
