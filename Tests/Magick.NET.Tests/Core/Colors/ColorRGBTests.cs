1eead51c5baf84c52e0e303cf8b3c3f418567dc1
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-17 23:16:50 +0100

    Added extra tests for ColorRGB.

--- Tests/Magick.NET.Tests/Core/Colors/ColorRGBTests.cs
+++ Tests/Magick.NET.Tests/Core/Colors/ColorRGBTests.cs
@@ -18,7 +18,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Magick.NET.Tests
 {
   [TestClass]
-  public class ColorRGBTests : ColorBaseTests<ColorRGB>
+  public partial class ColorRGBTests : ColorBaseTests<ColorRGB>
   {
     [TestMethod]
     public void Test_IComparable()
