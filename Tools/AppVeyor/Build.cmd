9001137e2825008ad57d1ae2292776eb6394f2e2
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-17 22:16:02 +0100

    Only copy the libs that we need during the build.

--- Tools/AppVeyor/Build.cmd
+++ Tools/AppVeyor/Build.cmd
@@ -3,9 +3,17 @@ call "%vs140comntools%vsvars32.bat"
 
 set LIBDIR=C:\Magick.NET.libs
 set TARGET=C:\Magick.NET\ImageMagick
-xcopy %LIBDIR%\lib\Release %TARGET%\lib\Release /Y /S /I
-xcopy %LIBDIR%\Q8\lib\Release %TARGET%\Q8\lib\Release /Y /S /I
-xcopy %LIBDIR%\Q16\lib\Release %TARGET%\Q16\lib\Release /Y /S /I
-xcopy %LIBDIR%\Q16-HDRI\lib\Release %TARGET%\Q16-HDRI\lib\Release /Y /S /I
 
+if "%2"=="AnyCPU" goto anycpu
+
+xcopy %LIBDIR%\lib\Release\%2 %TARGET%\lib\Release\%2 /Y /S /I
+xcopy %LIBDIR%\%1\lib\Release\%2 %TARGET%\$1\lib\Release\%2 /Y /S /I
+
+goto build
+
+:anycpu
+xcopy %LIBDIR%\lib\Release %TARGET%\lib\Release /Y /S /I
+xcopy %LIBDIR%\%1\lib\Release %TARGET%\$1\lib\Release /Y /S /I
+
+:build
 powershell -ExecutionPolicy Unrestricted ..\Scripts\AppVeyor\Build.ps1 %1 %2
