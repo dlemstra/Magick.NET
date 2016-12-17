1968156596a0775a31214f977699bf3bebdc8a86
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-17 22:50:49 +0100

    Make sure only the used lib files end up in the Magick.NET.libs folder.

--- Tools/AppVeyor/Build.cmd
+++ Tools/AppVeyor/Build.cmd
@@ -7,13 +7,17 @@ set TARGET=C:\Magick.NET\ImageMagick
 if "%2"=="AnyCPU" goto anycpu
 
 xcopy %LIBDIR%\lib\Release\%2 %TARGET%\lib\Release\%2 /Y /S /I
-xcopy %LIBDIR%\%1\lib\Release\%2 %TARGET%\$1\lib\Release\%2 /Y /S /I
+xcopy %LIBDIR%\%1\lib\Release\%2 %TARGET%\%1\lib\Release\%2 /Y /S /I
 
 goto build
 
 :anycpu
 xcopy %LIBDIR%\lib\Release %TARGET%\lib\Release /Y /S /I
-xcopy %LIBDIR%\%1\lib\Release %TARGET%\$1\lib\Release /Y /S /I
+xcopy %LIBDIR%\%1\lib\Release %TARGET%\%1\lib\Release /Y /S /I
 
 :build
+
+rmdir /S /Q %LIBDIR%
+xcopy %TARGET% %LIBDIR% /Y /S /I
+
 powershell -ExecutionPolicy Unrestricted ..\Scripts\AppVeyor\Build.ps1 %1 %2
