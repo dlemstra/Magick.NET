f62aec2dba27af4e7991a8473bcb1c524f2592f9
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-26 21:33:15 +0100

    Changed threshold.

--- Tools/AppVeyor/CodeCoverage.cmd
+++ Tools/AppVeyor/CodeCoverage.cmd
@@ -4,7 +4,7 @@ call "%vs140comntools%vsvars32.bat"
 if not "%1" == "Q8" goto skip
 if not "%2" == "x86" goto skip
 
-..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"%VSINSTALLDIR%Common7\IDE\MSTest.exe" -targetargs:"/noresults /noisolation /testcontainer:""..\..\Tests\Magick.NET.Tests\bin\ReleaseQ8\x86\Magick.NET.Tests.dll" -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\Generated\*.cs -hideskipped:All -output:.\Magick.NET.Coverage.xml
+..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"%VSINSTALLDIR%Common7\IDE\MSTest.exe" -targetargs:"/noresults /noisolation /testcontainer:""..\..\Tests\Magick.NET.Tests\bin\ReleaseQ8\x86\Magick.NET.Tests.dll" -register:user -threshold:10 -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\Generated\*.cs -hideskipped:All -output:.\Magick.NET.Coverage.xml
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov
