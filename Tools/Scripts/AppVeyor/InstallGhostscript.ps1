8540647e2260bbd42c06b1aac9db35c428745f8a
Dirk Lemstra <dirk@git.imagemagick.org>
2017-01-04 23:02:12 +0100

    Disable checksum check.

--- Tools/Scripts/AppVeyor/InstallGhostscript.ps1
+++ Tools/Scripts/AppVeyor/InstallGhostscript.ps1
@@ -1,2 +1,3 @@
+$env:ChocolateyIgnoreChecksums = "true"
 Import-Module "C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1"
 Install-ChocolateyPackage "Ghostscript.app" "exe" "/S /NCRC" "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs920/gs920w32.exe"
