ce68e7326aa64d11d44060d4209e8195604382e0
Dirk Lemstra <dirk@git.imagemagick.org>
2017-01-15 18:28:37 +0100

    Published Magick.NET 7.0.4.400.

--- Source/Magick.NET/Core/Properties/AssemblyInfo.Core.cs
+++ Source/Magick.NET/Core/Properties/AssemblyInfo.Core.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("7.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.4.100")]
+[assembly: AssemblyFileVersion("7.0.4.400")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
