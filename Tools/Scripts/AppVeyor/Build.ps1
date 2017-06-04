1397d862dbcaf81998f5714b54ee0d42fd1de67d
Dirk Lemstra <dirk@git.imagemagick.org>
2017-06-04 23:09:18 +0200

    Removed obsolete fix.

--- Tools/Scripts/AppVeyor/Build.ps1
+++ Tools/Scripts/AppVeyor/Build.ps1
@@ -21,15 +21,6 @@ SetFolder $scriptPath
 . Tools\Scripts\Shared\GzipAssembly.ps1
 . Tools\Scripts\Shared\Publish.ps1
 
-function FixStrongNameForNet20()
-{
-  $fileName = "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\Platforms\Win32\Microsoft.Cpp.Win32.targets"
-  $content = [IO.File]::ReadAllText($fileName)
-  $content = $content.Replace('="%(Link.DelaySign)"', '="$(LinkDelaySign)"').Replace('="%(Link.KeyFile)"', '="$(LinkKeyFile)"')
-
-  [IO.File]::WriteAllText($fileName, $content)
-}
-
 function AppVeyorBuild($quantum, $platform, $version)
 {
   if ($platform -eq "AnyCPU")
@@ -65,6 +56,5 @@ $quantum = $args[0]
 $platform = $args[1]
 
 $version = GetDevVersion
-FixStrongNameForNet20
 UpdateAssemblyInfos $version
 AppVeyorBuild $quantum $platform $version
