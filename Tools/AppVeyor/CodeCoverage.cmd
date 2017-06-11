87c0a88ca66d9b24050ae266164f1c58e3726963
Dirk Lemstra <dirk@git.imagemagick.org>
2017-06-11 23:30:27 +0200

    Fixed codecoverage tests.

--- Tools/AppVeyor/CodeCoverage.cmd
+++ Tools/AppVeyor/CodeCoverage.cmd
@@ -6,7 +6,7 @@ if not "%2" == "x86" goto skip
 
 ..\Programs\NuGet.exe restore packages.config -PackagesDirectory .
 
-OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"%VSINSTALLDIR%Common7\IDE\MSTest.exe" -targetargs:"/noresults /noisolation /testcontainer:""..\..\Tests\Magick.NET.Tests\bin\ReleaseQ8\x86\net45\Magick.NET.Tests.dll" -register:user -threshold:10 -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\Generated\*.cs;*\MagickColors.cs -hideskipped:All -output:.\Magick.NET.Coverage.xml
+OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"%VSINSTALLDIR%Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" -targetargs:"/platform:x86 /inIsolation ..\..\Tests\Magick.NET.Tests\bin\ReleaseQ8\x86\net45\Magick.NET.Tests.dll" -register:user -threshold:10 -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\Generated\*.cs;*\MagickColors.cs -hideskipped:All -output:.\Magick.NET.Coverage.xml
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov
