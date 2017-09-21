a91d498e84d48cb53899be3f134a33b517f06ec6
Dirk Lemstra <dirk@git.imagemagick.org>
2017-09-21 13:20:14 +0200

    Use a different version of vstest.console to make sure the exit code is correct.

--- Tools/AppVeyor/Test.cmd
+++ Tools/AppVeyor/Test.cmd
@@ -6,10 +6,12 @@ if "%1" == "AnyCPU" (
   set platform="x86"
 )
 
+set vstest="%VSINSTALLDIR%Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+
 for /r ..\..\Tests\Magick.NET.Tests\bin %%a in (*.dll) do (
   if "%%~nxa"=="Magick.NET.Tests.dll" (
     echo "Running tests from: %%~dpnxa"
-    vstest.console /inIsolation /platform:%platform% /logger:AppVeyor %%~dpnxa
+    %vstest% %%~dpnxa /inIsolation /platform:%platform% /TestAdapterPath:%%~dpa /logger:AppVeyor
     if %errorlevel% neq 0 exit /b %errorlevel%
   )
 )
