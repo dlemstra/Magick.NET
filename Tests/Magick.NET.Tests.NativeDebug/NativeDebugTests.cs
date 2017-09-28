b32c351e4270447dfd844c3b042a8a429cda09d2
Dirk Lemstra <dirk@git.imagemagick.org>
2017-09-28 16:04:02 +0200

    Removed local test code.

--- Tests/Magick.NET.Tests.NativeDebug/NativeDebugTests.cs
+++ Tests/Magick.NET.Tests.NativeDebug/NativeDebugTests.cs
@@ -1,7 +1,5 @@
 ﻿using ImageMagick;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.IO;
-using System.Linq;
 
 namespace Magick.NET.Tests.NativeDebug
 {
@@ -11,13 +9,6 @@ namespace Magick.NET.Tests.NativeDebug
         [TestMethod]
         public void TestNative()
         {
-            using (var image = new MagickImage(@"i:\tiff\layers\new\layer3.tif[0]"))
-            {
-                //Assert.AreEqual("1", image.ProfileNames.Skip(1).First());
-
-                var bytes = image.GetProfile("tiff:37724").ToByteArray();
-                File.WriteAllBytes(@"i:\tiff\layers\new\profile2.txt", bytes);
-            }
         }
     }
 }
