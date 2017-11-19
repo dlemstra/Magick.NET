b34b898e739633d43af32b2aee63edc930cffe18
Dirk Lemstra <dirk@git.imagemagick.org>
2017-11-19 10:30:11 +0100

    Make sure the correct dll is tested.

--- Tools/AppVeyor/Test.cmd
+++ Tools/AppVeyor/Test.cmd
@@ -10,10 +10,10 @@ if "%1" == "AnyCPU" (
 
 set vstest="%VSINSTALLDIR%\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"
 
-for /r ..\..\Tests\Magick.NET.Tests\bin %%a in (*.dll) do (
-   if "%%~nxa"=="Magick.NET.Tests.dll" (
-     echo "Running tests from: %%~dpnxa"
-     %vstest% %%~dpnxa /platform:%platform% /TestAdapterPath:%%~dpa
-     if !errorlevel! neq 0 exit /b !errorlevel!
-   )
+for /d %%d in (..\..\Tests\Magick.NET.Tests\bin\Test*) do (
+    set folder=%%~d\%1\net45
+    set filename=!folder!\Magick.NET.Tests.dll
+    echo "Running tests from: !filename!"
+    %vstest% !filename! /platform:%platform% /TestAdapterPath:!folder!
+    if !errorlevel! neq 0 exit /b !errorlevel!
 )
