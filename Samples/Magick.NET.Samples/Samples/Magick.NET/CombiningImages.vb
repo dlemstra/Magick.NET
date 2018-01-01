9943055423b90ac35933dde602b3af9aab746a8a
Dirk Lemstra <dirk@git.imagemagick.org>
2018-01-01 22:24:28 +0100

    Changed copyright year.

--- Samples/Magick.NET.Samples/Samples/Magick.NET/CombiningImages.vb
+++ Samples/Magick.NET.Samples/Samples/Magick.NET/CombiningImages.vb
@@ -1,4 +1,4 @@
-﻿' Copyright 2013-2017 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+﻿' Copyright 2013-2018 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
 '
 ' Licensed under the ImageMagick License (the "License"); you may Not use this file except in
 ' compliance with the License. You may obtain a copy of the License at
