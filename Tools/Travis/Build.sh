e8e88f1401c2ffc06c9eefb40846b37e7bb8cf8a
Dirk Lemstra <dirk@git.imagemagick.org>
2018-04-20 23:17:39 +0200

    Added Q16 and Q16-HDRI Linux build.

--- Tools/Travis/Build.sh
+++ Tools/Travis/Build.sh
@@ -12,3 +12,19 @@ wget -O Q8.zip https://www.dropbox.com/sh/tycq7qh50zssgr8/AACt2zhBn8GdVlWVxsKzw7
 unzip Q8.zip -x / -d Tests/Magick.NET.Tests/bin/TestQ8/AnyCPU/netcoreapp2.0/
 
 dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ8
+
+# Build Q16
+dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime linux-x64 -c TestQ16
+
+wget -O Q16.zip https://www.dropbox.com/sh/5xmw2tl3q7a930u/AADN7QlNsvzhFXazc3FrleTYa?dl=1
+unzip Q16.zip -x / -d Tests/Magick.NET.Tests/bin/TestQ16/AnyCPU/netcoreapp2.0/
+
+dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ16
+
+# Build Q16-HDRI
+dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime linux-x64 -c TestQ16-HDRI
+
+wget -O Q16-HDRI.zip https://www.dropbox.com/sh/tbsisu1byt8csbr/AABv32VeYzU6IBE5EyWL3v9za?dl=1
+unzip Q16-HDRI.zip -x / -d Tests/Magick.NET.Tests/bin/TestQ16-HDRI/AnyCPU/netcoreapp2.0/
+
+dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ16-HDRI
