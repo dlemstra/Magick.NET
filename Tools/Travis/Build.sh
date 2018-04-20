aa24b2a3dd14d73f7b1c80f6e4850e07ba18c136
Dirk Lemstra <dirk@git.imagemagick.org>
2018-04-20 20:19:49 +0200

    Added travis build.

---
+++ Tools/Travis/Build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+cd ..
+
+# Build Q8
+dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime linux-x64 -c TestQ8
+
+wget -O Q8.zip https://www.dropbox.com/sh/tycq7qh50zssgr8/AACt2zhBn8GdVlWVxsKzw71Ka?dl=1
+unzip Q8.zip -d Tests/Magick.NET.Tests/bin/TestQ8/AnyCPU/netcoreapp2.0/
+
+dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ8 --no-build
