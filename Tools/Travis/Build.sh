1874917ed728c7a6a162523fb190cae26571fcc7
Dirk Lemstra <dirk@git.imagemagick.org>
2018-04-20 21:26:37 +0200

    Fixed unzipping and removed --no-build.

--- Tools/Travis/Build.sh
+++ Tools/Travis/Build.sh
@@ -4,6 +4,6 @@
 dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime linux-x64 -c TestQ8
 
 wget -O Q8.zip https://www.dropbox.com/sh/tycq7qh50zssgr8/AACt2zhBn8GdVlWVxsKzw71Ka?dl=1
-unzip Q8.zip -d Tests/Magick.NET.Tests/bin/TestQ8/AnyCPU/netcoreapp2.0/
+unzip Q8.zip -x / -d Tests/Magick.NET.Tests/bin/TestQ8/AnyCPU/netcoreapp2.0/
 
-dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ8 --no-build
+dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ8
