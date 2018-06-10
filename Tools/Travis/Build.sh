f6e99c17501bfbfeee923c809204639c2bbb8bee
Dirk Lemstra <dirk@git.imagemagick.org>
2018-06-10 09:44:38 +0200

    Use file links instead of folder links because that doesn't seem to work anymore.

--- Tools/Travis/Build.sh
+++ Tools/Travis/Build.sh
@@ -10,35 +10,35 @@ fi
 # Build Q8
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime linux-x64 -c TestQ8
-  wget -O Q8.zip https://www.dropbox.com/sh/tycq7qh50zssgr8/AACt2zhBn8GdVlWVxsKzw71Ka?dl=1
+  link=https://www.dropbox.com/s/j2bf7pjg78un8p9/Magick.NET-Q8-x64.Native.dll.so?dl=1
 else
   dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime osx-x64 -c TestQ8
-  wget -O Q8.zip https://www.dropbox.com/sh/hsumjx0e99xqk5d/AACovle8G5SJgc2H7XqhhNU1a?dl=1
+  link=https://www.dropbox.com/s/w9569iz6cryg1a0/libMagick.NET-Q8-x64.Native.dll.dylib?dl=1
 fi
-unzip Q8.zip -x / -d Tests/Magick.NET.Tests/bin/TestQ8/AnyCPU/netcoreapp2.0/
+wget -P Tests/Magick.NET.Tests/bin/TestQ8/AnyCPU/netcoreapp2.0/ --content-disposition "$link"
 
 dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ8
 
 # Build Q16
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime linux-x64 -c TestQ16
-  wget -O Q16.zip https://www.dropbox.com/sh/5xmw2tl3q7a930u/AADN7QlNsvzhFXazc3FrleTYa?dl=1
+  link=https://www.dropbox.com/s/04i9mjj7f43en9x/Magick.NET-Q16-x64.Native.dll.so?dl=1
 else
   dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime osx-x64 -c TestQ16
-  wget -O Q16.zip https://www.dropbox.com/sh/2bh81m9djp4kgxu/AABNBxdtHS5SYIxPEkmhMnwfa?dl=1
+  link=https://www.dropbox.com/s/84vbyl4bms1eg9g/libMagick.NET-Q16-x64.Native.dll.dylib?dl=1
 fi
-unzip Q16.zip -x / -d Tests/Magick.NET.Tests/bin/TestQ16/AnyCPU/netcoreapp2.0/
+wget -P Tests/Magick.NET.Tests/bin/TestQ16/AnyCPU/netcoreapp2.0/ --content-disposition "$link"
 
 dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ16
 
 # Build Q16-HDRI
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime linux-x64 -c TestQ16-HDRI
-  wget -O Q16-HDRI.zip https://www.dropbox.com/sh/tbsisu1byt8csbr/AABv32VeYzU6IBE5EyWL3v9za?dl=1
+  link=https://www.dropbox.com/s/hhhcjoumoeoejun/Magick.NET-Q16-HDRI-x64.Native.dll.so?dl=1
 else
   dotnet build Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 --runtime osx-x64 -c TestQ16-HDRI
-  wget -O Q16-HDRI.zip https://www.dropbox.com/sh/5mirllz36axixfh/AAC-odW0R4E9WbIAn5Ix-QIPa?dl=1
+  link=https://www.dropbox.com/s/zqd8d1jjxswt1sp/libMagick.NET-Q16-HDRI-x64.Native.dll.dylib?dl=1
 fi
-unzip Q16-HDRI.zip -x / -d Tests/Magick.NET.Tests/bin/TestQ16-HDRI/AnyCPU/netcoreapp2.0/
+wget -P Tests/Magick.NET.Tests/bin/TestQ16-HDRI/AnyCPU/netcoreapp2.0/ --content-disposition "$link"
 
 dotnet test Tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp2.0 -c TestQ16-HDRI
