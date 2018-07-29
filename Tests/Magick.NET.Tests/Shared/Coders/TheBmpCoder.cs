0d7da48a2e9fa5a6bbf140c9f8587422be444347
Dirk Lemstra <dirk@git.imagemagick.org>
2018-07-29 17:08:42 +0200

    Added unit test to make sure #210 stays fixed.

---
+++ Tests/Magick.NET.Tests/Shared/Coders/TheBmpCoder.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2013-2018 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests.Shared.Coders
+{
+    [TestClass]
+    public class TheBmpCoder
+    {
+        [TestMethod]
+        public void ShouldBeAbleToReadBmp3Format()
+        {
+            using (TemporaryFile file = new TemporaryFile(Files.MagickNETIconPNG))
+            {
+                using (IMagickImage image = new MagickImage(file))
+                {
+                    image.Write(file, MagickFormat.Bmp3);
+                }
+
+                var settings = new MagickReadSettings()
+                {
+                    Format = MagickFormat.Bmp3,
+                };
+
+                using (IMagickImage image = new MagickImage(file, settings))
+                {
+                    Assert.AreEqual(MagickFormat.Bmp3, image.Format);
+                }
+            }
+        }
+    }
+}
