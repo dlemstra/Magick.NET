abb5efb819e72742b912ad762262dc8230c8a709
Dirk Lemstra <dirk@git.imagemagick.org>
2018-09-01 21:11:37 +0200

    Published Magick.NET 7.7.0.0.

--- Publish/Publish.cmd
+++ Publish/Publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "..\Tools\VsDevCmd.cmd"
-powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "7.6.0.1"
+powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "7.7.0.0"
 pause
