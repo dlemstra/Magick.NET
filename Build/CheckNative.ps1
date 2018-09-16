3bba8cb327171545ed33f707acdefe78a9dc889d
Dirk Lemstra <dirk@git.imagemagick.org>
2018-09-16 21:05:41 +0200

    Enabled check inside CheckNative.ps1

--- Build/CheckNative.ps1
+++ Build/CheckNative.ps1
@@ -1,5 +1,5 @@
 $files = git diff --name-only HEAD~1 HEAD
 
-#if ($files -contains "ImageMagick/Source/Checkout.sh") {
+if ($files -contains "ImageMagick/Source/Checkout.sh") {
     Write-Host "##vso[task.setvariable variable=BuildNative;isOutput=true]true"
-#}
+}
