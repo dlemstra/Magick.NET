b2b8d9ff416db8990d0310b7eb4de36ed4adda92
Dirk Lemstra <dirk@git.imagemagick.org>
2018-09-16 20:43:25 +0200

    Mark variable as output variable

--- Build/CheckNative.ps1
+++ Build/CheckNative.ps1
@@ -1,5 +1,5 @@
 $files = git diff --name-only HEAD~1 HEAD
 
 #if ($files -contains "ImageMagick/Source/Checkout.sh") {
-    Write-Host "##vso[task.setvariable variable=BuildNative;]true"
+    Write-Host "##vso[task.setvariable variable=BuildNative;isOutput=true]true"
 #}
