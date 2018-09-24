989ffb0cd68e1964e0d1d25bbd63d4a146500d28
Dirk Lemstra <dirk@lemstra.org>
2018-09-24 22:42:56 +0200

    Allow force of native build through an environment variable

--- Build/CheckNative.ps1
+++ Build/CheckNative.ps1
@@ -1,5 +1,14 @@
-$files = git diff --name-only HEAD~1 HEAD
+function BuildNative() {
+  if ($env:native -eq "true") {
+    return $true
+  }
 
-if ($files -contains "ImageMagick/Source/ImageMagick.commit") {
+  $files = git diff --name-only HEAD~1 HEAD
+  return $files -contains "ImageMagick/Source/ImageMagick.commit"
+}
+
+if (BuildNative) {
     Write-Host "##vso[task.setvariable variable=BuildNative;isOutput=true]true"
+} else {
+    Write-Host "Not building the native library."
 }
