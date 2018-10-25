9ed8647aeabfe1f7f89d579ecc57ed88806f3335
Dirk Lemstra <dirk@lemstra.org>
2018-10-25 08:06:14 +0200

    Added unit test for bug reported in issue #338.

---
+++ Tests/Magick.NET.Tests/Windows/Coders/TheLabelCoder.cs
@@ -0,0 +1,44 @@
+﻿// Copyright 2013-2018 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+#if !NETCORE
+
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    [TestClass]
+    public class TheLabelCoder
+    {
+        [TestMethod]
+        public void ShouldUseTheDensity()
+        {
+            var readSettings = new MagickReadSettings()
+            {
+                FontPointsize = 14,
+                FontFamily = "Calibri",
+                StrokeColor = MagickColors.Black,
+                BackgroundColor = MagickColors.White,
+                Density = new Density(96, 96, DensityUnit.PixelsPerInch),
+            };
+
+            using (IMagickImage image = new MagickImage($"label:Masai Mara", readSettings))
+            {
+                Assert.AreEqual(91, image.Width);
+                Assert.AreEqual(21, image.Height);
+            }
+        }
+    }
+}
+
+#endif
