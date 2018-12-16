197a4529572458115f5657dea6ec931a3d57a52b
Dirk Lemstra <dirk@lemstra.org>
2018-12-16 18:16:27 +0100

    Corrected unit tests due to changes in the clahe method of ImageMagick.

--- Tests/Magick.NET.Tests/Shared/MagickImageTests/TheClaheMethod.cs
+++ Tests/Magick.NET.Tests/Shared/MagickImageTests/TheClaheMethod.cs
@@ -28,13 +28,8 @@ namespace Magick.NET.Tests.Shared
                     using (IMagickImage result = image.Clone())
                     {
                         result.Clahe(10, 20, 30, 1.5);
-#if Q8
-                        Assert.AreEqual(0.03, image.Compare(result, ErrorMetric.RootMeanSquared), 0.01);
-#elif Q16 || Q16HDRI
+
                         Assert.AreEqual(0.08, image.Compare(result, ErrorMetric.RootMeanSquared), 0.01);
-#else
-#error Not implemented!
-#endif
                     }
                 }
             }
