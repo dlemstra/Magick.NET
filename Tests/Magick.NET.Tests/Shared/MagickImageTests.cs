d165c3ad162498b84c349d0eb244a41143cc2fda
Dirk Lemstra <dirk@lemstra.org>
2018-12-16 11:58:13 +0100

    Removed count to get a better error message.

--- Tests/Magick.NET.Tests/Shared/MagickImageTests.cs
+++ Tests/Magick.NET.Tests/Shared/MagickImageTests.cs
@@ -2490,10 +2490,7 @@ namespace Magick.NET.Tests
             {
                 IEnumerable<string> names = image.ProfileNames;
                 Assert.IsNotNull(names);
-                Assert.AreEqual(5, names.Count());
-                Assert.AreEqual("8bim,exif,icc,iptc,xmp", string.Join(",", (from name in names
-                                                                            orderby name
-                                                                            select name).ToArray()));
+                Assert.AreEqual("8bim,exif,icc,iptc,xmp", string.Join(",", names));
             }
 
             using (IMagickImage image = new MagickImage(Files.RedPNG))
