6be9b65163bd5bc04fe4c999a16ff6608493accf
Dirk Lemstra <dirk@lemstra.org>
2019-05-13 22:53:27 +0200

    Published Magick.NET 7.13.0.0.

--- Publish/Publish.cmd
+++ Publish/Publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "..\Tools\VsDevCmd.cmd"
-powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "7.12.0.0"
+powershell -ExecutionPolicy Unrestricted ..\Tools\Scripts\Publish.ps1 "7.13.0.0"
 pause
