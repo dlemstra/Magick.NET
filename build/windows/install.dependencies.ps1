8095a3d478ca2a857dfbe077648ab72eed8f3c24
Dirk Lemstra <dirk@lemstra.org>
2019-06-10 11:08:37 +0200

    Sign the libraries.

--- build/windows/install.dependencies.ps1
+++ build/windows/install.dependencies.ps1
@@ -10,14 +10,24 @@
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 
+param (
+    [string]$pfxUri = ''
+)
+
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 
+Write-Host "Installing NuGet"
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $targetNugetExe = "$PSScriptRoot\..\..\tools\windows\nuget.exe"
 Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
 
+Write-Host "Installing Ghostscript 9.26"
 $sourceGhostscriptExe = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs926/gs926aw32.exe"
 $targetGhostscriptExe = "$PSScriptRoot\..\..\tools\windows\gs926aw32.exe"
 Invoke-WebRequest $sourceGhostscriptExe -OutFile $targetGhostscriptExe
-
 & $targetGhostscriptExe /S
+
+if ($pfxUri.Length -gt 0) {
+    Write-Host "Downloading code signing certificate"
+    Invoke-WebRequest $pfxUri -OutFile "$PSScriptRoot\ImageMagick.pfx"
+}
