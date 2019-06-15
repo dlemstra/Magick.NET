18799cf811a9c168b53c7c7cda875f08961e718f
Dirk Lemstra <dirk@lemstra.org>
2019-06-15 16:19:14 +0200

    Added class that can be used to create values for a specific tag.

---
+++ src/Magick.NET/Shared/Profiles/Exif/Values/ExifValues.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System;
+
+namespace ImageMagick
+{
+    /// <summary>
+    /// Contains the possible exif values.
+    /// </summary>
+    public static partial class ExifValues
+    {
+        internal static IExifValue Create(ExifTag tag)
+        {
+            Throw.IfTrue(nameof(tag), tag == ExifTag.Unknown, "Invalid Tag");
+
+            switch (tag)
+            {
+                case ExifTag.ClipPath:
+                case ExifTag.VersionYear:
+                case ExifTag.XMP:
+                case ExifTag.CFAPattern2:
+                case ExifTag.TIFFEPStandardID:
+                case ExifTag.XPTitle:
+                case ExifTag.XPComment:
+                case ExifTag.XPAuthor:
+                case ExifTag.XPKeywords:
+                case ExifTag.XPSubject:
+                case ExifTag.GPSVersionID:
+                    return new ExifByteArray(tag, ExifDataType.Byte);
+
+                case ExifTag.FaxProfile:
+                case ExifTag.ModeNumber:
+                case ExifTag.GPSAltitudeRef:
+                    return new ExifByte(tag, ExifDataType.Byte);
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
