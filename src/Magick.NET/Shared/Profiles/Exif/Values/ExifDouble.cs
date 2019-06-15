02780f04733874bf69b25ef4fc1ba31c3a680740
Dirk Lemstra <dirk@lemstra.org>
2019-06-15 17:23:14 +0200

    Added ExifDouble and ExifDoubleArray.

---
+++ src/Magick.NET/Shared/Profiles/Exif/Values/ExifDouble.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System.Globalization;
+
+namespace ImageMagick
+{
+    /// <summary>
+    /// Exif value that contains a <see cref="double"/>.
+    /// </summary>
+    public sealed class ExifDouble : ExifValue<double>
+    {
+        internal ExifDouble(ExifTag tag)
+            : base(tag, ExifDataType.Double)
+        {
+        }
+
+        /// <summary>
+        /// Gets a string that represents the current value.
+        /// </summary>
+        protected override string StringValue => Value.ToString(CultureInfo.InvariantCulture);
+
+        internal static ExifDouble Create(ExifTag tag, double value) => new ExifDouble(tag) { Value = value };
+
+        /// <summary>
+        /// Tries to set the value and returns a value indicating whether the value could be set.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>A value indicating whether the value could be set.</returns>
+        protected override bool TrySetValue(object value)
+        {
+            switch (value)
+            {
+                case int intValue:
+                    Value = intValue;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ src/Magick.NET/Shared/Profiles/Exif/Values/ExifDoubleArray.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+namespace ImageMagick
+{
+    /// <summary>
+    /// Exif value that contains a <see cref="T:double[]"/>.
+    /// </summary>
+    public sealed class ExifDoubleArray : ExifArrayValue<double>
+    {
+        internal ExifDoubleArray(ExifTag tag)
+            : base(tag, ExifDataType.Double)
+        {
+        }
+
+        internal static ExifDoubleArray Create(ExifTag tag, double[] value) => new ExifDoubleArray(tag) { Value = value };
+    }
+}
