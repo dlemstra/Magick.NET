32dee2110c5de64fbc87d455c608fa5bf1d064dc
Dirk Lemstra <dirk@lemstra.org>
2019-06-15 18:31:41 +0200

    Added ExifSignedByte and ExifSignedByteArray.

---
+++ src/Magick.NET/Shared/Profiles/Exif/Values/ExifSignedByte.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System.Globalization;
+
+namespace ImageMagick
+{
+    /// <summary>
+    /// Exif value that contains a <see cref="sbyte"/>.
+    /// </summary>
+    public sealed class ExifSignedByte : ExifValue<sbyte>
+    {
+        private ExifSignedByte(ExifTag tag)
+            : base(tag, ExifDataType.SignedByte)
+        {
+        }
+
+        /// <summary>
+        /// Gets a string that represents the current value.
+        /// </summary>
+        protected override string StringValue => Value.ToString("X2", CultureInfo.InvariantCulture);
+
+        internal static ExifSignedByte Create(ExifTag tag, sbyte value) => new ExifSignedByte(tag) { Value = value };
+
+        /// <summary>
+        /// Tries to set the value and returns a value indicating whether the value could be set.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>A value indicating whether the value could be set.</returns>
+        protected override bool TrySetValue(object value)
+        {
+            switch (value)
+            {
+                case int intValue:
+                    Value = (sbyte)intValue;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ src/Magick.NET/Shared/Profiles/Exif/Values/ExifSignedByteArray.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+namespace ImageMagick
+{
+    /// <summary>
+    /// Exif value that contains a <see cref="T:sbyte[]"/>.
+    /// </summary>
+    public sealed class ExifSignedByteArray : ExifArrayValue<sbyte>
+    {
+        private ExifSignedByteArray(ExifTag tag)
+            : base(tag, ExifDataType.SignedByte)
+        {
+        }
+
+        internal static ExifSignedByteArray Create(ExifTag tag, sbyte[] value) => new ExifSignedByteArray(tag) { Value = value };
+    }
+}
