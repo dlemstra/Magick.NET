9271259f6200c109f67522b7025c34107139eb87
Dirk Lemstra <dirk@lemstra.org>
2019-06-16 21:27:14 +0200

    Added batch file to generate the Drawables.

---
+++ tools/FileGenerators/FileGenerator.ps1
@@ -0,0 +1,40 @@
+# Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+#
+# Licensed under the ImageMagick License (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+#
+#   https://www.imagemagick.org/script/license.php
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the
+# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+param (
+    [string]$name
+)
+
+. $PSScriptRoot\..\windows\utils.ps1
+
+function executeFile($path)
+{
+  $executable = fullPath $path
+  Invoke-Expression $executable
+  checkExitCode "Failed to execute: $($path)"
+}
+
+function generateFiles($name)
+{
+  buildSolution "tools\FileGenerators\FileGenerator.$name.sln" "Configuration=Release"
+  executeFile "tools\FileGenerators\$name\bin\Release\FileGenerator.$name.exe"
+}
+
+function buildMagickNET()
+{
+  buildSolution "Magick.NET.sln" "Configuration=ReleaseQ8,RunCodeAnalysis=false,TargetFramework=net40,Platform=x86"
+  buildSolution "Magick.NET.sln" "Configuration=ReleaseQ16,RunCodeAnalysis=false,TargetFramework=net40,Platform=x86"
+  buildSolution "Magick.NET.sln" "Configuration=ReleaseQ16-HDRI,RunCodeAnalysis=false,TargetFramework=net40,Platform=x86"
+}
+
+buildMagickNET
+generateFiles $name
