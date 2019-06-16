fb8da40439690b5d15939d478919093ab438697b
Dirk Lemstra <dirk@lemstra.org>
2019-06-16 23:58:48 +0200

    Added batch file to push the NuGet packages.

---
+++ publish/push.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+echo Are you sure?
+pause
+
+set /p ApiKey=<api.key.txt
+if not "%ApiKey%"=="" goto push
+
+echo Unable to find ApiKey.txt
+goto done
+
+:push
+for /r %%i in (*.nupkg) do ..\tools\windows\nuget.exe push %%i %ApiKey% -Source nuget.org
+
+:done
+pause
