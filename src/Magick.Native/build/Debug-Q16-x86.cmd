afa49bbbdac40bbc71f14755e7c07a1fd8436597
Dirk Lemstra <dirk@lemstra.org>
2019-06-23 13:52:13 +0200

    Added batch files to do a debug build of the native library to make it possible to step into the native code..

---
+++ src/Magick.Native/build/Debug-Q16-x86.cmd
@@ -0,0 +1,20 @@
+@echo off
+call "..\..\..\tools\windows\init.visualstudio.cmd"
+
+set quantumName=Q16
+set platformName=x86
+
+cd ..\..\..\..\Magick.Native\build\dotnet\windows\
+
+powershell .\build.ImageMagick.ps1 -config Debug -quantumName %quantumName% -platformName %platformName%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+powershell .\build.Native.ps1 -config Debug -quantumName %quantumName% -platformName %platformName%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cd ..\..\..\src\Native\bin\Debug%quantumName%\%platformName%
+
+copy /y Magick.Native-%quantumName%-%platformName%.dll ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\Debug%quantumName%\%platformName%\net45
+copy /y Magick.Native-%quantumName%-%platformName%.dll ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\Debug%quantumName%\%platformName%\netcoreapp2.0
+
+pause
