1dc6436732529b897a3d747bd60af212d81699e6
Dirk Lemstra <dirk@lemstra.org>
2019-06-23 11:34:58 +0200

    Upgraded Magick.Native.

--- src/Magick.Native/install.sh
+++ src/Magick.Native/install.sh
@@ -7,7 +7,7 @@ installPackage() {
     mkdir temp
 
     # Temporary download from DropBox
-    nuget_url="https://dl.dropboxusercontent.com/s/aeoswqx1km8s861/Magick.Native.$version.nupkg"
+    nuget_url="https://dl.dropboxusercontent.com/s/r0lvym0jrqyw0hz/Magick.Native.$version.nupkg"
     curl -s -o Magick.Native.$version.nupkg $nuget_url
 
     cwd=$(pwd)
