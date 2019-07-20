d6b4c5f713b32a30d7e965c12d496a330171cae1
Dirk Lemstra <dirk@lemstra.org>
2019-07-20 22:57:20 +0200

    Refactored unit tests for the GifOptimizer.

---
+++ tests/Magick.NET.Tests/Shared/Optimizers/GifOptimizerTests/TheCompressMethod.cs
@@ -0,0 +1,122 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System;
+using System.IO;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    public partial class GifOptimizerTests
+    {
+        [TestClass]
+        public class TheCompressMethod : GifOptimizerTests
+        {
+            [TestMethod]
+            public void ShouldCompress()
+            {
+                AssertCompressSmaller(Files.FujiFilmFinePixS1ProGIF);
+            }
+
+            [TestMethod]
+            public void ShouldTryToCompress()
+            {
+                AssertCompressNotSmaller(Files.RoseSparkleGIF);
+            }
+
+            [TestMethod]
+            public void ShouldBeAbleToCompressFileTwoTimes()
+            {
+                AssertCompressTwice(Files.FujiFilmFinePixS1ProGIF);
+            }
+
+            [TestClass]
+            public class WithFile : TheCompressMethod
+            {
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileIsNull()
+                {
+                    ExceptionAssert.Throws<ArgumentNullException>("file", () => Optimizer.Compress((FileInfo)null));
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileIsInvalid()
+                {
+                    ExceptionAssert.Throws<MagickCorruptImageErrorException>(() => Optimizer.Compress(Files.InvitationTIF));
+                }
+            }
+
+            [TestClass]
+            public class WithFileName : TheCompressMethod
+            {
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileNameIsNull()
+                {
+                    ExceptionAssert.Throws<ArgumentNullException>("fileName", () => Optimizer.Compress((string)null));
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileNameIsEmpty()
+                {
+                    ExceptionAssert.Throws<ArgumentException>("fileName", () => Optimizer.Compress(string.Empty));
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileNameIsInvalid()
+                {
+                    ExceptionAssert.Throws<MagickBlobErrorException>(() =>
+                    {
+                        Optimizer.Compress(Files.Missing);
+                    }, "error/blob.c/OpenBlob");
+                }
+            }
+
+            [TestClass]
+            public class WithStreamName : TheCompressMethod
+            {
+                [TestMethod]
+                public void ShouldThrowExceptionWhenStreamIsNull()
+                {
+                    ExceptionAssert.Throws<ArgumentNullException>("stream", () => Optimizer.Compress((Stream)null));
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenStreamIsNotReadable()
+                {
+                    using (TestStream stream = new TestStream(false, true, true))
+                    {
+                        ExceptionAssert.Throws<ArgumentException>("stream", () => Optimizer.Compress(stream));
+                    }
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenStreamIsNotWriteable()
+                {
+                    using (TestStream stream = new TestStream(true, false, true))
+                    {
+                        ExceptionAssert.Throws<ArgumentException>("stream", () => Optimizer.Compress(stream));
+                    }
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenStreamIsNotSeekable()
+                {
+                    using (TestStream stream = new TestStream(true, true, false))
+                    {
+                        ExceptionAssert.Throws<ArgumentException>("stream", () => Optimizer.Compress(stream));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/Magick.NET.Tests/Shared/Optimizers/GifOptimizerTests/TheLosslessCompressMethod.cs
@@ -0,0 +1,122 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System;
+using System.IO;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    public partial class GifOptimizerTests
+    {
+        [TestClass]
+        public class TheLosslessCompressMethod : GifOptimizerTests
+        {
+            [TestMethod]
+            public void ShouldCompressLossless()
+            {
+                AssertLosslessCompressSmaller(Files.FujiFilmFinePixS1ProGIF);
+            }
+
+            [TestMethod]
+            public void ShouldTryToCompressLossLess()
+            {
+                AssertLosslessCompressNotSmaller(Files.RoseSparkleGIF);
+            }
+
+            [TestMethod]
+            public void ShouldBeAbleToCompressFileTwoTimes()
+            {
+                AssertLosslessCompressTwice(Files.FujiFilmFinePixS1ProGIF);
+            }
+
+            [TestClass]
+            public class WithFile : TheCompressMethod
+            {
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileIsNull()
+                {
+                    ExceptionAssert.Throws<ArgumentNullException>("file", () => Optimizer.LosslessCompress((FileInfo)null));
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileIsInvalid()
+                {
+                    ExceptionAssert.Throws<MagickCorruptImageErrorException>(() => Optimizer.LosslessCompress(Files.InvitationTIF));
+                }
+            }
+
+            [TestClass]
+            public class WithFileName : TheCompressMethod
+            {
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileNameIsNull()
+                {
+                    ExceptionAssert.Throws<ArgumentNullException>("fileName", () => Optimizer.LosslessCompress((string)null));
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileNameIsEmpty()
+                {
+                    ExceptionAssert.Throws<ArgumentException>("fileName", () => Optimizer.LosslessCompress(string.Empty));
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenFileNameIsInvalid()
+                {
+                    ExceptionAssert.Throws<MagickBlobErrorException>(() =>
+                    {
+                        Optimizer.LosslessCompress(Files.Missing);
+                    }, "error/blob.c/OpenBlob");
+                }
+            }
+
+            [TestClass]
+            public class WithStreamName : TheCompressMethod
+            {
+                [TestMethod]
+                public void ShouldThrowExceptionWhenStreamIsNull()
+                {
+                    ExceptionAssert.Throws<ArgumentNullException>("stream", () => Optimizer.LosslessCompress((Stream)null));
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenStreamIsNotReadable()
+                {
+                    using (TestStream stream = new TestStream(false, true, true))
+                    {
+                        ExceptionAssert.Throws<ArgumentException>("stream", () => Optimizer.LosslessCompress(stream));
+                    }
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenStreamIsNotWriteable()
+                {
+                    using (TestStream stream = new TestStream(true, false, true))
+                    {
+                        ExceptionAssert.Throws<ArgumentException>("stream", () => Optimizer.LosslessCompress(stream));
+                    }
+                }
+
+                [TestMethod]
+                public void ShouldThrowExceptionWhenStreamIsNotSeekable()
+                {
+                    using (TestStream stream = new TestStream(true, true, false))
+                    {
+                        ExceptionAssert.Throws<ArgumentException>("stream", () => Optimizer.LosslessCompress(stream));
+                    }
+                }
+            }
+        }
+    }
+}
