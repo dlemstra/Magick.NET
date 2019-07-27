8a849f34df9e4180032b67757d1b14861b3ed36d
Dirk Lemstra <dirk@lemstra.org>
2019-07-27 11:42:07 +0200

    Revert changes.

--- build/linux/install.dependencies.sh
+++ build/linux/install.dependencies.sh
@@ -3,6 +3,10 @@ set -e
 
 apt update
 
+apt install cabextract nuget xfonts-utils  -y
+
+curl -s -o ttf-mscorefonts-installer_3.6_all.deb http://ftp.us.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
+
 sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"
 
-apt install msttcorefonts
+dpkg -i ttf-mscorefonts-installer_3.6_all.deb
