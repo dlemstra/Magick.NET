15dc2b397635ab2a484a1e67d675b257c6133a8e
Dirk Lemstra <dirk@lemstra.org>
2019-08-06 21:21:24 +0200

    Moved shard logic to Build.cmd.

---
+++ src/Magick.Native/build/Build.cmd
@@ -0,0 +1,37 @@
+@echo off
+call "..\..\..\tools\windows\init.visualstudio.cmd"
+
+set config=%1
+set quantumName=%2
+set platformName=%3
+
+if "%config%"=="" goto invalid
+if "%quantumName%"=="" goto invalid
+if "%platformName%"=="" goto invalid
+
+cd ..\..\..\..\Magick.Native\build\dotnet\windows\
+
+powershell .\build.ImageMagick.ps1 -config %config% -quantumName %quantumName% -platformName %platformName%
+if %errorlevel% neq 0 goto done
+
+powershell .\build.Native.ps1 -config  %config% -quantumName %quantumName% -platformName %platformName%
+if %errorlevel% neq 0 goto done
+
+cd ..\..\..\src\Magick.Native\bin\Debug%quantumName%\%platformName%
+
+copy /y Magick.Native-%quantumName%-%platformName%.dll ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\%config%%quantumName%\%platformName%\net45
+copy /y Magick.Native-%quantumName%-%platformName%.pdb ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\%config%%quantumName%\%platformName%\net45
+copy /y Magick.Native-%quantumName%-%platformName%.dll ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\%config%%quantumName%\%platformName%\netcoreapp2.0
+copy /y Magick.Native-%quantumName%-%platformName%.pdb ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\%config%%quantumName%\%platformName%\netcoreapp2.0
+
+goto done
+
+:invalid
+
+echo.
+echo Do not use this script directly.
+echo.
+
+:done
+
+pause
--- src/Magick.Native/build/Debug-Q16-x86.cmd
+++ src/Magick.Native/build/Debug-Q16-x86.cmd
@@ -1,22 +1,2 @@
 @echo off
-call "..\..\..\tools\windows\init.visualstudio.cmd"
-
-set quantumName=Q16
-set platformName=x86
-
-cd ..\..\..\..\Magick.Native\build\dotnet\windows\
-
-powershell .\build.ImageMagick.ps1 -config Debug -quantumName %quantumName% -platformName %platformName%
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-powershell .\build.Native.ps1 -config Debug -quantumName %quantumName% -platformName %platformName%
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-cd ..\..\..\src\Magick.Native\bin\Debug%quantumName%\%platformName%
-
-copy /y Magick.Native-%quantumName%-%platformName%.dll ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\Debug%quantumName%\%platformName%\net45
-copy /y Magick.Native-%quantumName%-%platformName%.pdb ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\Debug%quantumName%\%platformName%\net45
-copy /y Magick.Native-%quantumName%-%platformName%.dll ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\Debug%quantumName%\%platformName%\netcoreapp2.0
-copy /y Magick.Native-%quantumName%-%platformName%.pdb ..\..\..\..\..\..\Magick.NET\tests\Magick.NET.Tests\bin\Debug%quantumName%\%platformName%\netcoreapp2.0
-
-pause
+call "Build.cmd" Debug Q16 x86
