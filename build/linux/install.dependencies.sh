cf5c133f92818f0dabe63397db08a23fe6275b9c
Dirk Lemstra <dirk@lemstra.org>
2019-08-15 19:46:06 +0200

    Revert to previous method.

--- build/linux/install.dependencies.sh
+++ build/linux/install.dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"
-
 apt update
 
-apt install msttcorefonts -y
+apt install cabextract nuget xfonts-utils  -y
+
+curl -s -o ttf-mscorefonts-installer_3.6_all.deb http://ftp.us.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
+
+sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"
+
+dpkg -i ttf-mscorefonts-installer_3.6_all.deb
