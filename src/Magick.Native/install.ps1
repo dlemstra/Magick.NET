d6b561e4a42f1cdcd2a532cf282bcbb14eff5c37
Dirk Lemstra <dirk@lemstra.org>
2019-08-19 21:33:46 +0200

    Fixed indentation.

--- src/Magick.Native/install.ps1
+++ src/Magick.Native/install.ps1
@@ -89,16 +89,16 @@ function copyResources($source, $target) {
 }
 
 function createCompressedLibrary($folder, $quantum, $platform) {
-  $output = New-Object System.IO.FileStream "$folder\Magick.Native-$quantum-$platform.gz", ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
-  $gzipStream = New-Object System.IO.Compression.GzipStream $output, ([IO.Compression.CompressionMode]::Compress)
-  $input = New-Object System.IO.FileStream "$folder\Magick.Native-$quantum-$platform.dll", ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read)
-  $buffer = New-Object byte[]($input.Length)
-  $buffer = New-Object byte[]($input.Length)
-  [void]($input.Read($buffer, 0, $input.Length))
-  $gzipStream.Write($buffer, 0, $buffer.Length)
-  $input.Close()
-  $gzipStream.Close()
-  $output.Close()
+    $output = New-Object System.IO.FileStream "$folder\Magick.Native-$quantum-$platform.gz", ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
+    $gzipStream = New-Object System.IO.Compression.GzipStream $output, ([IO.Compression.CompressionMode]::Compress)
+    $input = New-Object System.IO.FileStream "$folder\Magick.Native-$quantum-$platform.dll", ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read)
+    $buffer = New-Object byte[]($input.Length)
+    $buffer = New-Object byte[]($input.Length)
+    [void]($input.Read($buffer, 0, $input.Length))
+    $gzipStream.Write($buffer, 0, $buffer.Length)
+    $input.Close()
+    $gzipStream.Close()
+    $output.Close()
 }
 
 function createCompressedLibraries($folder) {
