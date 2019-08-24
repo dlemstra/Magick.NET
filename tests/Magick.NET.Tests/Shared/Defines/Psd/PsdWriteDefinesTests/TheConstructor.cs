de773287b42a7c81678de42f9dbaee8709266824
Dirk Lemstra <dirk@lemstra.org>
2019-08-24 16:23:57 +0200

    Refactored unit tests for PSD defines.

---
+++ tests/Magick.NET.Tests/Shared/Defines/Psd/PsdWriteDefinesTests/TheConstructor.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    public partial class PsdWriteDefinesTests
+    {
+        [TestClass]
+        public class TheConstructor
+        {
+            [TestMethod]
+            public void ShouldSetAdditionalInfoToNullWhenNotSpecified()
+            {
+                using (IMagickImage image = new MagickImage())
+                {
+                    image.Settings.SetDefines(new PsdWriteDefines());
+
+                    Assert.AreEqual("None", image.Settings.GetDefine(MagickFormat.Psd, "additional-info"));
+                }
+            }
+        }
+    }
+}
