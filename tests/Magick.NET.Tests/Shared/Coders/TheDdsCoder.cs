3729ba798e72a472382b2243528380a08d555807
Dirk Lemstra <dirk@lemstra.org>
2019-08-24 10:17:25 +0200

    Refactored unit tests for DDS defines.

---
+++ tests/Magick.NET.Tests/Shared/Coders/TheDdsCoder.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System.IO;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    [TestClass]
+    public class TheDdsCoder
+    {
+        [TestMethod]
+        public void ShouldUseDxt1AsTheDefaultCompression()
+        {
+            using (IMagickImage input = new MagickImage(Files.Builtin.Logo))
+            {
+                using (IMagickImage output = WriteDds(input))
+                {
+                    Assert.AreEqual(CompressionMethod.DXT1, output.Compression);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ShouldUseDxt5AsTheDefaultCompressionForImagesWithAnAlphaChannel()
+        {
+            using (IMagickImage input = new MagickImage(Files.Builtin.Logo))
+            {
+                input.Alpha(AlphaOption.Set);
+
+                using (IMagickImage output = WriteDds(input))
+                {
+                    Assert.AreEqual(CompressionMethod.DXT5, output.Compression);
+                }
+            }
+        }
+
+        private static IMagickImage WriteDds(IMagickImage input)
+        {
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                input.Format = MagickFormat.Dds;
+                input.Write(memStream);
+                memStream.Position = 0;
+
+                return new MagickImage(memStream);
+            }
+        }
+    }
+}
---
+++ tests/Magick.NET.Tests/Shared/Defines/Dds/DdsWriteDefinesTests/TheFastMipmapsProperty.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    public partial class DdsWriteDefinesTests
+    {
+        [TestClass]
+        public class TheFastMipmapsProperty
+        {
+            [TestMethod]
+            public void ShouldSetTheDefine()
+            {
+                using (IMagickImage image = new MagickImage())
+                {
+                    var defines = new DdsWriteDefines
+                    {
+                        FastMipmaps = true,
+                    };
+
+                    image.Settings.SetDefines(defines);
+
+                    Assert.AreEqual("True", image.Settings.GetDefine(MagickFormat.Dds, "fast-mipmaps"));
+                }
+            }
+        }
+    }
+}
