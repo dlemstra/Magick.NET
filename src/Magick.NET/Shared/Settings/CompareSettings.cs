3e99b9eca6cb731094bb7a22995c933cc0aecf87
Dirk Lemstra <dirk@lemstra.org>
2019-09-02 19:25:07 +0200

    Removed unused using.

--- src/Magick.NET/Shared/Settings/CompareSettings.cs
+++ src/Magick.NET/Shared/Settings/CompareSettings.cs
@@ -10,8 +10,6 @@
 // either express or implied. See the License for the specific language governing permissions
 // and limitations under the License.
 
-using System;
-
 namespace ImageMagick
 {
     /// <summary>
