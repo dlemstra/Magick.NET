d6490bb171eb099262e9975fa3d633ec7645648a
Dirk Lemstra <dirk@lemstra.org>
2020-01-06 20:56:23 +0100

    Use single quotes instead for the powershell arguments.

--- build/windows/test.Magick.NET.cmd
+++ build/windows/test.Magick.NET.cmd
@@ -1,5 +1,6 @@
 @echo off
 call "..\..\tools\windows\init.visualstudio.cmd"
 
-powershell .\test.Magick.NET.ps1 -quantumName %1 -platformName %2
+set PLATFORM_NAME=%2
+powershell .\test.Magick.NET.ps1 -quantumName %1 -platformName %PLATFORM_NAME:"='%
 if %errorlevel% neq 0 exit /b %errorlevel%
