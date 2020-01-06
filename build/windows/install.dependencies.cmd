992ab33e3a57a4d868de4438bc59cc2fafa86e46
Dirk Lemstra <dirk@lemstra.org>
2020-01-06 22:33:46 +0100

    Run installer from batch file instead.

--- build/windows/install.dependencies.cmd
+++ build/windows/install.dependencies.cmd
@@ -2,3 +2,6 @@
 
 powershell .\install.dependencies.ps1
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+..\..\tools\windows\gs927w32.exe /S
+if %errorlevel% neq 0 exit /b %errorlevel%
--- build/windows/install.dependencies.ps1
+++ build/windows/install.dependencies.ps1
@@ -14,16 +14,15 @@ param (
     [string]$pfxUri = ''
 )
 
-Write-Host "Installing NuGet"
+Write-Host "Downloading NuGet"
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $targetNugetExe = "$PSScriptRoot\..\..\tools\windows\nuget.exe"
 Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
 
-Write-Host "Installing Ghostscript 9.27"
+Write-Host "Downloading Ghostscript 9.27"
 $sourceGhostscriptExe = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs927/gs927w32.exe"
 $targetGhostscriptExe = "$PSScriptRoot\..\..\tools\windows\gs927w32.exe"
 Invoke-WebRequest $sourceGhostscriptExe -OutFile $targetGhostscriptExe
-& $targetGhostscriptExe /S
 
 if ($pfxUri.Length -gt 0) {
     Write-Host "Downloading code signing certificate"
