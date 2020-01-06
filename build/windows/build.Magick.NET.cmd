fc80aca21add34749f4971ac6b113c72074e6a53
Dirk Lemstra <dirk@lemstra.org>
2020-01-06 20:24:45 +0100

    Added missing arguments.

--- build/windows/build.Magick.NET.cmd
+++ build/windows/build.Magick.NET.cmd
@@ -1,5 +1,5 @@
 @echo off
 call "..\..\tools\windows\init.visualstudio.cmd"
 
-powershell .\build.Magick.NET.ps1 -config %1
+powershell .\build.Magick.NET.ps1 -quantumName %1 -platformName %2 -config %3
 if %errorlevel% neq 0 exit /b %errorlevel%
--- build/windows/test.Magick.NET.cmd
+++ build/windows/test.Magick.NET.cmd
@@ -1,5 +1,5 @@
 @echo off
 call "..\..\tools\windows\init.visualstudio.cmd"
 
-powershell .\test.Magick.NET.ps1
+powershell .\test.Magick.NET.ps1 -quantumName %1 -platformName %2
 if %errorlevel% neq 0 exit /b %errorlevel%
