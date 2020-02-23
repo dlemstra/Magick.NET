0763b1507813a94674dc28fb1ca0644568749708
Dirk Lemstra <dirk@lemstra.org>
2020-02-23 13:34:10 +0100

    Use new switch syntax.

--- src/Magick.NET/Shared/Settings/MagickSettings.cs
+++ src/Magick.NET/Shared/Settings/MagickSettings.cs
@@ -691,26 +691,16 @@ namespace ImageMagick
             return instance;
         }
 
-        private string GetFormat()
+        private string GetFormat() => Format switch
         {
-            switch (Format)
-            {
-                case MagickFormat.Unknown:
-                    return null;
-                case MagickFormat.ThreeFr:
-                    return "3FR";
-                case MagickFormat.ThreeG2:
-                    return "3G2";
-                case MagickFormat.ThreeGp:
-                    return "3GP";
-                case MagickFormat.RadialGradient:
-                    return "RADIAL-GRADIENT";
-                case MagickFormat.SparseColor:
-                    return "SPARSE-COLOR";
-                default:
-                    return EnumHelper.GetName(Format).ToUpperInvariant();
-            }
-        }
+            MagickFormat.Unknown => null,
+            MagickFormat.ThreeFr => "3FR",
+            MagickFormat.ThreeG2 => "3G2",
+            MagickFormat.ThreeGp => "3GP",
+            MagickFormat.RadialGradient => "RADIAL-GRADIENT",
+            MagickFormat.SparseColor => "SPARSE-COLOR",
+            _ => EnumHelper.GetName(Format).ToUpperInvariant(),
+        };
 
         private void SetOptionAndArtifact(string key, double value) => SetOptionAndArtifact(key, value.ToString(CultureInfo.InvariantCulture));
 
