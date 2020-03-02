e0ab351a4889714ef2fa3cc6535866c0a3c20a53
Dirk Lemstra <dirk@lemstra.org>
2020-03-02 20:52:56 +0100

    Moved unit tests for the AutoThreshold method of MagickImage.

---
+++ tests/Magick.NET.Tests/Shared/MagickImageTests/TheAutoThresholdMethod.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2013-2020 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System.Collections.Generic;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        [TestClass]
+        public class TheAutoThresholdMethod
+        {
+            [TestMethod]
+            public void ShouldThresholdImageWithOTSUMethod()
+            {
+                using (IMagickImage image = new MagickImage(Files.FujiFilmFinePixS1ProPNG))
+                {
+                    image.AutoThreshold(AutoThresholdMethod.OTSU);
+
+                    Dictionary<MagickColor, int> colors = image.Histogram();
+
+                    Assert.AreEqual(ColorType.Bilevel, image.DetermineColorType());
+                    Assert.AreEqual(67844, colors[MagickColors.Black]);
+                    Assert.AreEqual(172156, colors[MagickColors.White]);
+                }
+            }
+
+            [TestMethod]
+            public void ShouldThresholdImageWithTriangleMethod()
+            {
+                using (IMagickImage image = new MagickImage(Files.FujiFilmFinePixS1ProPNG))
+                {
+                    image.AutoThreshold(AutoThresholdMethod.Triangle);
+
+                    Dictionary<MagickColor, int> colors = image.Histogram();
+
+                    Assert.AreEqual(ColorType.Bilevel, image.DetermineColorType());
+                    Assert.AreEqual(210553, colors[MagickColors.Black]);
+                    Assert.AreEqual(29447, colors[MagickColors.White]);
+                }
+            }
+        }
+    }
+}
