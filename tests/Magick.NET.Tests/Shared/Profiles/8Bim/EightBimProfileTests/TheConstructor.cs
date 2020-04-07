fd0a12c60f4f35688fef071effc9f8e0fa037f4c
Dirk Lemstra <dirk@lemstra.org>
2020-04-07 22:22:37 +0200

    Moved EightBimProfileTests to separate files.

---
+++ tests/Magick.NET.Tests/Shared/Profiles/8Bim/EightBimProfileTests/TheConstructor.cs
@@ -0,0 +1,69 @@
+﻿// Copyright 2013-2020 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    public partial class EightBimProfileTests
+    {
+        [TestClass]
+        public class TheConstructor
+        {
+            [TestMethod]
+            public void ShouldAllowInvalidValues()
+            {
+                var bytes = ToBytes('8', 'B', 'I', 'M', (short)42, (byte)0, 1);
+
+                var profile = new EightBimProfile(bytes);
+                Assert.AreEqual(0, profile.Values.Count());
+
+                bytes = ToBytes('8', 'B', 'I', 'M', (short)42, (short)0, -1);
+
+                profile = new EightBimProfile(bytes);
+                Assert.AreEqual(0, profile.Values.Count());
+            }
+
+            [TestMethod]
+            public void ShouldReadProfileValues()
+            {
+                var bytes = ToBytes('8', 'B', 'I', 'M', (short)2000, (short)0, 1, (byte)0);
+
+                var profile = new EightBimProfile(bytes);
+                Assert.AreEqual(1, profile.Values.Count());
+                Assert.AreEqual(0, profile.ClipPaths.Count());
+            }
+
+            private static byte[] ToBytes(params object[] objects)
+            {
+                var bytes = new List<byte>();
+                foreach (object obj in objects)
+                {
+                    if (obj is byte)
+                        bytes.Add((byte)obj);
+                    else if (obj is char)
+                        bytes.Add((byte)(char)obj);
+                    else if (obj is short)
+                        bytes.AddRange(BitConverter.GetBytes((short)obj).Reverse());
+                    else if (obj is int)
+                        bytes.AddRange(BitConverter.GetBytes((int)obj).Reverse());
+                }
+
+                return bytes.ToArray();
+            }
+        }
+    }
+}
