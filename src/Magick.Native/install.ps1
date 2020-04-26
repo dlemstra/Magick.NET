5c46357a88fea98b1382aa863fad93a9f6fcf2f5
Dirk Lemstra <dirk@lemstra.org>
2020-04-26 21:52:22 +0200

    Upgraded Magick.Native.

--- src/Magick.Native/install.ps1
+++ src/Magick.Native/install.ps1
@@ -19,7 +19,7 @@ function installPackage($version, $target) {
     [void](New-Item -ItemType directory -Path $temp)
 
     # Temporary download from DropBox
-    $url = "https://dl.dropboxusercontent.com/s/xcauylva2zpc4a8/Magick.Native.$version.nupkg"
+    $url = "https://dl.dropboxusercontent.com/s/lug39yf0d1ocndo/Magick.Native.$version.nupkg"
     Invoke-WebRequest $url -Outfile "$temp\Magick.Native.$version.nupkg"
     ..\..\tools\windows\nuget.exe install Magick.Native -Version $version -OutputDirectory "$target" -Source $temp
 
