8137f8d6fd4f4e174904e00f8a2ce0231f46d26d
Dirk Lemstra <dirk@lemstra.org>
2020-05-23 21:16:28 +0200

    Refactored unit tests for Percentage.

---
+++ tests/Magick.NET.Tests/Shared/Types/PercentageTests/TheEqualsMethod.cs
@@ -0,0 +1,84 @@
+﻿// Copyright 2013-2020 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Core.Tests
+{
+    public partial class PercentageTests
+    {
+        [TestClass]
+        public class TheEqualsMethod
+        {
+            [TestMethod]
+            public void ShouldReturnFalseWhenInstanceIsNull()
+            {
+                Percentage percentage = default;
+
+                Assert.IsFalse(percentage.Equals(null));
+            }
+
+            [TestMethod]
+            public void ShouldReturnTrueWhenInstanceIsTheSame()
+            {
+                Percentage percentage = default;
+
+                Assert.IsTrue(percentage.Equals(percentage));
+            }
+
+            [TestMethod]
+            public void ShouldReturnTrueWhenObjectIsTheSame()
+            {
+                Percentage percentage = default;
+
+                Assert.IsTrue(percentage.Equals((object)percentage));
+            }
+
+            [TestMethod]
+            public void ShouldReturnTrueWhenInstanceIsEqual()
+            {
+                var first = new Percentage(50.0);
+                var second = new Percentage(50.0);
+
+                Assert.IsTrue(first.Equals(second));
+            }
+
+            [TestMethod]
+            public void ShouldReturnTrueWhenObjectIsEqual()
+            {
+                var first = new Percentage(50.0);
+                var second = new Percentage(50.0);
+
+                Assert.IsTrue(first.Equals((object)second));
+            }
+
+            [TestMethod]
+            public void ShouldReturnFalseWhenInstanceIsNotEqual()
+            {
+                var first = new Percentage(50.0);
+                var second = new Percentage(50.1);
+
+                Assert.IsFalse(first.Equals(second));
+            }
+
+            [TestMethod]
+            public void ShouldReturnFalseWhenObjectIsNotEqual()
+            {
+                var first = new Percentage(50.0);
+                var second = new Percentage(50.1);
+
+                Assert.IsFalse(first.Equals((object)second));
+            }
+        }
+    }
+}
---
+++ tests/Magick.NET.Tests/Shared/Types/PercentageTests/TheToInt32Method.cs
@@ -0,0 +1,38 @@
+﻿// Copyright 2013-2020 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    public partial class PercentageTests
+    {
+        [TestClass]
+        public class TheToInt32Method
+        {
+            [TestMethod]
+            public void ShouldReturnZeroForZeroPointFour()
+            {
+                var percentage = new Percentage(0.4);
+                Assert.AreEqual(0, percentage.ToInt32());
+            }
+
+            [TestMethod]
+            public void ShouldReturnOneForZeroPointFive()
+            {
+                var percentage = new Percentage(0.5);
+                Assert.AreEqual(1, percentage.ToInt32());
+            }
+        }
+    }
+}
