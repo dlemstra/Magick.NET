4ed5229f18431db10a359864e27bf47f82a9f537
Dirk Lemstra <dirk@lemstra.org>
2020-05-25 07:55:59 +0200

    Corrected using.

--- src/Magick.NET/Shared/Extensions/IExifProfileExtensions.cs
+++ src/Magick.NET/Shared/Extensions/IExifProfileExtensions.cs
@@ -10,8 +10,9 @@
 // either express or implied. See the License for the specific language governing permissions
 // and limitations under the License.
 
-#if Q8
 using System;
+
+#if Q8
 using QuantumType = System.Byte;
 #elif Q16
 using QuantumType = System.UInt16;
