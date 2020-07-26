9d2aca681764e4bfe1914ec6534d51086398ba97
Dirk Lemstra <dirk@lemstra.org>
2020-07-26 15:23:39 +0200

    Corrected method name.

--- tests/Magick.NET.Tests/Shared/Extensions/IDeskewSettingsExtensionsTests/TheSetImageArtifactsMethod.cs
+++ tests/Magick.NET.Tests/Shared/Extensions/IDeskewSettingsExtensionsTests/TheSetImageArtifactsMethod.cs
@@ -34,7 +34,7 @@ namespace Magick.NET.Tests
             }
 
             [TestMethod]
-            public void ShouldSetRemoveAutoCrop()
+            public void ShouldSetAutoCrop()
             {
                 using (var image = new MagickImage())
                 {
