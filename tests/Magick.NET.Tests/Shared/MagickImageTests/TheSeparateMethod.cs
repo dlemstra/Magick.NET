aaa6ba1657c57d47bced9a0d8de82e2eb3437bbb
Dirk Lemstra <dirk@lemstra.org>
2020-08-09 18:16:08 +0200

    Code cleanup.

--- tests/Magick.NET.Tests/Shared/MagickImageTests/TheSeparateMethod.cs
+++ tests/Magick.NET.Tests/Shared/MagickImageTests/TheSeparateMethod.cs
@@ -10,11 +10,7 @@
 // either express or implied. See the License for the specific language governing permissions
 // and limitations under the License.
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ImageMagick;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
