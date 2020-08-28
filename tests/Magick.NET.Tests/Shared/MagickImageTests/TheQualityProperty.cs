033ed39462c90f228b12d170386e35b91caf6127
Dirk Lemstra <dirk@lemstra.org>
2020-08-28 10:45:19 +0200

    Added unit tests for the quality property.

---
+++ tests/Magick.NET.Tests/Shared/MagickImageTests/TheQualityProperty.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2013-2020 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        [TestClass]
+        public class TheQualityProperty
+        {
+            [TestMethod]
+            public void ShouldNotAllowValueBelowOne()
+            {
+                var image = new MagickImage
+                {
+                    Quality = 0,
+                };
+
+                Assert.AreEqual(image.Quality, 1);
+            }
+
+            [TestMethod]
+            public void ShouldNotAllowValueAbove100()
+            {
+                var image = new MagickImage
+                {
+                    Quality = 101,
+                };
+
+                Assert.AreEqual(image.Quality, 100);
+            }
+
+            [TestMethod]
+            public void ShouldSetTheBackgroundColorWhenReadingImage()
+            {
+                using (var image = new MagickImage(Files.CMYKJPG))
+                {
+                    Assert.AreEqual(91, image.Quality);
+                }
+            }
+        }
+    }
+}
