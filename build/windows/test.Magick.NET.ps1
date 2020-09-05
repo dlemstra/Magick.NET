4f2e9f09fc7c5c7d8255e32d8010359384059367
Dirk Lemstra <dirk@lemstra.org>
2020-09-05 17:57:47 +0200

    More build tweaks.

--- build/windows/test.Magick.NET.ps1
+++ build/windows/test.Magick.NET.ps1
@@ -39,14 +39,15 @@ function testMagickNET($quantumName, $platformName) {
     runTests $quantumName $platformName "net45" "Magick.NET"
 
     if ($platformName -ne "Any CPU") {
-        runTests "" $platformName "netcoreapp3.0" "Magick.NET.Core"
         runTests $quantumName $platformName "netcoreapp3.0" "Magick.NET"
-        runTests $quantumName $platformName "netcoreapp3.0" "Magick.NET.SystemDrawing"
-        runTests $quantumName $platformName "netcoreapp3.0" "Magick.NET.SystemWindowsMedia"
 
         if ($quantumName -contains "OpenMP") {
             return
         }
+
+        runTests "" $platformName "netcoreapp3.0" "Magick.NET.Core"
+        runTests $quantumName $platformName "netcoreapp3.0" "Magick.NET.SystemDrawing"
+        runTests $quantumName $platformName "netcoreapp3.0" "Magick.NET.SystemWindowsMedia"
     } else {
         runTests "" $platformName "net45" "Magick.NET.Core"
     }
