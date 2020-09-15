780baecb753cb67609029b53f184c5b77b58f853
Dirk Lemstra <dirk@lemstra.org>
2020-09-15 22:15:42 +0200

    Updated file name.

--- build/windows/install.dependencies.cmd
+++ build/windows/install.dependencies.cmd
@@ -3,5 +3,5 @@
 powershell .\install.dependencies.ps1
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-..\..\tools\windows\gs927w32.exe /S
+..\..\tools\windows\gs9531w32.exe /S
 if %errorlevel% neq 0 exit /b %errorlevel%
