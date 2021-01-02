24d5421fe57c9b99268cc32fa4a1629d2ba92077
Dirk Lemstra <dirk@lemstra.org>
2021-01-02 21:40:29 +0100

    Added unit test for the xc coder.

---
+++ tests/Magick.NET.Tests/Coders/TheXcCoder.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2013-2020 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class TheXcCoder
+    {
+        [Fact]
+        public void ShouldHandleSrgbaColor()
+        {
+            using (var image = new MagickImage("xc:srgba(255,0,0,1", 1, 1))
+            {
+                ColorAssert.Equal(MagickColors.Red, image, 0, 0);
+            }
+        }
+    }
+}
