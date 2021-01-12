0b198beafbe2e880823fffd2a2145adb58b2f94f
Dirk Lemstra <dirk@lemstra.org>
2021-01-12 23:51:17 +0100

    Try to repair the file before running the unit tests.

--- build/linux/install.dependencies.sh
+++ build/linux/install.dependencies.sh
@@ -15,7 +15,3 @@ wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9
 tar zxvf ghostscript-9.53.3-linux-x86_64.tgz
 cp ghostscript-9.53.3-linux-x86_64/gs-9533-linux-x86_64 /usr/bin/gs
 chmod 755 /usr/bin/gs
-
-filename=/__w/Magick.NET/Magick.NET/tests/Magick.NET.Tests/Images/Coders/sample.pdf
-echo gs -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT --permit-file-read="$filename" -c "($filename) (r) file runpdfbegin pdfpagecount = quit"
-gs -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT --permit-file-read="$filename" -c "($filename) (r) file runpdfbegin pdfpagecount = quit"
--- build/linux/test.Magick.NET.sh
+++ build/linux/test.Magick.NET.sh
@@ -10,6 +10,15 @@ buildAndTest() {
     dotnet test tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f netcoreapp3.1 -c Test$quantum
 }
 
+filename=tests/Magick.NET.Tests/Images/Coders/sample.pdf
+echo gs -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT --permit-file-read="$filename" -c "($filename) (r) file runpdfbegin pdfpagecount = quit"
+gs -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT --permit-file-read="$filename" -c "($filename) (r) file runpdfbegin pdfpagecount = quit"
+
+gs -o fixed.pdf -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress $filename
+cp fixed.pdf $filename
+
+gs -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT --permit-file-read="$filename" -c "($filename) (r) file runpdfbegin pdfpagecount = quit"
+
 buildAndTest "Q8"
 buildAndTest "Q16"
 buildAndTest "Q16-HDRI"
