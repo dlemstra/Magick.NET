3c1424b76e159089da65f0779290f29b6a5b73ea
Dirk Lemstra <dirk@lemstra.org>
2021-01-12 23:27:47 +0100

    Check output of the Ghostscript command.

--- build/linux/install.dependencies.sh
+++ build/linux/install.dependencies.sh
@@ -15,3 +15,7 @@ wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9
 tar zxvf ghostscript-9.53.3-linux-x86_64.tgz
 cp ghostscript-9.53.3-linux-x86_64/gs-9533-linux-x86_64 /usr/bin/gs
 chmod 755 /usr/bin/gs
+
+filename=/__w/Magick.NET/Magick.NET/tests/Magick.NET.Tests/Images/Coders/sample.pdf
+echo gs -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT --permit-file-read="$filename" -c "($filename) (r) file runpdfbegin pdfpagecount = quit"
+gs -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT --permit-file-read="$filename" -c "($filename) (r) file runpdfbegin pdfpagecount = quit"
