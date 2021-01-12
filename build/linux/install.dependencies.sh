54edb3bb86db6bb202ad38cd62383d98576d632a
Dirk Lemstra <dirk@lemstra.org>
2021-01-12 22:57:10 +0100

    Removed temporary check.

--- build/linux/install.dependencies.sh
+++ build/linux/install.dependencies.sh
@@ -15,5 +15,3 @@ wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9
 tar zxvf ghostscript-9.53.3-linux-x86_64.tgz
 cp ghostscript-9.53.3-linux-x86_64/gs-9533-linux-x86_64 /usr/bin/gs
 chmod 755 /usr/bin/gs
-which gs
-gs -h
