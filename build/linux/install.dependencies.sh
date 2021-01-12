fc84ca666f06b940292f559c6a7a677cafac4eb2
Dirk Lemstra <dirk@lemstra.org>
2021-01-12 21:54:26 +0100

    Make files executable.

--- build/linux/install.dependencies.sh
+++ build/linux/install.dependencies.sh
@@ -14,3 +14,4 @@ fc-cache
 wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9533/ghostscript-9.53.3-linux-x86_64.tgz
 tar zxvf ghostscript-9.53.3-linux-x86_64.tgz
 cp ghostscript-9.53.3-linux-x86_64/gs-9533-linux-x86_64 /usr/bin/gs
+chmod 755 /usr/bin/gs
