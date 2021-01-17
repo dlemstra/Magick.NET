e3b01aec65633f0b801c65fc1fba1ddbcdf45d9f
Dirk Lemstra <dirk@lemstra.org>
2021-01-17 11:33:34 +0100

    Moved test to the correct file.

---
+++ tests/Magick.NET.Tests/Profiles/Iptc/IptcProfileTests/TheGetAllValuesMethod.cs
@@ -0,0 +1,38 @@
+﻿// Copyright 2013-2021 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
+//
+// Licensed under the ImageMagick License (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+//
+//   https://www.imagemagick.org/script/license.php
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System.Linq;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class IptcProfileTests
+    {
+        public class TheGetAllValuesMethod
+        {
+            [Fact]
+            public void ShouldReturnAllValues()
+            {
+                var profile = new IptcProfile();
+                profile.SetValue(IptcTag.Byline, "test");
+                profile.SetValue(IptcTag.Byline, "test2");
+                profile.SetValue(IptcTag.Caption, "test");
+
+                var result = profile.GetAllValues(IptcTag.Byline);
+
+                Assert.NotNull(result);
+                Assert.Equal(2, result.Count());
+            }
+        }
+    }
+}
--- tests/Magick.NET.Tests/Profiles/Iptc/IptcProfileTests/TheGetValueMethod.cs
+++ tests/Magick.NET.Tests/Profiles/Iptc/IptcProfileTests/TheGetValueMethod.cs
@@ -44,20 +44,6 @@ namespace Magick.NET.Tests
                     Assert.Equal("Communications", value.Value);
                 }
             }
-
-            [Fact]
-            public void ShouldReturnAllValues()
-            {
-                var profile = new IptcProfile();
-                profile.SetValue(IptcTag.Byline, "test");
-                profile.SetValue(IptcTag.Byline, "test2");
-                profile.SetValue(IptcTag.Caption, "test");
-
-                var result = profile.GetAllValues(IptcTag.Byline);
-
-                Assert.NotNull(result);
-                Assert.Equal(2, result.Count());
-            }
         }
     }
 }
