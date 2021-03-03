0549449cec2a6dc4b8e42db96e6342d909086124
Dirk Lemstra <dirk@lemstra.org>
2021-03-03 20:04:15 +0100

    Removed year from the Copyright in the file header and made it much smaller.

--- tests/Magick.NET.Tests/MagickNETTests/Init.cs
+++ tests/Magick.NET.Tests/MagickNETTests/Init.cs
@@ -1,14 +1,5 @@
-﻿// Copyright 2013-2021 Dirk Lemstra <https://github.com/dlemstra/Magick.NET/>
-//
-// Licensed under the ImageMagick License (the "License"); you may not use this file except in
-// compliance with the License. You may obtain a copy of the License at
-//
-//   https://imagemagick.org/script/license.php
-//
-// Unless required by applicable law or agreed to in writing, software distributed under the
-// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions
-// and limitations under the License.
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
 
 using System.IO;
 using Xunit;
