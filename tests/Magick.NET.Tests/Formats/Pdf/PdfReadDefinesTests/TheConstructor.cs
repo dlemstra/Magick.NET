8aaaf54ec4a0fec0e5be575b259f0685909fa504
Dirk Lemstra <dirk@lemstra.org>
2021-04-18 14:29:37 +0200

    Added extra test.

---
+++ tests/Magick.NET.Tests/Formats/Pdf/PdfReadDefinesTests/TheConstructor.cs
@@ -0,0 +1,30 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using ImageMagick.Formats;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class PdfReadDefinesTests
+    {
+        public class TheConstructor
+        {
+            [Fact]
+            public void ShouldNotSetAnyDefine()
+            {
+                using (var image = new MagickImage())
+                {
+                    image.Settings.SetDefines(new PdfReadDefines());
+
+                    Assert.Null(image.Settings.GetDefine(MagickFormat.Pdf, "fit-page"));
+                    Assert.Null(image.Settings.GetDefine(MagickFormat.Pdf, "interpolate"));
+                    Assert.Null(image.Settings.GetDefine("authenticate"));
+                    Assert.Null(image.Settings.GetDefine(MagickFormat.Pdf, "use-cropbox"));
+                    Assert.Null(image.Settings.GetDefine(MagickFormat.Pdf, "use-trimbox"));
+                }
+            }
+        }
+    }
+}
