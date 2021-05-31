90d62e6ee76f2554c9564ccf5be478647f6102a3
Dirk Lemstra <dirk@lemstra.org>
2021-05-31 21:57:21 +0200

    Published Magick.NET 7.24.1.0

--- publish/update.version.cmd
+++ publish/update.version.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-powershell .\update.version.ps1 -library "Magick.NET" -version "7.24.0.0"
-powershell .\update.version.ps1 -library "Magick.NET.Core" -version "7.0.0.0"
-powershell .\update.version.ps1 -library "Magick.NET.SystemDrawing" -version "2.0.16.0"
-powershell .\update.version.ps1 -library "Magick.NET.SystemWindowsMedia" -version "3.0.10.0"
+powershell .\update.version.ps1 -library "Magick.NET" -version "7.24.1.0"
+powershell .\update.version.ps1 -library "Magick.NET.Core" -version "7.0.1.0"
+powershell .\update.version.ps1 -library "Magick.NET.SystemDrawing" -version "2.0.17.0"
+powershell .\update.version.ps1 -library "Magick.NET.SystemWindowsMedia" -version "3.0.11.0"
 powershell .\update.version.ps1
