21e9c292a2abf1f3cfd2db7040c4105400382aba
Dirk Lemstra <dirk@lemstra.org>
2021-05-31 21:49:11 +0200

    Added unit tests for writing the xmp profile in the webp decoder.

---
+++ tests/Magick.NET.Tests/Coders/TheWebPCoder.cs
@@ -0,0 +1,33 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Text;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class TheWebPCoder
+    {
+        [Fact]
+        public void ShouldReadAndWriteTheXmpProfile()
+        {
+            var xmpData = Encoding.UTF8.GetBytes(@"<x:xmpmeta xmlns:x=""adobe:ns:meta/"" x:xmptk=""XMPTk 2.8""><rdf:RDF xmlns:rdf=""http://www.w3.org/1999/02/22-rdf-syntax-ns#""></rdf:RDF></x:xmpmeta>");
+
+            using (var input = new MagickImage(Files.Builtin.Logo))
+            {
+                input.SetProfile(new XmpProfile(xmpData));
+
+                var data = input.ToByteArray(MagickFormat.WebP);
+
+                using (var output = new MagickImage(data))
+                {
+                    var profile = output.GetXmpProfile();
+
+                    Assert.NotNull(profile);
+                    Assert.Equal(xmpData, profile.ToByteArray());
+                }
+            }
+        }
+    }
+}
