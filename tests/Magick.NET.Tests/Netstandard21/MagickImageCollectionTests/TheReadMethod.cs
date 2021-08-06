8601168719176567de31137b6e8cf64bed123e4a
Dirk Lemstra <dirk@lemstra.org>
2021-08-06 21:39:55 +0200

    Added missing unit tests.

--- tests/Magick.NET.Tests/Netstandard21/MagickImageCollectionTests/TheReadMethod.cs
+++ tests/Magick.NET.Tests/Netstandard21/MagickImageCollectionTests/TheReadMethod.cs
@@ -4,6 +4,7 @@
 #if NETCORE
 
 using System;
+using System.Buffers;
 using System.IO;
 using System.Text;
 using ImageMagick;
@@ -15,6 +16,120 @@ namespace Magick.NET.Tests
     {
         public partial class TheReadMethod
         {
+            public class WithReadOnlySequence
+            {
+                [Fact]
+                public void ShouldThrowExceptionWhenDataIsEmpty()
+                {
+                    using (var images = new MagickImageCollection())
+                    {
+                        Assert.Throws<ArgumentException>("data", () => images.Read(ReadOnlySequence<byte>.Empty));
+                    }
+                }
+
+                [Fact]
+                public void ShouldResetTheFormatAfterReading()
+                {
+                    var readSettings = new MagickReadSettings
+                    {
+                        Format = MagickFormat.Png,
+                    };
+
+                    var bytes = File.ReadAllBytes(Files.CirclePNG);
+
+                    using (var images = new MagickImageCollection())
+                    {
+                        images.Read(new ReadOnlySequence<byte>(bytes), readSettings);
+
+                        Assert.Equal(MagickFormat.Unknown, images[0].Settings.Format);
+                    }
+                }
+
+                [Fact]
+                public void ShouldSupportMultipleSegments()
+                {
+                    using (var images = new MagickImageCollection())
+                    {
+                        var bytes = File.ReadAllBytes(Files.SnakewarePNG);
+                        var sequence = TestReadOnlySequence.Create(bytes);
+
+                        images.Read(sequence);
+
+                        Assert.Equal(286, images[0].Width);
+                        Assert.Equal(67, images[0].Height);
+                    }
+                }
+            }
+
+            public class WithReadOnlySequenceAndMagickFormat
+            {
+                [Fact]
+                public void ShouldThrowExceptionWhenDataIsEmpty()
+                {
+                    using (var images = new MagickImageCollection())
+                    {
+                        Assert.Throws<ArgumentException>("data", () => images.Read(ReadOnlySequence<byte>.Empty, MagickFormat.Png));
+                    }
+                }
+
+                [Fact]
+                public void ShouldUseTheCorrectReaderWhenFormatIsSet()
+                {
+                    var bytes = Encoding.ASCII.GetBytes("%PDF-");
+
+                    using (var images = new MagickImageCollection())
+                    {
+                        var exception = Assert.Throws<MagickCorruptImageErrorException>(() => images.Read(new ReadOnlySequence<byte>(bytes), MagickFormat.Png));
+
+                        Assert.Contains("ReadPNGImage", exception.Message);
+                    }
+                }
+            }
+
+            public class WithReadOnlySequenceAndMagickReadSettings
+            {
+                [Fact]
+                public void ShouldThrowExceptionWhenDataIsEmpty()
+                {
+                    using (var images = new MagickImageCollection())
+                    {
+                        var settings = new MagickReadSettings();
+
+                        Assert.Throws<ArgumentException>("data", () => images.Read(ReadOnlySequence<byte>.Empty, settings));
+                    }
+                }
+
+                [Fact]
+                public void ShouldUseTheCorrectReaderWhenFormatIsSet()
+                {
+                    var bytes = Encoding.ASCII.GetBytes("%PDF-");
+                    var settings = new MagickReadSettings
+                    {
+                        Format = MagickFormat.Png,
+                    };
+
+                    using (var images = new MagickImageCollection())
+                    {
+                        var exception = Assert.Throws<MagickCorruptImageErrorException>(() => images.Read(new ReadOnlySequence<byte>(bytes), settings));
+
+                        Assert.Contains("ReadPNGImage", exception.Message);
+                    }
+                }
+
+                [Fact]
+                public void ShouldNotThrowExceptionWhenSettingsIsNull()
+                {
+                    var bytes = File.ReadAllBytes(Files.SnakewarePNG);
+
+                    using (var images = new MagickImageCollection())
+                    {
+                        images.Read(new ReadOnlySequence<byte>(bytes), null);
+
+                        Assert.Single(images);
+                    }
+                }
+            }
+
             public class WithReadonlySpan
             {
                 [Fact]
