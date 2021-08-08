a70bcbc5f34dcee38ca9a1122f9c930ddebe33ef
Dirk Lemstra <dirk@lemstra.org>
2021-08-08 12:00:02 +0200

    Added missing unit test.

--- tests/Magick.NET.Tests/Netstandard21/MagickImageTests/TheWriteMethod.cs
+++ tests/Magick.NET.Tests/Netstandard21/MagickImageTests/TheWriteMethod.cs
@@ -7,6 +7,7 @@ using System;
 using System.Buffers;
 using System.IO;
 using ImageMagick;
+using ImageMagick.Formats;
 using Xunit;
 
 #if Q8
@@ -60,6 +61,53 @@ namespace Magick.NET.Tests
                         using (var output = new MagickImage(bufferWriter.WrittenSpan))
                         {
                             Assert.Equal(MagickFormat.Tiff, output.Format);
+                        }
+                    }
+                }
+            }
+
+            public class WithBufferWriterAndWriteDefines
+            {
+                [Fact]
+                public void ShouldThrowExceptionWhenBufferWriterIsNull()
+                {
+                    using (var image = new MagickImage())
+                    {
+                        var defines = new JpegWriteDefines();
+
+                        Assert.Throws<ArgumentNullException>("bufferWriter", () => image.Write((IBufferWriter<QuantumType>)null, defines));
+                    }
+                }
+
+                [Fact]
+                public void ShouldThrowExceptionWhenDefinesIsNull()
+                {
+                    using (var image = new MagickImage())
+                    {
+                        var bufferWriter = new ArrayBufferWriter<byte>();
+
+                        Assert.Throws<ArgumentNullException>("defines", () => image.Write(bufferWriter, null));
+                    }
+                }
+
+                [Fact]
+                public void ShouldUseTheSpecifiedFormat()
+                {
+                    using (var input = new MagickImage(Files.CirclePNG))
+                    {
+                        var bufferWriter = new ArrayBufferWriter<byte>();
+
+                        var defines = new JpegWriteDefines
+                        {
+                            DctMethod = JpegDctMethod.Fast,
+                        };
+
+                        input.Write(bufferWriter, defines);
+                        Assert.Equal(MagickFormat.Png, input.Format);
+
+                        using (var output = new MagickImage(bufferWriter.WrittenSpan))
+                        {
+                            Assert.Equal(MagickFormat.Jpeg, output.Format);
                         }
                     }
                 }
