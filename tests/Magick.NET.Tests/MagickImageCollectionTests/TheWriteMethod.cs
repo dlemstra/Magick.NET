58273bc568a495643e3dd8d765ef433f83e268ab
Dirk Lemstra <dirk@lemstra.org>
2021-08-08 11:16:23 +0200

    Corrected unit test order.

--- tests/Magick.NET.Tests/MagickImageCollectionTests/TheWriteMethod.cs
+++ tests/Magick.NET.Tests/MagickImageCollectionTests/TheWriteMethod.cs
@@ -76,6 +76,48 @@ namespace Magick.NET.Tests
                 }
             }
 
+            public class WithFileName
+            {
+                [Fact]
+                public void ShouldThrowExceptionWhenFileIsNull()
+                {
+                    using (var images = new MagickImageCollection())
+                    {
+                        Assert.Throws<ArgumentNullException>("fileName", () => images.Write((string)null));
+                    }
+                }
+            }
+
+            public class WithFileNameAndMagickFormat
+            {
+                [Fact]
+                public void ShouldThrowExceptionWhenFileIsNull()
+                {
+                    using (var images = new MagickImageCollection())
+                    {
+                        Assert.Throws<ArgumentNullException>("fileName", () => images.Write((string)null, MagickFormat.Bmp));
+                    }
+                }
+
+                [Fact]
+                public void ShouldUseTheSpecifiedFormat()
+                {
+                    using (var input = new MagickImageCollection(Files.CirclePNG))
+                    {
+                        using (var tempfile = new TemporaryFile("foobar"))
+                        {
+                            input.Write(tempfile.FullName, MagickFormat.Tiff);
+
+                            using (var output = new MagickImageCollection(tempfile.FullName))
+                            {
+                                Assert.Single(output);
+                                Assert.Equal(MagickFormat.Tiff, output[0].Format);
+                            }
+                        }
+                    }
+                }
+            }
+
             public class WithStream
             {
                 [Fact]
@@ -129,48 +171,6 @@ namespace Magick.NET.Tests
                         using (var memoryStream = new MemoryStream())
                         {
                             Assert.Throws<MagickMissingDelegateErrorException>(() => input.Write(memoryStream, MagickFormat.Xc));
-                        }
-                    }
-                }
-            }
-
-            public class WithFileName
-            {
-                [Fact]
-                public void ShouldThrowExceptionWhenFileIsNull()
-                {
-                    using (var images = new MagickImageCollection())
-                    {
-                        Assert.Throws<ArgumentNullException>("fileName", () => images.Write((string)null));
-                    }
-                }
-            }
-
-            public class WithFileNameAndMagickFormat
-            {
-                [Fact]
-                public void ShouldThrowExceptionWhenFileIsNull()
-                {
-                    using (var images = new MagickImageCollection())
-                    {
-                        Assert.Throws<ArgumentNullException>("fileName", () => images.Write((string)null, MagickFormat.Bmp));
-                    }
-                }
-
-                [Fact]
-                public void ShouldUseTheSpecifiedFormat()
-                {
-                    using (var input = new MagickImageCollection(Files.CirclePNG))
-                    {
-                        using (var tempfile = new TemporaryFile("foobar"))
-                        {
-                            input.Write(tempfile.FullName, MagickFormat.Tiff);
-
-                            using (var output = new MagickImageCollection(tempfile.FullName))
-                            {
-                                Assert.Single(output);
-                                Assert.Equal(MagickFormat.Tiff, output[0].Format);
-                            }
                         }
                     }
                 }
