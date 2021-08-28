95b28cdba3c938855caadbb5275e2a6f19e6ee9c
Dirk Lemstra <dirk@lemstra.org>
2021-08-28 22:36:37 +0200

    Refactored XmlHelperTests.

---
+++ tests/Magick.NET.Core.Tests/Helpers/XmlHelperTests/TheCreateElementMethod.cs
@@ -0,0 +1,37 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Xml;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Core.Tests
+{
+    public partial class XmlHelperTests
+    {
+        public class TheCreateElementMethod
+        {
+            [Fact]
+            public void ShouldAddXmlDocumentAsDocumentElement()
+            {
+                var doc = new XmlDocument();
+                var element = XmlHelper.CreateElement(doc, "test");
+
+                Assert.Equal(doc.DocumentElement, element);
+                Assert.Equal("test", element.Name);
+            }
+
+            [Fact]
+            public void ShouldAddXmlNodeAsChildElement()
+            {
+                var doc = new XmlDocument();
+                var root = XmlHelper.CreateElement(doc, "root");
+
+                var element = XmlHelper.CreateElement(root, "test");
+
+                Assert.Equal(root.FirstChild, element);
+                Assert.Equal("test", element.Name);
+            }
+        }
+    }
+}
---
+++ tests/Magick.NET.Core.Tests/Helpers/XmlHelperTests/TheSetAttributeMethod.cs
@@ -0,0 +1,42 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Xml;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Core.Tests
+{
+    public partial class XmlHelperTests
+    {
+        public class TheSetAttributeMethod
+        {
+            [Fact]
+            public void ShouldAddAttribute()
+            {
+                var doc = new XmlDocument();
+                var element = doc.CreateElement("test");
+
+                XmlHelper.SetAttribute(element, "attr", "val");
+
+                var attribute = element.Attributes["attr"];
+                Assert.NotNull(attribute);
+                Assert.Equal("val", attribute.Value);
+            }
+
+            [Fact]
+            public void ShouldChangeValueOfAttribute()
+            {
+                var doc = new XmlDocument();
+                var element = doc.CreateElement("test");
+
+                var attribute = doc.CreateAttribute("attr");
+                element.Attributes.Append(attribute);
+
+                XmlHelper.SetAttribute(element, "attr", 42);
+
+                Assert.Equal("42", attribute.Value);
+            }
+        }
+    }
+}
