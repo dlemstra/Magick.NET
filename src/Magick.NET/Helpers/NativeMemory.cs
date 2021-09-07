0fe8ac640bb1f0461c001a66189a3c97a522c844
Dirk Lemstra <dirk@lemstra.org>
2021-09-07 21:43:27 +0200

    Corrected the net20 implementation of NativeMemory.

--- src/Magick.NET/Helpers/NativeMemory.cs
+++ src/Magick.NET/Helpers/NativeMemory.cs
@@ -10,22 +10,22 @@ namespace ImageMagick.Helpers
 #if Q16
         public static void Copy(ushort* source, ushort* destination, long length)
         {
-            var size = sizeof(ushort) * length;
 #if NET20
-            while (size >= 4)
+            while (length >= 4)
             {
                 *(destination++) = *(source++);
                 *(destination++) = *(source++);
                 *(destination++) = *(source++);
                 *(destination++) = *(source++);
-                size -= 4;
+                length -= 4;
             }
 
-            while (size-- > 0)
+            while (length-- > 0)
             {
                 *(destination++) = *(source++);
             }
 #else
+            var size = sizeof(ushort) * length;
             Buffer.MemoryCopy(source, destination, size, size);
 #endif
         }
@@ -34,22 +34,22 @@ namespace ImageMagick.Helpers
 #if Q16HDRI
         public static void Copy(float* source, float* destination, long length)
         {
-            var size = sizeof(float) * length;
 #if NET20
-            while (size >= 4)
+            while (length >= 4)
             {
                 *(destination++) = *(source++);
                 *(destination++) = *(source++);
                 *(destination++) = *(source++);
                 *(destination++) = *(source++);
-                size -= 4;
+                length -= 4;
             }
 
-            while (size-- > 0)
+            while (length-- > 0)
             {
                 *(destination++) = *(source++);
             }
 #else
+            var size = sizeof(float) * length;
             Buffer.MemoryCopy(source, destination, size, size);
 #endif
         }
