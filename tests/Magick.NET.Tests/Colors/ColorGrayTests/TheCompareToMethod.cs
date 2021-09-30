ad046a9a38093acbddcaa5d4d302ff8004f74f68
Dirk Lemstra <dirk@lemstra.org>
2021-09-30 21:27:53 +0200

    Added extra tests for the Compare method.

---
+++ tests/Magick.NET.Tests/Colors/ColorGrayTests/TheCompareToMethod.cs
@@ -0,0 +1,48 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class ColorGrayTests
+    {
+        public class TheCompareToMethod
+        {
+            [Fact]
+            public void ShouldReturnTheCorrectValueWhenOtherIsNull()
+            {
+                var color = new ColorGray(1);
+
+                Assert.Equal(1, color.CompareTo(null));
+            }
+
+            [Fact]
+            public void ShouldReturnTheCorrectValueWhenOtherIsEqual()
+            {
+                var color = new ColorGray(1);
+
+                Assert.Equal(0, color.CompareTo(color));
+            }
+
+            [Fact]
+            public void ShouldReturnTheCorrectValueWhenOtherIsLower()
+            {
+                var color = new ColorGray(1.0);
+                var other = new ColorGray(0.5);
+
+                Assert.Equal(1, color.CompareTo(other));
+            }
+
+            [Fact]
+            public void ShouldReturnTheCorrectValueWhenOtherIsHigher()
+            {
+                var color = new ColorGray(0.25);
+                var other = new ColorGray(0.5);
+
+                Assert.Equal(-1, color.CompareTo(other));
+            }
+        }
+    }
+}
---
+++ tests/Magick.NET.Tests/Colors/ColorMonoTests/TheCompareToMethod.cs
@@ -0,0 +1,48 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class ColorMonoTests
+    {
+        public class TheCompareToMethod
+        {
+            [Fact]
+            public void ShouldReturnTheCorrectValueWhenOtherIsNull()
+            {
+                var color = new ColorMono(true);
+
+                Assert.Equal(1, color.CompareTo(null));
+            }
+
+            [Fact]
+            public void ShouldReturnTheCorrectValueWhenOtherIsEqual()
+            {
+                var color = new ColorMono(true);
+
+                Assert.Equal(0, color.CompareTo(color));
+            }
+
+            [Fact]
+            public void ShouldReturnTheCorrectValueWhenOtherIsLower()
+            {
+                var color = new ColorMono(false);
+                var other = new ColorMono(true);
+
+                Assert.Equal(1, color.CompareTo(other));
+            }
+
+            [Fact]
+            public void ShouldReturnTheCorrectValueWhenOtherIsHigher()
+            {
+                var color = new ColorMono(true);
+                var other = new ColorMono(false);
+
+                Assert.Equal(-1, color.CompareTo(other));
+            }
+        }
+    }
+}
