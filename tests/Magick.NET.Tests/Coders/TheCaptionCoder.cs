09467e53de03f50017b825c07fb9f8d07abed191
Dirk Lemstra <dirk@lemstra.org>
2021-10-04 19:08:55 +0200

    Added unit tests for issue 4305 of ImageMagick.

---
+++ tests/Magick.NET.Tests/Coders/TheCaptionCoder.cs
@@ -0,0 +1,68 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class TheCaptionCoder
+    {
+        [Fact]
+        public void ShouldNotReplaceNonBreakingSpaceWithNewline()
+        {
+            var settings = new MagickReadSettings
+            {
+                Font = Files.Fonts.Arial,
+                FontPointsize = 19,
+                Width = 160,
+            };
+
+            using (var image = new MagickImage("caption:Нуорунен в Карелии", settings))
+            {
+                image.Trim();
+
+                Assert.Equal(88, image.Width);
+                Assert.Equal(41, image.Height);
+            }
+        }
+
+        [Fact]
+        public void ShouldReplaceOghamSpaceMarkWithNewline()
+        {
+            var settings = new MagickReadSettings
+            {
+                Font = Files.Fonts.Arial,
+                FontPointsize = 19,
+                Width = 160,
+            };
+
+            using (var image = new MagickImage("caption:Нуорунен в Карелии", settings))
+            {
+                image.Trim();
+
+                Assert.Equal(99, image.Width);
+                Assert.Equal(41, image.Height);
+            }
+        }
+
+        [Fact]
+        public void ShouldAddNewlineWhenTextDoesNotContainNewline()
+        {
+            var settings = new MagickReadSettings
+            {
+                Font = Files.Fonts.Arial,
+                FontPointsize = 19,
+                Width = 160,
+            };
+
+            using (var image = new MagickImage("caption:НуоруненвКарелии", settings))
+            {
+                image.Trim();
+
+                Assert.Equal(157, image.Width);
+                Assert.Equal(37, image.Height);
+            }
+        }
+    }
+}
