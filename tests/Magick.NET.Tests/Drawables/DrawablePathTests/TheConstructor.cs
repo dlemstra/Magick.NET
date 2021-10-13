c44715ffa13241300a3271f0133a9a737fe6a7cb
Dirk Lemstra <dirk@lemstra.org>
2021-10-13 18:34:15 +0200

    Refactored DrawablePathTests.

---
+++ tests/Magick.NET.Tests/Drawables/DrawablePathTests/TheConstructor.cs
@@ -0,0 +1,21 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class DrawablePathTests
+    {
+        public class TheConstructor
+        {
+            [Fact]
+            public void ShouldSetPathsToEmptyCollection()
+            {
+                var path = new DrawablePath();
+                Assert.Empty(path.Paths);
+            }
+        }
+    }
+}
---
+++ tests/Magick.NET.Tests/Drawables/DrawablePathTests/TheDrawMethod.cs
@@ -0,0 +1,22 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class DrawablePathTests
+    {
+        public class TheDrawMethod
+        {
+            [Fact]
+            public void ShouldSupportNullArgument()
+            {
+                var path = new DrawablePath();
+
+                ((IDrawingWand)path).Draw(null);
+            }
+        }
+    }
+}
