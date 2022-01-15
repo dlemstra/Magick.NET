4ebe6bdf7399df7668c2dac183988d90764c9198
Dirk Lemstra <dirk@lemstra.org>
2022-01-15 16:12:03 +0100

    Also add OpenMP to the local arm64 test batch file.

--- build/windows/test.Magick.NET.arm64.cmd
+++ build/windows/test.Magick.NET.arm64.cmd
@@ -5,9 +5,17 @@ echo "src\Magick.Native\Install.cmd should be executed before running the tests"
 pause
 
 "c:\Program Files\dotnet\dotnet.exe" test -c TestQ8 /p:Platform=arm64 --framework net60 ..\..\tests\Magick.NET.Tests\Magick.NET.Tests.csproj
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 goto DONE
 "c:\Program Files\dotnet\dotnet.exe" test -c TestQ16 /p:Platform=arm64 --framework net60 ..\..\tests\Magick.NET.Tests\Magick.NET.Tests.csproj
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 goto DONE
 "c:\Program Files\dotnet\dotnet.exe" test -c TestQ16-HDRI /p:Platform=arm64 --framework net60 ..\..\tests\Magick.NET.Tests\Magick.NET.Tests.csproj
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 goto DONE
+"c:\Program Files\dotnet\dotnet.exe" test -c TestQ8-OpenMP /p:Platform=arm64 --framework net60 ..\..\tests\Magick.NET.Tests\Magick.NET.Tests.csproj
+if %errorlevel% neq 0 goto DONE
+"c:\Program Files\dotnet\dotnet.exe" test -c TestQ16-OpenMP /p:Platform=arm64 --framework net60 ..\..\tests\Magick.NET.Tests\Magick.NET.Tests.csproj
+if %errorlevel% neq 0 goto DONE
+"c:\Program Files\dotnet\dotnet.exe" test -c TestQ16-HDRI-OpenMP /p:Platform=arm64 --framework net60 ..\..\tests\Magick.NET.Tests\Magick.NET.Tests.csproj
+if %errorlevel% neq 0 goto DONE
+
+:DONE
 pause
