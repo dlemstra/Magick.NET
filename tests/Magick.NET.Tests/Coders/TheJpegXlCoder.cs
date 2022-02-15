7b86547fdeb1c6de79ba99c038f5cd4d22bb6e8e
Dirk Lemstra <dirk@lemstra.org>
2022-02-15 21:55:28 +0100

    Added unit test to check if a jpeg-xl image can be written.

---
+++ tests/Magick.NET.Tests/Coders/TheJpegXlCoder.cs
@@ -0,0 +1,26 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System.IO;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public class TheJpegXlCoder
+    {
+        [Fact]
+        public void ShouldWriteCorrectOutputImage()
+        {
+            using (var image = new MagickImage(Files.Builtin.Logo))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    image.Write(memoryStream, MagickFormat.Jxl);
+
+                    Assert.InRange(memoryStream.Length, 20000, 40000);
+                }
+            }
+        }
+    }
+}
