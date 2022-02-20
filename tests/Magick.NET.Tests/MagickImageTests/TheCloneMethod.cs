2f1cc11932d148059b94424c02e85f412ed3a2d5
Dirk Lemstra <dirk@lemstra.org>
2022-02-20 21:51:38 +0100

    Added unit test for the Clone method to make sure the issue reported in #1140 stays fixed.

---
+++ tests/Magick.NET.Tests/MagickImageTests/TheCloneMethod.cs
@@ -0,0 +1,34 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        public class TheCloneMethod
+        {
+            [Fact]
+            public void ShouldThrowExceptionWhenNoImageIsRead()
+            {
+                using (var image = new MagickImage())
+                {
+                    Assert.Throws<MagickCorruptImageErrorException>(() => image.Clone());
+                }
+            }
+
+            [Fact]
+            public void ShouldCloneTheImage()
+            {
+                using (var image = new MagickImage(Files.Builtin.Logo))
+                {
+                    var clone = image.Clone();
+
+                    Assert.False(ReferenceEquals(image, clone));
+                }
+            }
+        }
+    }
+}
