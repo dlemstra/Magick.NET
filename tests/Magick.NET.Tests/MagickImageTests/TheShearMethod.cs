b26665dce0d103e0166ecec6143137c0fdcf4042
Dirk Lemstra <dirk@lemstra.org>
2022-04-06 08:33:30 +0200

    Moved unit test to separate file.

---
+++ tests/Magick.NET.Tests/MagickImageTests/TheShearMethod.cs
@@ -0,0 +1,33 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        public class TheShearMethod
+        {
+            [Fact]
+            public void ShouldShearTheImage()
+            {
+                using (var image = new MagickImage(Files.TestPNG))
+                {
+                    image.BackgroundColor = MagickColors.Firebrick;
+                    image.VirtualPixelMethod = VirtualPixelMethod.Background;
+                    image.Shear(20, 40);
+
+                    Assert.Equal(186, image.Width);
+                    Assert.Equal(195, image.Height);
+
+                    ColorAssert.Equal(MagickColors.Red, image, 14, 68);
+                    ColorAssert.Equal(MagickColors.Firebrick, image, 45, 6);
+                    ColorAssert.Equal(MagickColors.Blue, image, 150, 171);
+                    ColorAssert.Equal(MagickColors.Firebrick, image, 158, 181);
+                }
+            }
+        }
+    }
+}
