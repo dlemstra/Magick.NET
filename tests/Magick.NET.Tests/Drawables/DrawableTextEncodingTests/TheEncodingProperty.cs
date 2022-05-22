c286ad4f5a4cd3b79a76beec7627fde086e8db25
Dirk Lemstra <dirk@lemstra.org>
2022-05-22 15:30:08 +0200

    Refactor unit tests.

---
+++ tests/Magick.NET.Tests/Drawables/DrawableTextEncodingTests/TheEncodingProperty.cs
@@ -0,0 +1,25 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Text;
+using ImageMagick;
+
+namespace Magick.NET.Tests
+{
+    public partial class DrawableTextEncodingTests
+    {
+        public class TheEncodingProperty
+        {
+            public void ShouldAllowNullValue()
+            {
+                var encoding = new DrawableTextEncoding(Encoding.UTF8);
+                encoding.Encoding = null;
+
+                using (var image = new MagickImage(MagickColors.Firebrick, 10, 10))
+                {
+                    image.Draw(encoding);
+                }
+            }
+        }
+    }
+}
