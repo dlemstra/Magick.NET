a3bbe22c0465dbc9ebbdf5ebec55b6ac980d40d3
Dirk Lemstra <dirk@lemstra.org>
2022-05-29 19:07:21 +0200

    Use shared script for the linux build.

--- build/shared/install.Magick.Native.sh
+++ build/shared/install.Magick.Native.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+github_username=$1
+github_token=$2
+config=$3
+arch=$4
+
 cd src/Magick.Native
-./create-nuget-config.sh $1 $2
-./install.sh linux x64
+./create-nuget-config.sh $github_username $github_token
+./install.sh $config $arch
