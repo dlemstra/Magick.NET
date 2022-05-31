df1d653f05b25cdbedf18669a3d967e8be41e3f2
Dirk Lemstra <dirk@lemstra.org>
2022-05-31 21:26:45 +0200

    Execute OpenMP tests for linux-arm64.

--- build/linux-arm64/github-actions.sh
+++ build/linux-arm64/github-actions.sh
@@ -5,3 +5,5 @@ cd /Magick.NET
 build/linux-arm64/install.dependencies.sh
 build/shared/build.Magick.NET.sh
 build/shared/test.Magick.NET.sh
+build/shared/build.Magick.NET.sh OpenMP
+build/shared/test.Magick.NET.sh OpenMP
