44e8dcab1f7253d9556db5691b70647ada22a5dc
Dirk Lemstra <dirk@lemstra.org>
2022-07-02 11:44:40 +0200

    Added unit tests for the Strip method.

---
+++ tests/Magick.NET.Tests/MagickImageTests/TheStripMethod.cs
@@ -0,0 +1,48 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        public class TheStripMethod
+        {
+            [Fact]
+            public void ShouldRemovePropertiesFromImage()
+            {
+                using (var image = new MagickImage(Files.Builtin.Logo))
+                {
+                    image.Comment = "FooBar";
+
+                    Assert.NotNull(image.Comment);
+                    Assert.NotNull(image.GetAttribute("date:create"));
+                    Assert.NotNull(image.GetAttribute("date:modify"));
+                    Assert.NotNull(image.GetAttribute("date:timestamp"));
+
+                    image.Strip();
+
+                    Assert.Null(image.Comment);
+                    Assert.Null(image.GetAttribute("date:create"));
+                    Assert.Null(image.GetAttribute("date:modify"));
+                    Assert.Null(image.GetAttribute("date:timestamp"));
+                }
+            }
+
+            [Fact]
+            public void ShouldRemoveProfilesFromImage()
+            {
+                using (var image = new MagickImage(Files.PictureJPG))
+                {
+                    Assert.NotNull(image.GetColorProfile());
+
+                    image.Strip();
+
+                    Assert.Null(image.GetColorProfile());
+                }
+            }
+        }
+    }
+}
