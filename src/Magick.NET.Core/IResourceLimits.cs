c0a2c4b917043cbef61560f39622be4cb535f83a
Dirk Lemstra <dirk@lemstra.org>
2022-07-09 11:02:31 +0200

    Added IResourceLimits interface that can be used to inject the ResourceLimits class.

---
+++ src/Magick.NET.Core/IResourceLimits.cs
@@ -0,0 +1,66 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+namespace ImageMagick
+{
+    /// <summary>
+    /// Interface that represents the resource limits of ImageMagick.
+    /// </summary>
+    public interface IResourceLimits
+    {
+        /// <summary>
+        /// Gets or sets the maximum width * height of an image that can reside in the pixel cache memory.
+        /// Images that exceed the area limit are cached to disk.
+        /// </summary>
+        ulong Area { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pixel cache limit in bytes. Requests for memory above this limit will fail.
+        /// </summary>
+        ulong Disk { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum height of an image.
+        /// </summary>
+        ulong Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum number of images in an image list.
+        /// </summary>
+        ulong ListLength { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max memory request in bytes. ImageMagick maintains a separate memory pool for large
+        /// resource requests. If the limit is exceeded, the allocation is instead memory-mapped on disk.
+        /// </summary>
+        ulong MaxMemoryRequest { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pixel cache limit in bytes. Once this memory limit is exceeded, all subsequent pixels cache
+        /// operations are to/from disk.
+        /// </summary>
+        ulong Memory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of threads used in multithreaded operations.
+        /// </summary>
+        ulong Thread { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time specified in milliseconds to periodically yield the CPU for.
+        /// </summary>
+        ulong Throttle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum width of an image.
+        /// </summary>
+        ulong Width { get; set; }
+
+        /// <summary>
+        /// Set the maximum percentage of memory that can be used for image data. This also changes
+        /// the <see cref="Area"/> limit to twice the number of bytes.
+        /// </summary>
+        /// <param name="percentage">The percentage to use.</param>
+        void LimitMemory(Percentage percentage);
+    }
+}
--- src/Magick.NET/ResourceLimits.cs
+++ src/Magick.NET/ResourceLimits.cs
@@ -6,7 +6,7 @@ namespace ImageMagick
     /// <summary>
     /// Class that can be used to set the limits to the resources that are being used.
     /// </summary>
-    public static partial class ResourceLimits
+    public partial class ResourceLimits : IResourceLimits
     {
         /// <summary>
         /// Gets or sets the maximum width * height of an image that can reside in the pixel cache memory.
@@ -46,16 +46,6 @@ namespace ImageMagick
         }
 
         /// <summary>
-        /// Gets or sets the pixel cache limit in bytes. Once this memory limit is exceeded, all subsequent pixels cache
-        /// operations are to/from disk.
-        /// </summary>
-        public static ulong Memory
-        {
-            get => NativeResourceLimits.Memory;
-            set => NativeResourceLimits.Memory = value;
-        }
-
-        /// <summary>
         /// Gets or sets the max memory request in bytes. ImageMagick maintains a separate memory pool for large
         /// resource requests. If the limit is exceeded, the allocation is instead memory-mapped on disk.
         /// </summary>
@@ -63,6 +53,16 @@ namespace ImageMagick
         {
             get => NativeResourceLimits.MaxMemoryRequest;
             set => NativeResourceLimits.MaxMemoryRequest = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the pixel cache limit in bytes. Once this memory limit is exceeded, all subsequent pixels cache
+        /// operations are to/from disk.
+        /// </summary>
+        public static ulong Memory
+        {
+            get => NativeResourceLimits.Memory;
+            set => NativeResourceLimits.Memory = value;
         }
 
         /// <summary>
@@ -93,6 +93,90 @@ namespace ImageMagick
         }
 
         /// <summary>
+        /// Gets or sets the maximum width * height of an image that can reside in the pixel cache memory.
+        /// Images that exceed the area limit are cached to disk.
+        /// </summary>
+        ulong IResourceLimits.Area
+        {
+            get => Area;
+            set => Area = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the pixel cache limit in bytes. Requests for memory above this limit will fail.
+        /// </summary>
+        ulong IResourceLimits.Disk
+        {
+            get => Disk;
+            set => Disk = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum height of an image.
+        /// </summary>
+        ulong IResourceLimits.Height
+        {
+            get => Height;
+            set => Height = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum number of images in an image list.
+        /// </summary>
+        ulong IResourceLimits.ListLength
+        {
+            get => ListLength;
+            set => ListLength = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the max memory request in bytes. ImageMagick maintains a separate memory pool for large
+        /// resource requests. If the limit is exceeded, the allocation is instead memory-mapped on disk.
+        /// </summary>
+        ulong IResourceLimits.MaxMemoryRequest
+        {
+            get => MaxMemoryRequest;
+            set => MaxMemoryRequest = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the pixel cache limit in bytes. Once this memory limit is exceeded, all subsequent pixels cache
+        /// operations are to/from disk.
+        /// </summary>
+        ulong IResourceLimits.Memory
+        {
+            get => Memory;
+            set => Memory = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the number of threads used in multithreaded operations.
+        /// </summary>
+        ulong IResourceLimits.Thread
+        {
+            get => Thread;
+            set => Thread = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the time specified in milliseconds to periodically yield the CPU for.
+        /// </summary>
+        ulong IResourceLimits.Throttle
+        {
+            get => Throttle;
+            set => Throttle = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum width of an image.
+        /// </summary>
+        ulong IResourceLimits.Width
+        {
+            get => Width;
+            set => Width = value;
+        }
+
+        /// <summary>
         /// Set the maximum percentage of memory that can be used for image data. This also changes
         /// the <see cref="Area"/> limit to twice the number of bytes.
         /// </summary>
@@ -103,5 +187,13 @@ namespace ImageMagick
 
             NativeResourceLimits.LimitMemory((double)percentage / 100.0);
         }
+
+        /// <summary>
+        /// Set the maximum percentage of memory that can be used for image data. This also changes
+        /// the <see cref="Area"/> limit to twice the number of bytes.
+        /// </summary>
+        /// <param name="percentage">The percentage to use.</param>
+        void IResourceLimits.LimitMemory(Percentage percentage)
+            => LimitMemory(percentage);
     }
 }
