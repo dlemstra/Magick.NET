a5843aaf52223c5a863d010138b6108df5da23b8
Dirk Lemstra <dirk@lemstra.org>
2022-07-17 11:06:22 +0200

    Moved unit tests for Convolve to a separate file.

--- tests/Magick.NET.Tests/MagickImageTests.cs
+++ tests/Magick.NET.Tests/MagickImageTests.cs
@@ -23,33 +23,6 @@ namespace Magick.NET.Tests
     public partial class MagickImageTests
     {
         [Fact]
-        public void Test_Convolve()
-        {
-            using (var image = new MagickImage("xc:", 1, 1))
-            {
-                image.BorderColor = MagickColors.Black;
-                image.Border(5);
-
-                Assert.Equal(11, image.Width);
-                Assert.Equal(11, image.Height);
-
-                var matrix = new ConvolveMatrix(3, 0, 0.5, 0, 0.5, 1, 0.5, 0, 0.5, 0);
-                image.Convolve(matrix);
-
-                MagickColor gray = new MagickColor("#800080008000");
-                ColorAssert.Equal(MagickColors.Black, image, 4, 4);
-                ColorAssert.Equal(gray, image, 5, 4);
-                ColorAssert.Equal(MagickColors.Black, image, 6, 4);
-                ColorAssert.Equal(gray, image, 4, 5);
-                ColorAssert.Equal(MagickColors.White, image, 5, 5);
-                ColorAssert.Equal(gray, image, 6, 5);
-                ColorAssert.Equal(MagickColors.Black, image, 4, 6);
-                ColorAssert.Equal(gray, image, 5, 6);
-                ColorAssert.Equal(MagickColors.Black, image, 6, 6);
-            }
-        }
-
-        [Fact]
         public void Test_CopyPixels()
         {
             using (var source = new MagickImage(MagickColors.White, 100, 100))
---
+++ tests/Magick.NET.Tests/MagickImageTests/TheConvolveMethod.cs
@@ -0,0 +1,55 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        public class TheConvolveMethod
+        {
+
+            [Fact]
+            public void ShouldThrowExceptionWhenMatrixIsNull()
+            {
+                using (var image = new MagickImage())
+                {
+                    Assert.Throws<ArgumentException>("matrix", () =>
+                    {
+                        image.Convolve(null);
+                    });
+                }
+            }
+
+            [Fact]
+            public void ShouldApplyTheSpecifiedMatrix()
+            {
+                using (var image = new MagickImage("xc:", 1, 1))
+                {
+                    image.BorderColor = MagickColors.Black;
+                    image.Border(5);
+
+                    Assert.Equal(11, image.Width);
+                    Assert.Equal(11, image.Height);
+
+                    var matrix = new ConvolveMatrix(3, 0, 0.5, 0, 0.5, 1, 0.5, 0, 0.5, 0);
+                    image.Convolve(matrix);
+
+                    var gray = new MagickColor("#800080008000");
+                    ColorAssert.Equal(MagickColors.Black, image, 4, 4);
+                    ColorAssert.Equal(gray, image, 5, 4);
+                    ColorAssert.Equal(MagickColors.Black, image, 6, 4);
+                    ColorAssert.Equal(gray, image, 4, 5);
+                    ColorAssert.Equal(MagickColors.White, image, 5, 5);
+                    ColorAssert.Equal(gray, image, 6, 5);
+                    ColorAssert.Equal(MagickColors.Black, image, 4, 6);
+                    ColorAssert.Equal(gray, image, 5, 6);
+                    ColorAssert.Equal(MagickColors.Black, image, 6, 6);
+                }
+            }
+        }
+    }
+}
