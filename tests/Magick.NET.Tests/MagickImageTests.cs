72bc48bb56993fa903ce5d01c8b5b01ead97dd40
Dirk Lemstra <dirk@lemstra.org>
2022-07-22 10:28:59 +0200

    Moved unit tests for SepiaTone to a separate file.

--- tests/Magick.NET.Tests/MagickImageTests.cs
+++ tests/Magick.NET.Tests/MagickImageTests.cs
@@ -37,29 +37,6 @@ namespace Magick.NET.Tests
         }
 
         [Fact]
-        public void Test_SepiaTone()
-        {
-            using (var image = new MagickImage(Files.Builtin.Logo))
-            {
-                image.SepiaTone();
-
-#if Q8
-                ColorAssert.Equal(new MagickColor("#472400"), image, 243, 45);
-                ColorAssert.Equal(new MagickColor("#522e00"), image, 394, 394);
-                ColorAssert.Equal(new MagickColor("#e4bb7c"), image, 477, 373);
-#elif Q16
-                ColorAssert.Equal(new MagickColor(OpenCLValue.Get("#45be23e80000", "#475f24bf0000")), image, 243, 45);
-                ColorAssert.Equal(new MagickColor(OpenCLValue.Get("#50852d680000", "#52672e770000")), image, 394, 394);
-                ColorAssert.Equal(new MagickColor(OpenCLValue.Get("#e273b8c17a35", "#e5adbb627bf2")), image, 477, 373);
-#else
-                ColorAssert.Equal(new MagickColor(OpenCLValue.Get("#45be23e90001", "#475f24bf0000")), image, 243, 45);
-                ColorAssert.Equal(new MagickColor(OpenCLValue.Get("#50862d690001", "#52672e770000")), image, 394, 394);
-                ColorAssert.Equal(new MagickColor(OpenCLValue.Get("#e274b8c17a35", "#e5adbb627bf2")), image, 477, 373);
-#endif
-            }
-        }
-
-        [Fact]
         public void Test_SetAttenuate()
         {
             using (var image = new MagickImage(Files.Builtin.Logo))
