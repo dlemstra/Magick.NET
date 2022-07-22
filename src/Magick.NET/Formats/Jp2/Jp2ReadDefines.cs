d088175bf0bef0babeb1f80ac1d516e40f547162
Dirk Lemstra <dirk@lemstra.org>
2022-07-22 11:45:47 +0200

    Changed Jp2ReadDefines to no longer use the ReadDefinesCreator class.

--- src/Magick.NET/Formats/Jp2/Jp2ReadDefines.cs
+++ src/Magick.NET/Formats/Jp2/Jp2ReadDefines.cs
@@ -2,22 +2,26 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System.Collections.Generic;
-using ImageMagick.Defines;
 
 namespace ImageMagick.Formats
 {
     /// <summary>
     /// Class for defines that are used when a <see cref="MagickFormat.Jp2"/> image is read.
     /// </summary>
-    public sealed class Jp2ReadDefines : ReadDefinesCreator
+    public sealed class Jp2ReadDefines : IReadDefines
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Jp2ReadDefines"/> class.
         /// </summary>
         public Jp2ReadDefines()
-          : base(MagickFormat.Jp2)
         {
         }
+
+        /// <summary>
+        /// Gets the format where the defines are for.
+        /// </summary>
+        public MagickFormat Format
+            => MagickFormat.Jp2;
 
         /// <summary>
         /// Gets or sets the maximum number of quality layers to decode (jp2:quality-layers).
@@ -32,15 +36,15 @@ namespace ImageMagick.Formats
         /// <summary>
         /// Gets the defines that should be set as a define on an image.
         /// </summary>
-        public override IEnumerable<IDefine> Defines
+        public IEnumerable<IDefine> Defines
         {
             get
             {
                 if (QualityLayers.HasValue)
-                    yield return CreateDefine("quality-layers", QualityLayers.Value);
+                    yield return new MagickDefine(Format, "quality-layers", QualityLayers.Value);
 
                 if (ReduceFactor.HasValue)
-                    yield return CreateDefine("reduce-factor", ReduceFactor.Value);
+                    yield return new MagickDefine(Format, "reduce-factor", ReduceFactor.Value);
             }
         }
     }
