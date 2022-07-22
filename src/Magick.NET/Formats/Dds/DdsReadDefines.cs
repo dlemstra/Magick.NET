87663bc1aab1a2b421c0d7572520db263810cd6b
Dirk Lemstra <dirk@lemstra.org>
2022-07-22 11:42:52 +0200

    Changed DdsReadDefines to no longer use the ReadDefinesCreator class.

--- src/Magick.NET/Formats/Dds/DdsReadDefines.cs
+++ src/Magick.NET/Formats/Dds/DdsReadDefines.cs
@@ -2,22 +2,26 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System.Collections.Generic;
-using ImageMagick.Defines;
 
 namespace ImageMagick.Formats
 {
     /// <summary>
     /// Class for defines that are used when a <see cref="MagickFormat.Dds"/> image is read.
     /// </summary>
-    public sealed class DdsReadDefines : ReadDefinesCreator
+    public sealed class DdsReadDefines : IReadDefines
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="DdsReadDefines"/> class.
         /// </summary>
         public DdsReadDefines()
-          : base(MagickFormat.Dds)
         {
         }
+
+        /// <summary>
+        /// Gets the format where the defines are for.
+        /// </summary>
+        public MagickFormat Format
+            => MagickFormat.Dds;
 
         /// <summary>
         /// Gets or sets a value indicating wether mipmaps should be skipped (dds:skip-mipmaps).
@@ -27,12 +31,12 @@ namespace ImageMagick.Formats
         /// <summary>
         /// Gets the defines that should be set as a define on an image.
         /// </summary>
-        public override IEnumerable<IDefine> Defines
+        public IEnumerable<IDefine> Defines
         {
             get
             {
                 if (SkipMipmaps.HasValue)
-                    yield return CreateDefine("skip-mipmaps", SkipMipmaps.Value);
+                    yield return new MagickDefine(Format, "skip-mipmaps", SkipMipmaps.Value);
             }
         }
     }
