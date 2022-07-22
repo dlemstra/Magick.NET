ee2805b83e643ead4691782dcee6ba49554eb4d6
Dirk Lemstra <dirk@lemstra.org>
2022-07-22 12:10:24 +0200

    Changed JxlWriteDefines to no longer use the WriteDefinesCreator class.

--- src/Magick.NET/Formats/Jxl/JxlWriteDefines.cs
+++ src/Magick.NET/Formats/Jxl/JxlWriteDefines.cs
@@ -2,37 +2,34 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System.Collections.Generic;
-using ImageMagick.Defines;
 
 namespace ImageMagick.Formats
 {
     /// <summary>
     /// Class for defines that are used when a <see cref="MagickFormat.Jxl"/> image is written.
     /// </summary>
-    public sealed class JxlWriteDefines : WriteDefinesCreator
+    public sealed class JxlWriteDefines : IWriteDefines
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="JxlWriteDefines"/> class.
-        /// </summary>
-        public JxlWriteDefines()
-          : base(MagickFormat.Jxl)
-        {
-        }
-
         /// <summary>
         /// Gets or sets the jpeg-xl encoding effort. Valid values are in the range of 3 (falcon) to 9 (tortois) (jxl:effort).
         /// </summary>
         public int? Effort { get; set; }
 
         /// <summary>
+        /// Gets the format where the defines are for.
+        /// </summary>
+        public MagickFormat Format
+            => MagickFormat.Jxl;
+
+        /// <summary>
         /// Gets the defines that should be set as a define on an image.
         /// </summary>
-        public override IEnumerable<IDefine> Defines
+        public IEnumerable<IDefine> Defines
         {
             get
             {
                 if (Effort.HasValue)
-                    yield return CreateDefine("effort", Effort.Value);
+                    yield return new MagickDefine(Format, "effort", Effort.Value);
             }
         }
     }
