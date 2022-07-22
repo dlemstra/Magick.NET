5731402dbbd6c3e4d8d17123d7baf59d57f0fbb5
Dirk Lemstra <dirk@lemstra.org>
2022-07-22 11:44:57 +0200

    Changed HeicReadDefines to no longer use the ReadDefinesCreator class.

--- src/Magick.NET/Formats/Heic/HeicReadDefines.cs
+++ src/Magick.NET/Formats/Heic/HeicReadDefines.cs
@@ -2,22 +2,26 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System.Collections.Generic;
-using ImageMagick.Defines;
 
 namespace ImageMagick.Formats
 {
     /// <summary>
     /// Class for defines that are used when a <see cref="MagickFormat.Heic"/> image is read.
     /// </summary>
-    public sealed class HeicReadDefines : ReadDefinesCreator
+    public sealed class HeicReadDefines : IReadDefines
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="HeicReadDefines"/> class.
         /// </summary>
         public HeicReadDefines()
-          : base(MagickFormat.Heic)
         {
         }
+
+        /// <summary>
+        /// Gets the format where the defines are for.
+        /// </summary>
+        public MagickFormat Format
+            => MagickFormat.Heic;
 
         /// <summary>
         /// Gets or sets a value indicating whether the depth image should be read (heic:depth-image).
@@ -32,15 +36,15 @@ namespace ImageMagick.Formats
         /// <summary>
         /// Gets the defines that should be set as a define on an image.
         /// </summary>
-        public override IEnumerable<IDefine> Defines
+        public IEnumerable<IDefine> Defines
         {
             get
             {
                 if (DepthImage == true)
-                    yield return CreateDefine("depth-image", DepthImage.Value);
+                    yield return new MagickDefine(Format, "depth-image", DepthImage.Value);
 
                 if (PreserveOrientation == true)
-                    yield return CreateDefine("preserve-orientation", PreserveOrientation.Value);
+                    yield return new MagickDefine(Format, "preserve-orientation", PreserveOrientation.Value);
             }
         }
     }
