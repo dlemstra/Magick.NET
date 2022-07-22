0e3acf0972af8b3ddc89bac39fcb1b262eb13aec
Dirk Lemstra <dirk@lemstra.org>
2022-07-22 12:13:05 +0200

    Changed PsdWriteDefines to no longer use the WriteDefinesCreator class.

--- src/Magick.NET/Formats/Psd/PsdWriteDefines.cs
+++ src/Magick.NET/Formats/Psd/PsdWriteDefines.cs
@@ -2,36 +2,33 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System.Collections.Generic;
-using ImageMagick.Defines;
 
 namespace ImageMagick.Formats
 {
     /// <summary>
     /// Class for defines that are used when a <see cref="MagickFormat.Psd"/> image is written.
     /// </summary>
-    public sealed class PsdWriteDefines : WriteDefinesCreator
+    public sealed class PsdWriteDefines : IWriteDefines
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PsdWriteDefines"/> class.
-        /// </summary>
-        public PsdWriteDefines()
-          : base(MagickFormat.Psd)
-        {
-        }
-
         /// <summary>
         /// Gets or sets which additional info should be written to the output file.
         /// </summary>
         public PsdAdditionalInfoPart AdditionalInfo { get; set; }
 
         /// <summary>
+        /// Gets the format where the defines are for.
+        /// </summary>
+        public MagickFormat Format
+            => MagickFormat.Psd;
+
+        /// <summary>
         /// Gets the defines that should be set as a define on an image.
         /// </summary>
-        public override IEnumerable<IDefine> Defines
+        public IEnumerable<IDefine> Defines
         {
             get
             {
-                yield return CreateDefine("additional-info", AdditionalInfo);
+                yield return new MagickDefine(Format, "additional-info", AdditionalInfo);
             }
         }
     }
