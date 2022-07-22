2f5e34f2fced27d16b99c86ab21c8165bfd361d2
Dirk Lemstra <dirk@lemstra.org>
2022-07-22 08:54:08 +0200

    Added extra unit test.

--- tests/Magick.NET.Tests/MagickImageTests/ThePerceptibleMethod.cs
+++ tests/Magick.NET.Tests/MagickImageTests/ThePerceptibleMethod.cs
@@ -39,6 +39,18 @@ namespace Magick.NET.Tests
             {
                 using (var image = new MagickImage(Files.Builtin.Logo))
                 {
+                    image.Perceptible(Quantum.Max * 0.4, Channels.Green);
+
+                    ColorAssert.Equal(new MagickColor("#f79868"), image, 300, 210);
+                    ColorAssert.Equal(new MagickColor("#666692"), image, 410, 405);
+                }
+            }
+
+            [Fact]
+            public void ShouldChangeTheSpecifiedChannelsWithTrueColorAlphaImage()
+            {
+                using (var image = new MagickImage(Files.Builtin.Logo))
+                {
                     image.ColorType = ColorType.TrueColorAlpha;
 
                     image.Perceptible(Quantum.Max * 0.4, Channels.Green);
