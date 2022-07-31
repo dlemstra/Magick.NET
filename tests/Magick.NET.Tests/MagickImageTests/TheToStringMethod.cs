bf1eab0e59062781249e582a47a812267e93278e
Dirk Lemstra <dirk@lemstra.org>
2022-07-31 10:11:08 +0200

    Moved unit tests for ToString to a separate file.

---
+++ tests/Magick.NET.Tests/MagickImageTests/TheToStringMethod.cs
@@ -0,0 +1,28 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        public class TheToStringMethod
+        {
+            [Fact]
+            public void ShouldReturnTheStringRepresentationOfTheImage()
+            {
+                using (var image = new MagickImage(Files.Builtin.Wizard))
+                {
+                    Assert.Equal("Gif 480x640 8-bit sRGB", image.ToString());
+                }
+
+                using (var image = new MagickImage(Files.TestPNG))
+                {
+                    Assert.Equal("Png 150x100 16-bit sRGB", image.ToString());
+                }
+            }
+        }
+    }
+}
