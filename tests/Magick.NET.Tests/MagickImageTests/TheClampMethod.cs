db845d7de9fd9e44db51758cc62bc9a6b9827ae0
Dirk Lemstra <dirk@lemstra.org>
2022-09-19 18:38:10 +0200

    Added unit tests for the clamp method.

---
+++ tests/Magick.NET.Tests/MagickImageTests/TheClampMethod.cs
@@ -0,0 +1,57 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        public class TheClampMethod
+        {
+            [Fact]
+            public void ShouldClampThePixelsToTheQuantumRange()
+            {
+#if Q16HDRI
+                using (var image = new MagickImage("xc:cmyka(200%, 200%, 200%, 200%, 2.0)", 1, 1))
+                {
+                    image.Clamp();
+
+                    using (var pixels = image.GetPixelsUnsafe())
+                    {
+                        var pixel = pixels.GetPixel(0, 0);
+                        Assert.Equal(Quantum.Max, pixel.GetChannel(image.ChannelOffset(PixelChannel.Cyan)));
+                        Assert.Equal(Quantum.Max, pixel.GetChannel(image.ChannelOffset(PixelChannel.Magenta)));
+                        Assert.Equal(Quantum.Max, pixel.GetChannel(image.ChannelOffset(PixelChannel.Yellow)));
+                        Assert.Equal(Quantum.Max, pixel.GetChannel(image.ChannelOffset(PixelChannel.Black)));
+                        Assert.Equal(Quantum.Max, pixel.GetChannel(image.ChannelOffset(PixelChannel.Alpha)));
+                    }
+                }
+#endif
+            }
+
+            [Fact]
+            public void ShouldClampThePixelsToTheQuantumRangeForTheSpecifiedChannel()
+            {
+#if Q16HDRI
+                var value = Quantum.Max * 2;
+                using (var image = new MagickImage("xc:cmyka(200%, 200%, 200%, 200%, 2.0)", 1, 1))
+                {
+                    image.Clamp(Channels.Black | Channels.Yellow);
+
+                    using (var pixels = image.GetPixelsUnsafe())
+                    {
+                        var pixel = pixels.GetPixel(0, 0);
+                        Assert.Equal(value, pixel.GetChannel(image.ChannelOffset(PixelChannel.Cyan)));
+                        Assert.Equal(value, pixel.GetChannel(image.ChannelOffset(PixelChannel.Magenta)));
+                        Assert.Equal(Quantum.Max, pixel.GetChannel(image.ChannelOffset(PixelChannel.Yellow)));
+                        Assert.Equal(Quantum.Max, pixel.GetChannel(image.ChannelOffset(PixelChannel.Black)));
+                        Assert.Equal(value, pixel.GetChannel(image.ChannelOffset(PixelChannel.Alpha)));
+                    }
+                }
+#endif
+            }
+        }
+    }
+}
