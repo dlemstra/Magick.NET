cb9e825b3f26fa45d75b98811cead2824209daf6
Dirk Lemstra <dirk@lemstra.org>
2023-01-20 10:37:38 +0100

    Moved unit test for WhiteThreshold to a separate file.

---
+++ tests/Magick.NET.Tests/MagickImageTests/TheWhiteThresholdMethod.cs
@@ -0,0 +1,25 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public partial class MagickImageTests
+    {
+        public class TheWhiteThresholdMethod
+        {
+            [Fact]
+            public void ShouldMakePixelsAboveThresholdWhite()
+            {
+                using (var image = new MagickImage(Files.MagickNETIconPNG))
+                {
+                    image.WhiteThreshold(new Percentage(10));
+                    ColorAssert.Equal(MagickColors.White, image, 43, 74);
+                    ColorAssert.Equal(MagickColors.White, image, 60, 74);
+                }
+            }
+        }
+    }
+}
