c5bc4e7dfea2fb9684c9bcdef7bfa84996a62a55
Dirk Lemstra <dirk@lemstra.org>
2023-03-09 08:14:03 +0100

    The macOS build now supports more delegates.

--- tests/Magick.NET.Tests/MagickNETTests/TheDelegatesProperty.cs
+++ tests/Magick.NET.Tests/MagickNETTests/TheDelegatesProperty.cs
@@ -16,10 +16,8 @@ namespace Magick.NET.Tests
 
                 if (OperatingSystem.IsWindows)
                     Assert.Equal("bzlib cairo freetype gslib heic jng jp2 jpeg jxl lcms lqr lzma openexr pangocairo png ps raqm raw rsvg tiff webp xml zip zlib", delegates);
-                else if (OperatingSystem.IsLinux)
-                    Assert.Equal("bzlib cairo fontconfig freetype heic jng jp2 jpeg jxl lcms lqr lzma openexr pangocairo png raqm raw rsvg tiff webp xml zip zlib", delegates);
                 else
-                    Assert.Equal("cairo fontconfig freetype heic jng jp2 jpeg lcms lqr openexr pangocairo png raqm raw rsvg tiff webp xml zlib", delegates);
+                    Assert.Equal("bzlib cairo fontconfig freetype heic jng jp2 jpeg jxl lcms lqr lzma openexr pangocairo png raqm raw rsvg tiff webp xml zip zlib", delegates);
             }
         }
     }
