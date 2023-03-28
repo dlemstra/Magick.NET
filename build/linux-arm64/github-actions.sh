3ac1c856a0eb21fec0b0ef4879fbb9581e49c925
Dirk Lemstra <dirk@lemstra.org>
2023-03-28 17:35:57 +0200

    Revert building outside the qemu container.

--- build/linux-arm64/github-actions.sh
+++ build/linux-arm64/github-actions.sh
@@ -3,5 +3,7 @@ set -e
 
 cd /Magick.NET
 build/linux-arm64/install.dependencies.sh
+build/shared/build.Magick.NET.sh
 build/shared/test.Magick.NET.sh
+build/shared/build.Magick.NET.sh OpenMP
 build/shared/test.Magick.NET.sh OpenMP
