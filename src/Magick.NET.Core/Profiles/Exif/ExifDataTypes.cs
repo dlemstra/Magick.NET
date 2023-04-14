a816b62c1c1a81f1d11246e7bc10f30224d8d892
Dirk Lemstra <dirk@lemstra.org>
2023-04-14 12:49:41 +0200

    Use switch expressions instead.

--- src/Magick.NET.Core/Profiles/Exif/ExifDataTypes.cs
+++ src/Magick.NET.Core/Profiles/Exif/ExifDataTypes.cs
@@ -8,28 +8,25 @@ namespace ImageMagick
     internal static class ExifDataTypes
     {
         public static uint GetSize(ExifDataType dataType)
-        {
-            switch (dataType)
+            => dataType switch
             {
-                case ExifDataType.String:
-                case ExifDataType.Byte:
-                case ExifDataType.SignedByte:
-                case ExifDataType.Undefined:
-                    return 1;
-                case ExifDataType.Short:
-                case ExifDataType.SignedShort:
-                    return 2;
-                case ExifDataType.Long:
-                case ExifDataType.SignedLong:
-                case ExifDataType.Float:
-                    return 4;
-                case ExifDataType.Double:
-                case ExifDataType.Rational:
-                case ExifDataType.SignedRational:
-                    return 8;
-                default:
-                    throw new NotSupportedException(dataType.ToString());
-            }
-        }
+                ExifDataType.String or
+                ExifDataType.Byte or
+                ExifDataType.SignedByte or
+                ExifDataType.Undefined => 1,
+
+                ExifDataType.Short or
+                ExifDataType.SignedShort => 2,
+
+                ExifDataType.Long or
+                ExifDataType.SignedLong or
+                ExifDataType.Float => 4,
+
+                ExifDataType.Double or
+                ExifDataType.Rational or
+                ExifDataType.SignedRational => 8,
+
+                _ => throw new NotSupportedException(dataType.ToString()),
+            };
     }
 }
