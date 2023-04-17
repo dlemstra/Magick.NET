b1d61978f5a6cc67916d88e70248bb2fb35cc44a
Dirk Lemstra <dirk@lemstra.org>
2023-04-17 17:48:43 +0200

    Added unit test for the extension issue reported in: https://github.com/ImageMagick/ImageMagick/issues/6242.

---
+++ tests/Magick.NET.Tests/Coders/TheMapCoder.cs
@@ -0,0 +1,41 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests
+{
+    public class TheMapCoder
+    {
+        [Fact]
+        public void CanBeReadFromFileWithMapExtensions()
+        {
+            using (var image = new MagickImage(Files.Builtin.Logo))
+            {
+                using (var file = new TemporaryFile("test.map"))
+                {
+                    image.Write(file.FullName, MagickFormat.Map);
+
+                    image.Read(file.FullName, image.Width, image.Height);
+
+                    Assert.Equal(MagickFormat.Map, image.Format);
+                }
+            }
+        }
+
+        [Fact]
+        public void CannotBeReadFromFileWithoutMapExtensions()
+        {
+            using (var image = new MagickImage(Files.Builtin.Logo))
+            {
+                using (var file = new TemporaryFile("test"))
+                {
+                    image.Write(file.FullName, MagickFormat.Map);
+
+                    Assert.Throws<MagickMissingDelegateErrorException>(() => image.Read(file.FullName, image.Width, image.Height));
+                }
+            }
+        }
+    }
+}
