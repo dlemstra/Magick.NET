e51d0a38825b5467baecd828954a4fb3ea5ad6a7
Dirk Lemstra <dirk@lemstra.org>
2023-04-25 23:29:09 +0200

    Try to set the fontconfig directory on macOS.

---
+++ tests/Magick.NET.Tests/FontConfig.cs
@@ -0,0 +1,17 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System.IO;
+using ImageMagick;
+
+namespace Magick.NET.Tests
+{
+    public static class FontConfig
+    {
+        public static void Initialize()
+        {
+            if (Runtime.IsMacOS)
+                MagickNET.SetFontConfigDirectory("/opt/X11/lib/X11/fontconfig");
+        }
+    }
+}
--- tests/Magick.NET.Tests/TestInitializer.cs
+++ tests/Magick.NET.Tests/TestInitializer.cs
@@ -23,6 +23,7 @@ namespace Magick.NET.Tests
         public static bool Initialize()
         {
             Ghostscript.Initialize();
+            FontConfig.Initialize();
 
             var configFiles = ConfigurationFiles.Default;
             configFiles.Policy.Data = ModifyPolicy(configFiles.Policy.Data);
