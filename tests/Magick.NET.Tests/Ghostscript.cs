e6b0b826b2f193e7911c109bf8a5dfcb027c8419
Dirk Lemstra <dirk@lemstra.org>
2023-05-08 09:17:24 +0200

    No longer force installation of Ghostscript with a local debug build.

--- tests/Magick.NET.Tests/Ghostscript.cs
+++ tests/Magick.NET.Tests/Ghostscript.cs
@@ -8,28 +8,33 @@ namespace Magick.NET.Tests
 {
     public static class Ghostscript
     {
-        public static bool IsAvailable
-        {
-            get
-            {
-                if (Runtime.IsWindows)
-                    return true;
-
-                return File.Exists("/usr/bin/gs");
-            }
-        }
+        public static bool IsAvailable { get; }
+#if DEBUG_TEST
+            = GetGhostscriptDirectory() is not null;
+#else
+            = Runtime.IsWindows || File.Exists("/usr/bin/gs");
+#endif
 
         public static void Initialize()
         {
+            var directory = GetGhostscriptDirectory();
+            if (directory is not null)
+                MagickNET.SetGhostscriptDirectory(directory);
+        }
+
+        private static string GetGhostscriptDirectory()
+        {
             if (!Runtime.IsWindows)
-                return;
+                return null;
 
             foreach (var version in new[] { "10.01.1", "10.00.1" })
             {
                 var directory = @$"C:\Program Files (x86)\gs\gs${version}\bin";
                 if (Directory.Exists(directory))
-                    MagickNET.SetGhostscriptDirectory(directory);
+                    return directory;
             }
+
+            return null;
         }
     }
 }
