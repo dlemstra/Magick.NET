4e66279655cb135dc75706e33e5301126d817c0a
Dirk Lemstra <dirk@lemstra.org>
2023-05-13 15:18:06 +0200

    Moved unit test to separate file.

---
+++ tests/Magick.NET.Tests/Helpers/QuantumConverterTests/TheToArrayMethod.cs
@@ -0,0 +1,21 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests;
+
+public partial class QuantumConverterTests
+{
+    public class TheToArrayMethod
+    {
+        [Fact]
+        public void ShouldReturnNullWhenIntPtrIsZero()
+        {
+            var value = QuantumConverter.ToArray(IntPtr.Zero, 4);
+            Assert.Null(value);
+        }
+    }
+}
