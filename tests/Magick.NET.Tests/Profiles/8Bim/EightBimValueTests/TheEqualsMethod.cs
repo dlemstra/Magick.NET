495e0dd5ec5dc23ad17209a25b44f696d7926964
Dirk Lemstra <dirk@lemstra.org>
2023-05-19 08:35:55 +0200

    Forgot to add file.

---
+++ tests/Magick.NET.Tests/Profiles/8Bim/EightBimValueTests/TheEqualsMethod.cs
@@ -0,0 +1,26 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Linq;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests;
+
+public partial class EightBimValueTests
+{
+    public class TheEqualsMethod
+    {
+        [Fact]
+        public void ShouldReturnTrueWhenTheObjectsAreEqual()
+        {
+            using var image = new MagickImage(Files.FujiFilmFinePixS1ProJPG);
+            var profile = image.Get8BimProfile();
+            var first = profile.Values.First();
+            var second = profile.Values.First();
+
+            Assert.True(first.Equals(second));
+            Assert.True(first.Equals((object)second));
+        }
+    }
+}
