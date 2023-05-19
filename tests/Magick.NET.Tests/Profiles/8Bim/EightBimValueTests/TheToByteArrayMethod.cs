50a0d3bf3d40ebc5ea097f55d7e75fff82eda0f1
Dirk Lemstra <dirk@lemstra.org>
2023-05-19 08:32:07 +0200

    Refactored unit tests.

---
+++ tests/Magick.NET.Tests/Profiles/8Bim/EightBimValueTests/TheToByteArrayMethod.cs
@@ -0,0 +1,25 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Linq;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests;
+
+public partial class EightBimValueTests
+{
+    public class TheToByteArrayMethod
+    {
+        [Fact]
+        public void ShouldConvertTheValueToByteArray()
+        {
+            using var image = new MagickImage(Files.FujiFilmFinePixS1ProJPG);
+            var profile = image.Get8BimProfile();
+            var value = profile.Values.First();
+            var bytes = value.ToByteArray();
+
+            Assert.Equal(273, bytes.Length);
+        }
+    }
+}
