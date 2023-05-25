330ceeab8819c9d1a7b66b53c0e951a01f935c5c
Caden-Fletcher <110023835+Caden-Fletcher@users.noreply.github.com>
2023-05-25 17:04:16 -0500

    Updated the CreateAnimatedGif() code sample.

--- samples/Magick.NET.Samples/CombiningImages.cs
+++ samples/Magick.NET.Samples/CombiningImages.cs
@@ -30,13 +30,15 @@ public static class CombiningImagesSamples
     {
         using var images = new MagickImageCollection();
 
-        // Add first image and set the animation delay to 100ms
+        // Add the first image, set the animation delay to 100ms, and set the disposal method
         images.Add(SampleFiles.SnakewarePng);
         images[0].AnimationDelay = 100;
+        images[0].GifDisposeMethod = GifDisposeMethod.Previous; // Prevents frames with transparent backgrounds from overlapping each other
 
-        // Add second image, set the animation delay to 100ms and flip the image
+        // Add the second image, set the animation delay to 100ms, set the disposal method, and flip the image
         images.Add(SampleFiles.SnakewarePng);
         images[1].AnimationDelay = 100;
+        images[1].GifDisposeMethod = GifDisposeMethod.Previous;
         images[1].Flip();
 
         // Optionally reduce colors
