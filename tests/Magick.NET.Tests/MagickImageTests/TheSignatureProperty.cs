6ec473467b48c11f27b65f99a6be20235759d472
Dirk Lemstra <dirk@lemstra.org>
2023-07-21 12:03:08 +0200

    Added extra unit test.

--- tests/Magick.NET.Tests/MagickImageTests/TheSignatureProperty.cs
+++ tests/Magick.NET.Tests/MagickImageTests/TheSignatureProperty.cs
@@ -11,13 +11,21 @@ public partial class MagickImageTests
     public class TheSignatureProperty
     {
         [Fact]
-        public void ShouldReturnImageSignature()
+        public void ShouldReturnImageSignatureForEmptyImage()
         {
             using var image = new MagickImage();
 
             Assert.Equal(0, image.Width);
             Assert.Equal(0, image.Height);
             Assert.Equal("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", image.Signature);
+        }
+
+        [Fact]
+        public void ShouldReturnImageSignatureOfImage()
+        {
+            using var image = new MagickImage(Files.Builtin.Logo);
+
+            Assert.Equal("1bed3c29f223f8c525206258d9601dd5da0bb572943b04bd6ad09ae5dc786d9d", image.Signature);
         }
     }
 }
