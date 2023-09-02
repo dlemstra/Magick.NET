9d78b23dc653c76579440670e95ea3cd8da56c82
Dirk Lemstra <dirk@lemstra.org>
2023-09-02 09:31:20 +0200

    Mark some of the channels as obsolete.

--- src/Magick.NET.Core/Enums/Channels.cs
+++ src/Magick.NET.Core/Enums/Channels.cs
@@ -101,15 +101,18 @@ public enum Channels : ulong
     /// <summary>
     /// Grays.
     /// </summary>
+    [Obsolete("The value 'Grays' will be removed in a future release.")]
     Grays = 0x0400,
 
     /// <summary>
     /// Sync.
     /// </summary>
+    [Obsolete("The value 'Sync' will be removed in a future release.")]
     Sync = 0x20000,
 
     /// <summary>
     /// Default.
     /// </summary>
+    [Obsolete("The value 'Default' will be removed in a future release use 'All' instead.")]
     Default = All,
 }
