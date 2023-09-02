29861af8a91b288225c5203bd7b7e79e4a25bb13
Dirk Lemstra <dirk@lemstra.org>
2023-09-02 10:29:26 +0200

    The value that is being shifted should be 64 bit.

--- src/Magick.NET.Core/Enums/Channels.cs
+++ src/Magick.NET.Core/Enums/Channels.cs
@@ -101,272 +101,272 @@ public enum Channels : long
     /// <summary>
     /// Meta0.
     /// </summary>
-    Meta0 = 1 << PixelChannel.Meta0,
+    Meta0 = 1L << PixelChannel.Meta0,
 
     /// <summary>
     /// Meta1.
     /// </summary>
-    Meta1 = 1 << PixelChannel.Meta1,
+    Meta1 = 1L << PixelChannel.Meta1,
 
     /// <summary>
     /// Meta2.
     /// </summary>
-    Meta2 = 1 << PixelChannel.Meta2,
+    Meta2 = 1L << PixelChannel.Meta2,
 
     /// <summary>
     /// Meta3.
     /// </summary>
-    Meta3 = 1 << PixelChannel.Meta3,
+    Meta3 = 1L << PixelChannel.Meta3,
 
     /// <summary>
     /// Meta4.
     /// </summary>
-    Meta4 = 1 << PixelChannel.Meta4,
+    Meta4 = 1L << PixelChannel.Meta4,
 
     /// <summary>
     /// Meta5.
     /// </summary>
-    Meta5 = 1 << PixelChannel.Meta5,
+    Meta5 = 1L << PixelChannel.Meta5,
 
     /// <summary>
     /// Meta6.
     /// </summary>
-    Meta6 = 1 << PixelChannel.Meta6,
+    Meta6 = 1L << PixelChannel.Meta6,
 
     /// <summary>
     /// Meta7.
     /// </summary>
-    Meta7 = 1 << PixelChannel.Meta7,
+    Meta7 = 1L << PixelChannel.Meta7,
 
     /// <summary>
     /// Meta8.
     /// </summary>
-    Meta8 = 1 << PixelChannel.Meta8,
+    Meta8 = 1L << PixelChannel.Meta8,
 
     /// <summary>
     /// Meta9.
     /// </summary>
-    Meta9 = 1 << PixelChannel.Meta9,
+    Meta9 = 1L << PixelChannel.Meta9,
 
     /// <summary>
     /// Meta10.
     /// </summary>
-    Meta10 = 1 << PixelChannel.Meta10,
+    Meta10 = 1L << PixelChannel.Meta10,
 
     /// <summary>
     /// Meta11.
     /// </summary>
-    Meta11 = 1 << PixelChannel.Meta11,
+    Meta11 = 1L << PixelChannel.Meta11,
 
     /// <summary>
     /// Meta12.
     /// </summary>
-    Meta12 = 1 << PixelChannel.Meta12,
+    Meta12 = 1L << PixelChannel.Meta12,
 
     /// <summary>
     /// Meta13.
     /// </summary>
-    Meta13 = 1 << PixelChannel.Meta13,
+    Meta13 = 1L << PixelChannel.Meta13,
 
     /// <summary>
     /// Meta14.
     /// </summary>
-    Meta14 = 1 << PixelChannel.Meta14,
+    Meta14 = 1L << PixelChannel.Meta14,
 
     /// <summary>
     /// Meta15.
     /// </summary>
-    Meta15 = 1 << PixelChannel.Meta15,
+    Meta15 = 1L << PixelChannel.Meta15,
 
     /// <summary>
     /// Meta16.
     /// </summary>
-    Meta16 = 1 << PixelChannel.Meta16,
+    Meta16 = 1L << PixelChannel.Meta16,
 
     /// <summary>
     /// Meta17.
     /// </summary>
-    Meta17 = 1 << PixelChannel.Meta17,
+    Meta17 = 1L << PixelChannel.Meta17,
 
     /// <summary>
     /// Meta18.
     /// </summary>
-    Meta18 = 1 << PixelChannel.Meta18,
+    Meta18 = 1L << PixelChannel.Meta18,
 
     /// <summary>
     /// Meta19.
     /// </summary>
-    Meta19 = 1 << PixelChannel.Meta19,
+    Meta19 = 1L << PixelChannel.Meta19,
 
     /// <summary>
     /// Meta20.
     /// </summary>
-    Meta20 = 1 << PixelChannel.Meta20,
+    Meta20 = 1L << PixelChannel.Meta20,
 
     /// <summary>
     /// Meta21.
     /// </summary>
-    Meta21 = 1 << PixelChannel.Meta21,
+    Meta21 = 1L << PixelChannel.Meta21,
 
     /// <summary>
     /// Meta22.
     /// </summary>
-    Meta22 = 1 << PixelChannel.Meta22,
+    Meta22 = 1L << PixelChannel.Meta22,
 
     /// <summary>
     /// Meta23.
     /// </summary>
-    Meta23 = 1 << PixelChannel.Meta23,
+    Meta23 = 1L << PixelChannel.Meta23,
 
     /// <summary>
     /// Meta24.
     /// </summary>
-    Meta24 = 1 << PixelChannel.Meta24,
+    Meta24 = 1L << PixelChannel.Meta24,
 
     /// <summary>
     /// Meta25.
     /// </summary>
-    Meta25 = 1 << PixelChannel.Meta25,
+    Meta25 = 1L << PixelChannel.Meta25,
 
     /// <summary>
     /// Meta26.
     /// </summary>
-    Meta26 = 1 << PixelChannel.Meta26,
+    Meta26 = 1L << PixelChannel.Meta26,
 
     /// <summary>
     /// Meta27.
     /// </summary>
-    Meta27 = 1 << PixelChannel.Meta27,
+    Meta27 = 1L << PixelChannel.Meta27,
 
     /// <summary>
     /// Meta28.
     /// </summary>
-    Meta28 = 1 << PixelChannel.Meta28,
+    Meta28 = 1L << PixelChannel.Meta28,
 
     /// <summary>
     /// Meta29.
     /// </summary>
-    Meta29 = 1 << PixelChannel.Meta29,
+    Meta29 = 1L << PixelChannel.Meta29,
 
     /// <summary>
     /// Meta30.
     /// </summary>
-    Meta30 = 1 << PixelChannel.Meta30,
+    Meta30 = 1L << PixelChannel.Meta30,
 
     /// <summary>
     /// Meta31.
     /// </summary>
-    Meta31 = 1 << PixelChannel.Meta31,
+    Meta31 = 1L << PixelChannel.Meta31,
 
     /// <summary>
     /// Meta32.
     /// </summary>
-    Meta32 = 1 << PixelChannel.Meta32,
+    Meta32 = 1L << PixelChannel.Meta32,
 
     /// <summary>
     /// Meta33.
     /// </summary>
-    Meta33 = 1 << PixelChannel.Meta33,
+    Meta33 = 1L << PixelChannel.Meta33,
 
     /// <summary>
     /// Meta34.
     /// </summary>
-    Meta34 = 1 << PixelChannel.Meta34,
+    Meta34 = 1L << PixelChannel.Meta34,
 
     /// <summary>
     /// Meta35.
     /// </summary>
-    Meta35 = 1 << PixelChannel.Meta35,
+    Meta35 = 1L << PixelChannel.Meta35,
 
     /// <summary>
     /// Meta36.
     /// </summary>
-    Meta36 = 1 << PixelChannel.Meta36,
+    Meta36 = 1L << PixelChannel.Meta36,
 
     /// <summary>
     /// Meta37.
     /// </summary>
-    Meta37 = 1 << PixelChannel.Meta37,
+    Meta37 = 1L << PixelChannel.Meta37,
 
     /// <summary>
     /// Meta38.
     /// </summary>
-    Meta38 = 1 << PixelChannel.Meta38,
+    Meta38 = 1L << PixelChannel.Meta38,
 
     /// <summary>
     /// Meta39.
     /// </summary>
-    Meta39 = 1 << PixelChannel.Meta39,
+    Meta39 = 1L << PixelChannel.Meta39,
 
     /// <summary>
     /// Meta40.
     /// </summary>
-    Meta40 = 1 << PixelChannel.Meta40,
+    Meta40 = 1L << PixelChannel.Meta40,
 
     /// <summary>
     /// Meta41.
     /// </summary>
-    Meta41 = 1 << PixelChannel.Meta41,
+    Meta41 = 1L << PixelChannel.Meta41,
 
     /// <summary>
     /// Meta42.
     /// </summary>
-    Meta42 = 1 << PixelChannel.Meta42,
+    Meta42 = 1L << PixelChannel.Meta42,
 
     /// <summary>
     /// Meta43.
     /// </summary>
-    Meta43 = 1 << PixelChannel.Meta43,
+    Meta43 = 1L << PixelChannel.Meta43,
 
     /// <summary>
     /// Meta44.
     /// </summary>
-    Meta44 = 1 << PixelChannel.Meta44,
+    Meta44 = 1L << PixelChannel.Meta44,
 
     /// <summary>
     /// Meta45.
     /// </summary>
-    Meta45 = 1 << PixelChannel.Meta45,
+    Meta45 = 1L << PixelChannel.Meta45,
 
     /// <summary>
     /// Meta46.
     /// </summary>
-    Meta46 = 1 << PixelChannel.Meta46,
+    Meta46 = 1L << PixelChannel.Meta46,
 
     /// <summary>
     /// Meta47.
     /// </summary>
-    Meta47 = 1 << PixelChannel.Meta47,
+    Meta47 = 1L << PixelChannel.Meta47,
 
     /// <summary>
     /// Meta48.
     /// </summary>
-    Meta48 = 1 << PixelChannel.Meta48,
+    Meta48 = 1L << PixelChannel.Meta48,
 
     /// <summary>
     /// Meta49.
     /// </summary>
-    Meta49 = 1 << PixelChannel.Meta49,
+    Meta49 = 1L << PixelChannel.Meta49,
 
     /// <summary>
     /// Meta50.
     /// </summary>
-    Meta50 = 1 << PixelChannel.Meta50,
+    Meta50 = 1L << PixelChannel.Meta50,
 
     /// <summary>
     /// Meta51.
     /// </summary>
-    Meta51 = 1 << PixelChannel.Meta51,
+    Meta51 = 1L << PixelChannel.Meta51,
 
     /// <summary>
     /// Meta52.
     /// </summary>
-    Meta52 = 1 << PixelChannel.Meta52,
+    Meta52 = 1L << PixelChannel.Meta52,
 
     /// <summary>
-    /// Meta53.
+    /// Meta5.
     /// </summary>
-    Meta53 = 1 << PixelChannel.Meta53,
+    Meta53 = 1L << PixelChannel.Meta53,
 
     /// <summary>
     /// Grays.
