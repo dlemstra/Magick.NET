d935af89b82cd148db45be4cdcf0017eb5a81ce1
Dirk Lemstra <dirk@lemstra.org>
2023-09-02 10:06:53 +0200

    Added meta channels.

--- src/Magick.NET.Core/Enums/Channels.cs
+++ src/Magick.NET.Core/Enums/Channels.cs
@@ -11,7 +11,7 @@ namespace ImageMagick;
 /// </summary>
 [Flags]
 [SuppressMessage("Naming", "CA1724", Justification = "No real collision.")]
-public enum Channels : ulong
+public enum Channels : long
 {
     /// <summary>
     /// Undefined.
@@ -97,6 +97,276 @@ public enum Channels : ulong
     /// CMYK.
     /// </summary>
     CMYK = Cyan | Magenta | Yellow | Black,
+
+    /// <summary>
+    /// Meta0.
+    /// </summary>
+    Meta0 = 1 << PixelChannel.Meta0,
+
+    /// <summary>
+    /// Meta1.
+    /// </summary>
+    Meta1 = 1 << PixelChannel.Meta1,
+
+    /// <summary>
+    /// Meta2.
+    /// </summary>
+    Meta2 = 1 << PixelChannel.Meta2,
+
+    /// <summary>
+    /// Meta3.
+    /// </summary>
+    Meta3 = 1 << PixelChannel.Meta3,
+
+    /// <summary>
+    /// Meta4.
+    /// </summary>
+    Meta4 = 1 << PixelChannel.Meta4,
+
+    /// <summary>
+    /// Meta5.
+    /// </summary>
+    Meta5 = 1 << PixelChannel.Meta5,
+
+    /// <summary>
+    /// Meta6.
+    /// </summary>
+    Meta6 = 1 << PixelChannel.Meta6,
+
+    /// <summary>
+    /// Meta7.
+    /// </summary>
+    Meta7 = 1 << PixelChannel.Meta7,
+
+    /// <summary>
+    /// Meta8.
+    /// </summary>
+    Meta8 = 1 << PixelChannel.Meta8,
+
+    /// <summary>
+    /// Meta9.
+    /// </summary>
+    Meta9 = 1 << PixelChannel.Meta9,
+
+    /// <summary>
+    /// Meta10.
+    /// </summary>
+    Meta10 = 1 << PixelChannel.Meta10,
+
+    /// <summary>
+    /// Meta11.
+    /// </summary>
+    Meta11 = 1 << PixelChannel.Meta11,
+
+    /// <summary>
+    /// Meta12.
+    /// </summary>
+    Meta12 = 1 << PixelChannel.Meta12,
+
+    /// <summary>
+    /// Meta13.
+    /// </summary>
+    Meta13 = 1 << PixelChannel.Meta13,
+
+    /// <summary>
+    /// Meta14.
+    /// </summary>
+    Meta14 = 1 << PixelChannel.Meta14,
+
+    /// <summary>
+    /// Meta15.
+    /// </summary>
+    Meta15 = 1 << PixelChannel.Meta15,
+
+    /// <summary>
+    /// Meta16.
+    /// </summary>
+    Meta16 = 1 << PixelChannel.Meta16,
+
+    /// <summary>
+    /// Meta17.
+    /// </summary>
+    Meta17 = 1 << PixelChannel.Meta17,
+
+    /// <summary>
+    /// Meta18.
+    /// </summary>
+    Meta18 = 1 << PixelChannel.Meta18,
+
+    /// <summary>
+    /// Meta19.
+    /// </summary>
+    Meta19 = 1 << PixelChannel.Meta19,
+
+    /// <summary>
+    /// Meta20.
+    /// </summary>
+    Meta20 = 1 << PixelChannel.Meta20,
+
+    /// <summary>
+    /// Meta21.
+    /// </summary>
+    Meta21 = 1 << PixelChannel.Meta21,
+
+    /// <summary>
+    /// Meta22.
+    /// </summary>
+    Meta22 = 1 << PixelChannel.Meta22,
+
+    /// <summary>
+    /// Meta23.
+    /// </summary>
+    Meta23 = 1 << PixelChannel.Meta23,
+
+    /// <summary>
+    /// Meta24.
+    /// </summary>
+    Meta24 = 1 << PixelChannel.Meta24,
+
+    /// <summary>
+    /// Meta25.
+    /// </summary>
+    Meta25 = 1 << PixelChannel.Meta25,
+
+    /// <summary>
+    /// Meta26.
+    /// </summary>
+    Meta26 = 1 << PixelChannel.Meta26,
+
+    /// <summary>
+    /// Meta27.
+    /// </summary>
+    Meta27 = 1 << PixelChannel.Meta27,
+
+    /// <summary>
+    /// Meta28.
+    /// </summary>
+    Meta28 = 1 << PixelChannel.Meta28,
+
+    /// <summary>
+    /// Meta29.
+    /// </summary>
+    Meta29 = 1 << PixelChannel.Meta29,
+
+    /// <summary>
+    /// Meta30.
+    /// </summary>
+    Meta30 = 1 << PixelChannel.Meta30,
+
+    /// <summary>
+    /// Meta31.
+    /// </summary>
+    Meta31 = 1 << PixelChannel.Meta31,
+
+    /// <summary>
+    /// Meta32.
+    /// </summary>
+    Meta32 = 1 << PixelChannel.Meta32,
+
+    /// <summary>
+    /// Meta33.
+    /// </summary>
+    Meta33 = 1 << PixelChannel.Meta33,
+
+    /// <summary>
+    /// Meta34.
+    /// </summary>
+    Meta34 = 1 << PixelChannel.Meta34,
+
+    /// <summary>
+    /// Meta35.
+    /// </summary>
+    Meta35 = 1 << PixelChannel.Meta35,
+
+    /// <summary>
+    /// Meta36.
+    /// </summary>
+    Meta36 = 1 << PixelChannel.Meta36,
+
+    /// <summary>
+    /// Meta37.
+    /// </summary>
+    Meta37 = 1 << PixelChannel.Meta37,
+
+    /// <summary>
+    /// Meta38.
+    /// </summary>
+    Meta38 = 1 << PixelChannel.Meta38,
+
+    /// <summary>
+    /// Meta39.
+    /// </summary>
+    Meta39 = 1 << PixelChannel.Meta39,
+
+    /// <summary>
+    /// Meta40.
+    /// </summary>
+    Meta40 = 1 << PixelChannel.Meta40,
+
+    /// <summary>
+    /// Meta41.
+    /// </summary>
+    Meta41 = 1 << PixelChannel.Meta41,
+
+    /// <summary>
+    /// Meta42.
+    /// </summary>
+    Meta42 = 1 << PixelChannel.Meta42,
+
+    /// <summary>
+    /// Meta43.
+    /// </summary>
+    Meta43 = 1 << PixelChannel.Meta43,
+
+    /// <summary>
+    /// Meta44.
+    /// </summary>
+    Meta44 = 1 << PixelChannel.Meta44,
+
+    /// <summary>
+    /// Meta45.
+    /// </summary>
+    Meta45 = 1 << PixelChannel.Meta45,
+
+    /// <summary>
+    /// Meta46.
+    /// </summary>
+    Meta46 = 1 << PixelChannel.Meta46,
+
+    /// <summary>
+    /// Meta47.
+    /// </summary>
+    Meta47 = 1 << PixelChannel.Meta47,
+
+    /// <summary>
+    /// Meta48.
+    /// </summary>
+    Meta48 = 1 << PixelChannel.Meta48,
+
+    /// <summary>
+    /// Meta49.
+    /// </summary>
+    Meta49 = 1 << PixelChannel.Meta49,
+
+    /// <summary>
+    /// Meta50.
+    /// </summary>
+    Meta50 = 1 << PixelChannel.Meta50,
+
+    /// <summary>
+    /// Meta51.
+    /// </summary>
+    Meta51 = 1 << PixelChannel.Meta51,
+
+    /// <summary>
+    /// Meta52.
+    /// </summary>
+    Meta52 = 1 << PixelChannel.Meta52,
+
+    /// <summary>
+    /// Meta53.
+    /// </summary>
+    Meta53 = 1 << PixelChannel.Meta53,
 
     /// <summary>
     /// Grays.
--- src/Magick.NET.Core/Enums/PixelChannel.cs
+++ src/Magick.NET.Core/Enums/PixelChannel.cs
@@ -59,6 +59,276 @@ public enum PixelChannel
     Index = 5,
 
     /// <summary>
+    /// Meta0.
+    /// </summary>
+    Meta0 = 10,
+
+    /// <summary>
+    /// Meta1.
+    /// </summary>
+    Meta1 = 11,
+
+    /// <summary>
+    /// Meta2.
+    /// </summary>
+    Meta2 = 12,
+
+    /// <summary>
+    /// Meta3.
+    /// </summary>
+    Meta3 = 13,
+
+    /// <summary>
+    /// Meta4.
+    /// </summary>
+    Meta4 = 14,
+
+    /// <summary>
+    /// Meta5.
+    /// </summary>
+    Meta5 = 15,
+
+    /// <summary>
+    /// Meta6.
+    /// </summary>
+    Meta6 = 16,
+
+    /// <summary>
+    /// Meta7.
+    /// </summary>
+    Meta7 = 17,
+
+    /// <summary>
+    /// Meta8.
+    /// </summary>
+    Meta8 = 18,
+
+    /// <summary>
+    /// Meta9.
+    /// </summary>
+    Meta9 = 19,
+
+    /// <summary>
+    /// Meta10.
+    /// </summary>
+    Meta10 = 20,
+
+    /// <summary>
+    /// Meta11.
+    /// </summary>
+    Meta11 = 21,
+
+    /// <summary>
+    /// Meta12.
+    /// </summary>
+    Meta12 = 22,
+
+    /// <summary>
+    /// Meta13.
+    /// </summary>
+    Meta13 = 23,
+
+    /// <summary>
+    /// Meta14.
+    /// </summary>
+    Meta14 = 24,
+
+    /// <summary>
+    /// Meta15.
+    /// </summary>
+    Meta15 = 25,
+
+    /// <summary>
+    /// Meta16.
+    /// </summary>
+    Meta16 = 26,
+
+    /// <summary>
+    /// Meta17.
+    /// </summary>
+    Meta17 = 27,
+
+    /// <summary>
+    /// Meta18.
+    /// </summary>
+    Meta18 = 28,
+
+    /// <summary>
+    /// Meta19.
+    /// </summary>
+    Meta19 = 29,
+
+    /// <summary>
+    /// Meta20.
+    /// </summary>
+    Meta20 = 30,
+
+    /// <summary>
+    /// Meta21.
+    /// </summary>
+    Meta21 = 31,
+
+    /// <summary>
+    /// Meta22.
+    /// </summary>
+    Meta22 = 32,
+
+    /// <summary>
+    /// Meta23.
+    /// </summary>
+    Meta23 = 33,
+
+    /// <summary>
+    /// Meta24.
+    /// </summary>
+    Meta24 = 34,
+
+    /// <summary>
+    /// Meta25.
+    /// </summary>
+    Meta25 = 35,
+
+    /// <summary>
+    /// Meta26.
+    /// </summary>
+    Meta26 = 36,
+
+    /// <summary>
+    /// Meta27.
+    /// </summary>
+    Meta27 = 37,
+
+    /// <summary>
+    /// Meta28.
+    /// </summary>
+    Meta28 = 38,
+
+    /// <summary>
+    /// Meta29.
+    /// </summary>
+    Meta29 = 39,
+
+    /// <summary>
+    /// Meta30.
+    /// </summary>
+    Meta30 = 40,
+
+    /// <summary>
+    /// Meta31.
+    /// </summary>
+    Meta31 = 41,
+
+    /// <summary>
+    /// Meta32.
+    /// </summary>
+    Meta32 = 42,
+
+    /// <summary>
+    /// Meta33.
+    /// </summary>
+    Meta33 = 43,
+
+    /// <summary>
+    /// Meta34.
+    /// </summary>
+    Meta34 = 44,
+
+    /// <summary>
+    /// Meta35.
+    /// </summary>
+    Meta35 = 45,
+
+    /// <summary>
+    /// Meta36.
+    /// </summary>
+    Meta36 = 46,
+
+    /// <summary>
+    /// Meta37.
+    /// </summary>
+    Meta37 = 47,
+
+    /// <summary>
+    /// Meta38.
+    /// </summary>
+    Meta38 = 48,
+
+    /// <summary>
+    /// Meta39.
+    /// </summary>
+    Meta39 = 49,
+
+    /// <summary>
+    /// Meta40.
+    /// </summary>
+    Meta40 = 50,
+
+    /// <summary>
+    /// Meta41.
+    /// </summary>
+    Meta41 = 51,
+
+    /// <summary>
+    /// Meta42.
+    /// </summary>
+    Meta42 = 52,
+
+    /// <summary>
+    /// Meta43.
+    /// </summary>
+    Meta43 = 53,
+
+    /// <summary>
+    /// Meta44.
+    /// </summary>
+    Meta44 = 54,
+
+    /// <summary>
+    /// Meta45.
+    /// </summary>
+    Meta45 = 55,
+
+    /// <summary>
+    /// Meta46.
+    /// </summary>
+    Meta46 = 56,
+
+    /// <summary>
+    /// Meta47.
+    /// </summary>
+    Meta47 = 57,
+
+    /// <summary>
+    /// Meta48.
+    /// </summary>
+    Meta48 = 58,
+
+    /// <summary>
+    /// Meta49.
+    /// </summary>
+    Meta49 = 59,
+
+    /// <summary>
+    /// Meta50.
+    /// </summary>
+    Meta50 = 60,
+
+    /// <summary>
+    /// Meta51.
+    /// </summary>
+    Meta51 = 61,
+
+    /// <summary>
+    /// Meta52.
+    /// </summary>
+    Meta52 = 62,
+
+    /// <summary>
+    /// Meta53.
+    /// </summary>
+    Meta53 = 63,
+
+    /// <summary>
     /// Composite.
     /// </summary>
     Composite = 64,
