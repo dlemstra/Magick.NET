d8e7f5b8abdc700e26237369f2dfde08a96ec78b
Dirk Lemstra <dirk@lemstra.org>
2023-09-02 12:12:32 +0200

    Added arguments to post-create-command.sh so this script can be re-used at a later moment.

--- .devcontainer/linux-x64/post-create-command.sh
+++ .devcontainer/linux-x64/post-create-command.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+config=$1
+arch=$2
+
 if [ -z "$GITHUB_USER" ]; then
     export GITHUB_USER='dlemstra'
 fi
@@ -9,4 +12,4 @@ if [ -z "$GITHUB_TOKEN" ]; then
     export GITHUB_TOKEN=$(cat /build/api.key.txt)
 fi
 
-build/shared/install.Magick.Native.sh ${GITHUB_USER} ${GITHUB_TOKEN} linux x64
+build/shared/install.Magick.Native.sh ${GITHUB_USER} ${GITHUB_TOKEN} ${config} ${arch}
