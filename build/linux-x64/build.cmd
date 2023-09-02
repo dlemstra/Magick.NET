9dbc85b97c6ab4b5080eb6fde578e002e1b89614
Dirk Lemstra <dirk@lemstra.org>
2023-09-02 12:15:04 +0200

    Removed batch files.

--- build/linux-x64/build.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-set /p GITHUB_TOKEN=<../../src/Magick.Native/api.key.txt
-docker build -f Dockerfile --build-arg GITHUB_TOKEN=%GITHUB_TOKEN% -t magick-net-linux ../..
--- build/linux-x64/run.openmp.cmd
+++
@@ -1,1 +0,0 @@
-docker run -v %~dp0:/output -w /magick -it magick-net-linux-openmp /bin/bash
