e70a471c70af404566747182c37217f5a3b97373
Dirk Lemstra <dirk@lemstra.org>
2023-12-03 16:26:02 +0100

    Use static version of ffmpeg instead.

--- build/osx-x64/install.dependencies.sh
+++ build/osx-x64/install.dependencies.sh
@@ -3,5 +3,9 @@ set -e
 
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-brew install ffmpeg fontconfig
+brew install fontconfig
 fc-list
+
+downloadUrl=$(curl 'https://evermeet.cx/ffmpeg/info/ffmpeg/6.0' -fsS| jq -rc '.download.zip.url') 
+curl -f -L -# --compressed -A 'https://github.com/eugeneware/ffmpeg-static binaries download script' -o "ffmpeg-darwin-x64.zip" "$downloadUrl"
+unzip -o -d /usr/local/bin -j ffmpeg-darwin-x64.zip ffmpeg
