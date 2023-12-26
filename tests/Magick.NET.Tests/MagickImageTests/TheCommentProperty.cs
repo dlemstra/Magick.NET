1115212a7402a323a50a438768a714413a8d04d7
Dirk Lemstra <dirk@lemstra.org>
2023-12-26 10:36:10 +0100

    Added extra unit tests to check if a comment with non-Latin1 characters is saved in an itxt chunk.

--- tests/Magick.NET.Tests/MagickImageTests/TheCommentProperty.cs
+++ tests/Magick.NET.Tests/MagickImageTests/TheCommentProperty.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 using ImageMagick;
+using ImageMagick.Formats;
 using Xunit;
 
 namespace Magick.NET.Tests;
@@ -37,6 +38,33 @@ public partial class MagickImageTests
             image.Comment = null;
 
             Assert.Null(image.GetAttribute("comment"));
+        }
+
+        [Fact]
+        public void ShouldBeStoredAsItxtInPngFileWhenStringContainsNonAnsiCharacters()
+        {
+            using var image = new MagickImage(MagickColors.Purple, 1, 1);
+            using var tempFile = new TemporaryFile("test.png");
+
+            var comment = "Hello";
+            image.Comment = comment;
+            image.Write(tempFile.File);
+
+            image.Read(tempFile.File);
+            Assert.Equal(comment, image.Comment);
+
+            var pngText = image.GetAttribute("png:text");
+            Assert.Equal("4 tEXt/zTXt/iTXt chunks were found", pngText);
+
+            comment = "Hello 😉";
+            image.Comment = comment;
+            image.Write(tempFile.File);
+
+            image.Read(tempFile.File);
+            Assert.Equal(comment, image.Comment);
+
+            pngText = image.GetAttribute("png:text");
+            Assert.Equal("3 tEXt/zTXt/iTXt chunks were found", pngText);
         }
     }
 }
