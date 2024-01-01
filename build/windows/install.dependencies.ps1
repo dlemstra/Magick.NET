6aaf0b8d5324f27fac56fc78f523f5bad0aac052
Dirk Lemstra <dirk@lemstra.org>
2024-01-01 10:53:25 +0100

    Removed old download action.

--- build/windows/install.dependencies.ps1
+++ build/windows/install.dependencies.ps1
@@ -1,10 +1,6 @@
 # Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
 # Licensed under the Apache License, Version 2.0.
 
-param (
-    [string]$pfxUri = ''
-)
-
 Write-Host "Downloading NuGet"
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $targetNugetExe = "$PSScriptRoot\..\..\tools\windows\nuget.exe"
@@ -19,8 +15,3 @@ Write-Host "Downloading FFmpeg 4.2.3"
 $sourceFFmpegExe = "https://github.com/ImageMagick/ImageMagick-Windows/releases/download/20200615/ffmpeg-4.2.3-win64.exe"
 $targetFFmpegExe = "c:\vcpkg\ffmpeg.exe"
 Invoke-WebRequest $sourceFFmpegExe -OutFile $targetFFmpegExe
-
-if ($pfxUri.Length -gt 0) {
-    Write-Host "Downloading code signing certificate"
-    Invoke-WebRequest $pfxUri -OutFile "$PSScriptRoot\ImageMagick.pfx"
-}
