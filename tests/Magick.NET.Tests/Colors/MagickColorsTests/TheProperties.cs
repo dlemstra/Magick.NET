de25c38d227f0ab10a8124a9c91f020d522a95f0
Dirk Lemstra <dirk@lemstra.org>
2024-01-17 20:08:27 +0100

    Added unit test for the output of the source generator.

---
+++ tests/Magick.NET.Tests/Colors/MagickColorsTests/TheProperties.cs
@@ -0,0 +1,27 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Linq;
+using System.Reflection;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests;
+
+public partial class MagickColorsTests
+{
+    public class TheProperties
+    {
+        [Fact]
+        public void ShouldContainTheCorrectNumberOfColors()
+        {
+            var colorCount = typeof(MagickColors)
+                .GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Where(property => property.PropertyType == typeof(MagickColor))
+                .Count();
+
+            Assert.Equal(143, colorCount);
+        }
+    }
+}
