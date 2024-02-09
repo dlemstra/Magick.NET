015ae29b7a56cd9358f8713dc0afc194143e4d6f
Florian Levis <florian.levis@alphanetworks.tv>
2024-02-09 16:44:06 +0100

    doc: add missing exception tag

--- src/Magick.NET/MagickImage.cs
+++ src/Magick.NET/MagickImage.cs
@@ -1314,6 +1314,7 @@ public sealed partial class MagickImage : IMagickImage<QuantumType>, INativeInst
     /// </summary>
     /// <param name="width">The width of the neighborhood in pixels (> 0).</param>
     /// <param name="height">The height of the neighborhood in pixels (> 0).</param>
+    /// <exception cref="MagickException">Thrown when an error is raised by ImageMagick.</exception>
     public void BilateralBlur(int width, int height)
     {
         Throw.IfFalse(nameof(width), width > 1, "The width must be > 1");
@@ -1329,6 +1330,7 @@ public sealed partial class MagickImage : IMagickImage<QuantumType>, INativeInst
     /// <param name="height">The height of the neighborhood in pixels (> 0).</param>
     /// <param name="intensitySigma">The sigma in the intensity space.</param>
     /// <param name="spatialSigma">The sigma in the coordinate space.</param>
+    /// <exception cref="MagickException">Thrown when an error is raised by ImageMagick.</exception>
     public void BilateralBlur(int width, int height, double intensitySigma, double spatialSigma)
     {
         Throw.IfFalse(nameof(width), width > 1, "The width must be > 1");
