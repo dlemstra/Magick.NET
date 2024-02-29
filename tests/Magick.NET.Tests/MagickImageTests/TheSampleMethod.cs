606ddcab1a7a25f9382e81ebee765ec2c6b453cc
Florian Levis <levis.florian@gmail.com>
2024-02-29 22:16:10 +0100

    tests: add missing tests for MagickImage.Sample method

--- tests/Magick.NET.Tests/MagickImageTests/TheSampleMethod.cs
+++ tests/Magick.NET.Tests/MagickImageTests/TheSampleMethod.cs
@@ -20,6 +20,20 @@ public partial class MagickImageTests
         }
 
         [Fact]
+        public void ShouldThrowExceptionWhenWidthIsNegative()
+        {
+            using var image = new MagickImage(Files.Builtin.Logo);
+            Assert.Throws<ArgumentException>("width", () => image.Sample(-1, 400));
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenHeightIsNegative()
+        {
+            using var image = new MagickImage(Files.Builtin.Logo);
+            Assert.Throws<ArgumentException>("height", () => image.Sample(400, -1));
+        }
+
+        [Fact]
         public void ShouldResizeTheImage()
         {
             using var image = new MagickImage(Files.Builtin.Logo);
