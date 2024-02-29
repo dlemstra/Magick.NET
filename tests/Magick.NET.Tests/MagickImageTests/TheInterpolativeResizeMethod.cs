51dcfb2e7192d64bd419af26e668bc0229a711d3
Florian Levis <levis.florian@gmail.com>
2024-02-29 21:45:44 +0100

    tests: add missing tests for MagickImage.InterpolativeResize method

--- tests/Magick.NET.Tests/MagickImageTests/TheInterpolativeResizeMethod.cs
+++ tests/Magick.NET.Tests/MagickImageTests/TheInterpolativeResizeMethod.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
 // Licensed under the Apache License, Version 2.0.
 
+using System;
 using ImageMagick;
 using Xunit;
 
@@ -10,6 +11,20 @@ public partial class MagickImageTests
 {
     public class TheInterpolativeResizeMethod
     {
+        [Fact]
+        public void ShouldThrowExceptionWhenWidthIsNegative()
+        {
+            using var image = new MagickImage(Files.RedPNG);
+            Assert.Throws<ArgumentException>("width", () => image.InterpolativeResize(-1, 32, PixelInterpolateMethod.Mesh));
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenHeightIsNegative()
+        {
+            using var image = new MagickImage(Files.RedPNG);
+            Assert.Throws<ArgumentException>("height", () => image.InterpolativeResize(32, -1, PixelInterpolateMethod.Mesh));
+        }
+
         [Fact]
         public void ShouldResizeTheImage()
         {
