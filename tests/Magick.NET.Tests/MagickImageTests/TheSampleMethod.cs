3313c7f16a4b542b9f9c2f00cb6452afe93bdee2
Florian Levis <levis.florian@gmail.com>
2024-02-29 22:56:11 +0100

    tests: add missing tests for MagickImage.Sample method

--- tests/Magick.NET.Tests/MagickImageTests/TheSampleMethod.cs
+++ tests/Magick.NET.Tests/MagickImageTests/TheSampleMethod.cs
@@ -11,56 +11,94 @@ public partial class MagickImageTests
 {
     public class TheSampleMethod
     {
-        [Fact]
-        public void ShouldThrowExceptionWhenGeometryIsNull()
+        public class WithGeometry
         {
-            using var image = new MagickImage();
+            [Fact]
+            public void ShouldThrowExceptionWhenGeometryIsNull()
+            {
+                using var image = new MagickImage();
 
-            Assert.Throws<ArgumentNullException>("geometry", () => image.Sample(null));
+                Assert.Throws<ArgumentNullException>("geometry", () => image.Sample(null));
+            }
+
+            [Fact]
+            public void ShouldUseTheSpecifiedGeometry()
+            {
+                using var image = new MagickImage(Files.Builtin.Logo);
+                image.Sample(new MagickGeometry(300, 300));
+
+                Assert.Equal(300, image.Width);
+                Assert.Equal(225, image.Height);
+            }
         }
 
-        [Fact]
-        public void ShouldThrowExceptionWhenWidthIsNegative()
+        public class WithWidthAndHeight
         {
-            using var image = new MagickImage(Files.Builtin.Logo);
-            Assert.Throws<ArgumentException>("width", () => image.Sample(-1, 400));
+            [Fact]
+            public void ShouldThrowExceptionWhenWidthIsNegative()
+            {
+                using var image = new MagickImage(Files.Builtin.Logo);
+                Assert.Throws<ArgumentException>("width", () => image.Sample(-1, 400));
+            }
+
+            [Fact]
+            public void ShouldThrowExceptionWhenHeightIsNegative()
+            {
+                using var image = new MagickImage(Files.Builtin.Logo);
+                Assert.Throws<ArgumentException>("height", () => image.Sample(400, -1));
+            }
+
+            [Fact]
+            public void ShouldResizeTheImage()
+            {
+                using var image = new MagickImage(Files.Builtin.Logo);
+                image.Sample(400, 400);
+
+                Assert.Equal(400, image.Width);
+                Assert.Equal(300, image.Height);
+            }
         }
 
-        [Fact]
-        public void ShouldThrowExceptionWhenHeightIsNegative()
+        public class WithPercentage
         {
-            using var image = new MagickImage(Files.Builtin.Logo);
-            Assert.Throws<ArgumentException>("height", () => image.Sample(400, -1));
-        }
+            [Fact]
+            public void ShouldThrowExceptionWhenPercentageIsNegative()
+            {
+                var percentage = new Percentage(-0.5);
+                using var image = new MagickImage(Files.Builtin.Logo);
 
-        [Fact]
-        public void ShouldResizeTheImage()
-        {
-            using var image = new MagickImage(Files.Builtin.Logo);
-            image.Sample(400, 400);
+                Assert.Throws<ArgumentException>("percentageWidth", () => image.Sample(percentage));
+            }
 
-            Assert.Equal(400, image.Width);
-            Assert.Equal(300, image.Height);
-        }
+            [Fact]
+            public void ShouldThrowExceptionWhenPercentageWidthIsNegative()
+            {
+                var percentageWidth = new Percentage(-0.5);
+                var percentageHeight = new Percentage(10);
+                using var image = new MagickImage(Files.Builtin.Logo);
 
-        [Fact]
-        public void ShouldUseTheSpecifiedGeometry()
-        {
-            using var image = new MagickImage(Files.Builtin.Logo);
-            image.Sample(new MagickGeometry(300, 300));
+                Assert.Throws<ArgumentException>("percentageWidth", () => image.Sample(percentageWidth, percentageHeight));
+            }
 
-            Assert.Equal(300, image.Width);
-            Assert.Equal(225, image.Height);
-        }
+            [Fact]
+            public void ShouldThrowExceptionWhenPercentageHeightIsNegative()
+            {
+                var percentageWidth = new Percentage(10);
+                var percentageHeight = new Percentage(-0.5);
+                using var image = new MagickImage(Files.Builtin.Logo);
 
-        [Fact]
-        public void ShouldUseTheSpecifiedPercentage()
-        {
-            using var image = new MagickImage(Files.Builtin.Logo);
-            image.Sample(new Percentage(50));
+                Assert.Throws<ArgumentException>("percentageHeight", () => image.Sample(percentageWidth, percentageHeight));
+            }
 
-            Assert.Equal(320, image.Width);
-            Assert.Equal(240, image.Height);
+            [Fact]
+            public void ShouldUseTheSpecifiedPercentage()
+            {
+                using var image = new MagickImage(Files.Builtin.Logo);
+                image.Sample(new Percentage(50));
+
+                Assert.Equal(320, image.Width);
+                Assert.Equal(240, image.Height);
+            }
         }
     }
 }
