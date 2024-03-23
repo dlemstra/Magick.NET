4c5d41a40b1ccbd8cca869175e18d5b9000acfcd
Dirk Lemstra <dirk@lemstra.org>
2024-03-23 08:28:27 +0100

    No longer split the async methods of the Bytes class in a separate file because this is no longer necessary.

--- src/Shared/Bytes.cs
+++ src/Shared/Bytes.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ImageMagick;
 
@@ -28,6 +30,15 @@ internal sealed partial class Bytes
             Throw.IfNullOrEmpty(nameof(stream), stream);
 
         var data = GetData(stream, out var length);
+
+        return new Bytes(data, length);
+    }
+
+    public static async Task<Bytes> CreateAsync(Stream stream, CancellationToken cancellationToken)
+    {
+        Throw.IfNullOrEmpty(nameof(stream), stream);
+
+        var (data, length) = await GetDataAsync(stream, cancellationToken).ConfigureAwait(false);
 
         return new Bytes(data, length);
     }
@@ -70,6 +81,36 @@ internal sealed partial class Bytes
         return GetDataFromMemoryStream(tempStream, out length);
     }
 
+    private static async Task<(byte[] Bytes, int Length)> GetDataAsync(Stream stream, CancellationToken cancellationToken)
+    {
+        int length;
+        byte[] bytes;
+
+        if (stream is MemoryStream memStream)
+        {
+            bytes = GetDataFromMemoryStream(memStream, out length);
+            return (bytes, length);
+        }
+
+        Throw.IfFalse(nameof(stream), stream.CanRead, "The stream is not readable.");
+
+        if (stream.CanSeek)
+            return await GetDataWithSeekableStreamAsync(stream, cancellationToken).ConfigureAwait(false);
+
+        int count;
+        var buffer = new byte[BufferSize];
+        using var tempStream = new MemoryStream();
+        while ((count = await stream.ReadAsync(buffer, 0, BufferSize, cancellationToken).ConfigureAwait(false)) != 0)
+        {
+            CheckLength(tempStream.Length + count);
+
+            tempStream.Write(buffer, 0, count);
+        }
+
+        bytes = GetDataFromMemoryStream(tempStream, out length);
+        return (bytes, length);
+    }
+
     private static byte[] GetDataWithSeekableStream(Stream stream, out int length)
     {
         CheckLength(stream.Length);
@@ -85,6 +126,23 @@ internal sealed partial class Bytes
         }
 
         return data;
+    }
+
+    private static async Task<(byte[] Bytes, int Length)> GetDataWithSeekableStreamAsync(Stream stream, CancellationToken cancellationToken)
+    {
+        CheckLength(stream.Length);
+
+        var length = (int)stream.Length;
+        var data = new byte[length];
+
+        var read = 0;
+        int bytesRead;
+        while ((bytesRead = await stream.ReadAsync(data, read, length - read, cancellationToken).ConfigureAwait(false)) != 0)
+        {
+            read += bytesRead;
+        }
+
+        return (data, length);
     }
 
     private static byte[] GetDataFromMemoryStream(MemoryStream memStream, out int length)
