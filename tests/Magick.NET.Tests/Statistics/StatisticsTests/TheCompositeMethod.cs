9daed8d4a8627ac2c8467531c711ab1c3023f71e
Dirk Lemstra <dirk@lemstra.org>
2024-04-22 11:06:52 +0200

    Updated unit tests due to changes in ImageMagick.

--- tests/Magick.NET.Tests/Statistics/StatisticsTests/TheCompositeMethod.cs
+++ tests/Magick.NET.Tests/Statistics/StatisticsTests/TheCompositeMethod.cs
@@ -19,7 +19,7 @@ public partial class StatisticsTests
             Assert.NotNull(statistics);
 
             Assert.Equal(8, statistics.Depth);
-            Assert.InRange(statistics.Entropy, 0.14, 0.15);
+            Assert.InRange(statistics.Entropy, 0.19, 0.20);
             Assert.InRange(statistics.Kurtosis, 0.044, 0.045);
             Assert.Equal(0, statistics.Minimum);
             Assert.InRange(statistics.Skewness, 1.37, 1.38);
