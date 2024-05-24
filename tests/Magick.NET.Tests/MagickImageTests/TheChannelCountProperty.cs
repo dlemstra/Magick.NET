c2ffda609baf3a5132cdb5a6ff5590069f8c9b3d
Dirk Lemstra <dirk@lemstra.org>
2024-05-24 15:15:01 +0200

    Added unit test for the ChannelCount property of MagickImage.

---
+++ tests/Magick.NET.Tests/MagickImageTests/TheChannelCountProperty.cs
@@ -0,0 +1,30 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Tests;
+
+public partial class MagickImageTests
+{
+    public class TheChannelCountProperty
+    {
+        [Fact]
+        public void ShouldReturnTheCorrectNumberOfChannelsForRgbaImage()
+        {
+            using var image = new MagickImage(Files.RoseSparkleGIF);
+            Assert.Equal(5, image.ChannelCount);
+        }
+
+        [Fact]
+        public void ShouldReturnTheCorrectChannelsForCmykaImage()
+        {
+            using var image = new MagickImage(Files.SnakewarePNG);
+            Assert.Equal(2, image.ChannelCount);
+
+            image.ColorSpace = ColorSpace.CMYK;
+            Assert.Equal(5, image.ChannelCount);
+        }
+    }
+}
