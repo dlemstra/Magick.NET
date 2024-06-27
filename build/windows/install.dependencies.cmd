87d67fe79cf9394b7d6095188e75a4318a574736
Dirk Lemstra <dirk@lemstra.org>
2024-06-27 20:42:02 +0200

    Sign the NuGet package again with dotnet sign.

--- build/windows/install.dependencies.cmd
+++ build/windows/install.dependencies.cmd
@@ -5,3 +5,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 ..\..\tools\windows\gs1000w32.exe /S
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+dotnet tool install --global sign --version 0.9.1-beta.24325.5
+if %errorlevel% neq 0 exit /b %errorlevel%
