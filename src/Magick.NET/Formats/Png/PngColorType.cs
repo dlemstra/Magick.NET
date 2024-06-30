49e3bd8dfde3b7147ba2995457eafa4c9dae3caa
Rubens Cordeiro <zyoncore@hotmail.com>
2024-06-30 15:31:35 +0200

    Added PngColorType enum to the project.

---
+++ src/Magick.NET/Formats/Png/PngColorType.cs
@@ -0,0 +1,36 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+namespace ImageMagick.Formats;
+
+/// <summary>
+/// Specifies the chunks to be included or excluded in the PNG image.
+/// This is a flags enumeration, allowing a bitwise combination of its member values.
+/// </summary>
+internal enum PngColorType
+{
+    /// <summary>
+    /// Grayscale color type.
+    /// </summary>
+    Grayscale = 0,
+
+    /// <summary>
+    /// RGB color type.
+    /// </summary>
+    RGB = 2,
+
+    /// <summary>
+    /// Indexed color type.
+    /// </summary>
+    Indexed = 3,
+
+    /// <summary>
+    /// Grayscale with alpha color type.
+    /// </summary>
+    GrayMatte = 4,
+
+    /// <summary>
+    /// 
+    RGBMatte = 6,
+}
+
