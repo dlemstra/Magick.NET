1837675dcaacd9b187ac2fbbbb376c8b61a68e33
Dirk Lemstra <dirk@lemstra.org>
2024-07-21 18:20:02 +0200

    Fixed StyleCop warning.

--- tests/Magick.NET.Tests/Formats/Png/PngWriteDefinesTests/TheColorTypeProperty.cs
+++ tests/Magick.NET.Tests/Formats/Png/PngWriteDefinesTests/TheColorTypeProperty.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
 // Licensed under the Apache License, Version 2.0.
 
+using System;
 using ImageMagick;
 using ImageMagick.Formats;
-using System;
 using Xunit;
 
 namespace Magick.NET.Tests;
