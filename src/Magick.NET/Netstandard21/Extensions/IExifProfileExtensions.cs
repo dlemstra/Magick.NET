10477ae03329d425cd70b681bd20c82533a9d8bf
Dirk Lemstra <dirk@lemstra.org>
2024-07-27 10:45:12 +0200

    The CreateThumbnail extension method of IExifProfile will no longer be available in netstandard20.

--- src/Magick.NET/Netstandard21/Extensions/IExifProfileExtensions.cs
+++ src/Magick.NET/Netstandard21/Extensions/IExifProfileExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
 // Licensed under the Apache License, Version 2.0.
 
-using System;
+#if NETSTANDARD2_1
 
 #if Q8
 using QuantumType = System.Byte;
@@ -25,9 +25,6 @@ public static class IExifProfileExtensions
     /// </summary>
     /// <param name="self">The exif profile.</param>
     /// <returns>The thumbnail in the exif profile when available.</returns>
-#if !NETSTANDARD2_1
-    [Obsolete($"This method will be no longer be available for netstandard20 in the next major release.")]
-#endif
     public static IMagickImage<QuantumType>? CreateThumbnail(this IExifProfile self)
     {
         Throw.IfNull(nameof(self), self);
@@ -38,20 +35,13 @@ public static class IExifProfileExtensions
         if (thumbnailLength == 0 || thumbnailOffset == 0)
             return null;
 
-#if NETSTANDARD2_1
         var data = self.ToReadOnlySpan();
 
         if (data.Length < (thumbnailOffset + thumbnailLength))
             return null;
 
         return new MagickImage(data.Slice(thumbnailOffset, thumbnailLength));
-#else
-        var data = self.ToByteArray();
-
-        if (data is null || data.Length < (thumbnailOffset + thumbnailLength))
-            return null;
-
-        return new MagickImage(data, thumbnailOffset, thumbnailLength);
-#endif
     }
 }
+
+#endif
