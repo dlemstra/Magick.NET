b8bc2bd01c3bad96260cfc25ce20a298853cfcaf
Dirk Lemstra <dirk@lemstra.org>
2024-09-09 21:37:55 +0200

    Also mention that an install is starting.

--- build/windows/install.dependencies.cmd
+++ build/windows/install.dependencies.cmd
@@ -3,14 +3,15 @@
 powershell .\install.dependencies.ps1
 if %errorlevel% == 0 goto installGhostscript
 
-echo Failed to install dependencies.
+echo Failed to install dependencies
 exit /b %errorlevel%
 
 :installGhostscript
+echo Installing Ghostscript
 ..\..\tools\windows\gs1000w32.exe /S
 if %errorlevel% == 0 goto done
 
-echo Failed to install Ghostscript.
+echo Failed to install Ghostscript
 exit /b %errorlevel%
 
 :done
