72a4a9848cf27b54a5ce39288dc2a71d68f91e1a
Dirk Lemstra <dirk@lemstra.org>
2024-11-09 11:55:40 +0100

    Minor doc tweak to make it more clear that Memory is set.

--- src/Magick.NET.Core/IResourceLimits.cs
+++ src/Magick.NET.Core/IResourceLimits.cs
@@ -70,8 +70,8 @@ public interface IResourceLimits
     ulong Width { get; set; }
 
     /// <summary>
-    /// Set the maximum percentage of memory that can be used for image data. This also changes
-    /// the <see cref="Area"/> limit to four times the number of bytes.
+    /// Set the maximum percentage of <see cref="Memory"/> that can be used for image data.
+    /// This also changes the <see cref="Area"/> limit to four times the number of bytes.
     /// </summary>
     /// <param name="percentage">The percentage to use.</param>
     void LimitMemory(Percentage percentage);
