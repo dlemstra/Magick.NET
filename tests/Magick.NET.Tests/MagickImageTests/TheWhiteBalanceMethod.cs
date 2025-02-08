9bc3430ede54ad90f4527048a68a9189f866b722
Dirk Lemstra <dirk@lemstra.org>
2025-02-08 22:06:06 +0100

    Updated unit tests due to changes in ImageMagick.

--- tests/Magick.NET.Tests/MagickImageTests/TheWhiteBalanceMethod.cs
+++ tests/Magick.NET.Tests/MagickImageTests/TheWhiteBalanceMethod.cs
@@ -18,9 +18,9 @@ public partial class MagickImageTests
 #if Q8
             ColorAssert.Equal(new MagickColor("#dd4946"), image, 45, 25);
 #elif Q16
-            ColorAssert.Equal(new MagickColor("#de4a4a714699"), image, 45, 25);
+            ColorAssert.Equal(new MagickColor("#de4a4a704697"), image, 45, 25);
 #else
-            ColorAssert.Equal(new MagickColor("#de494a714699"), image, 45, 25);
+            ColorAssert.Equal(new MagickColor("#de4a4a6f4696"), image, 45, 25);
 #endif
         }
 
@@ -33,10 +33,10 @@ public partial class MagickImageTests
 #if Q8
             ColorAssert.Equal(new MagickColor("#00a13b"), image, 45, 25);
 #elif Q16
-            ColorAssert.Equal(new MagickColor("#0000a2043c3e"), image, 45, 25);
+            ColorAssert.Equal(new MagickColor("#0000a2013c3c"), image, 45, 25);
 #else
             image.Clamp();
-            ColorAssert.Equal(new MagickColor("#0000a2033c3d"), image, 45, 25);
+            ColorAssert.Equal(new MagickColor("#0000a2013c3b"), image, 45, 25);
 #endif
         }
     }
--- tests/Magick.NET.Tests/Statistics/ChannelPerceptualHashTests/TheToStringMethod.cs
+++ tests/Magick.NET.Tests/Statistics/ChannelPerceptualHashTests/TheToStringMethod.cs
@@ -21,11 +21,11 @@ public partial class ChannelPerceptualHashTests
             Assert.NotNull(red);
 
 #if Q8
-            Assert.Equal("a65e687f9388c0e88f0261ce08eeb361d40a26ce81a1e823ec85b3b8cc3586ec889ad4", red.ToString());
+            Assert.Equal("a65e587f9388c0e88f0461ce08eeb861d40a26ce81a1e823ec85b3b8cc3586ec889ad4", red.ToString());
 #elif Q16
-            Assert.Equal("a658c87fa088c0588eb561ccb8ed8a61d43a646682939835e986ec98c78f887ae8c67f", red.ToString());
+            Assert.Equal("a658887fa088c0588eb461ccb8ed8961d43a646682939835e986ec98c78f887ae8c67f", red.ToString());
 #else
-            Assert.Equal("a658c87fa188c0588eb561ccb8ed8861d43a731182e3a83aa2876d48d19488f438dcb5", red.ToString());
+            Assert.Equal("a658887fa088c0588eb461ccb8ed8861d43a731182e3a83aa2876d48d19488f438dcb5", red.ToString());
 #endif
         }
     }
