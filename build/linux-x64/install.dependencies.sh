876266164bf6cdb3620705e9cc4f77069929fe59
Dirk Lemstra <dirk@lemstra.org>
2025-02-19 17:29:43 +0100

    Also install Ghostscript on Linux with the binaries from the release in the ImageMagick-Windows repository.

--- build/linux-x64/install.dependencies.sh
+++ build/linux-x64/install.dependencies.sh
@@ -11,7 +11,7 @@ cd msttcorefonts
 
 fc-cache
 
-wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9533/ghostscript-9.53.3-linux-x86_64.tgz
-tar zxvf ghostscript-9.53.3-linux-x86_64.tgz
-cp ghostscript-9.53.3-linux-x86_64/gs-9533-linux-x86_64 /usr/bin/gs
+wget https://github.com/ImageMagick/ImageMagick-Windows/releases/download/20200615/ghostscript-10.0.0-linux-x86_64.tgz
+tar zxvf ghostscript-10.0.0-linux-x86_64.tgz
+cp ghostscript-10.0.0-linux-x86_64/gs-1000-linux-x86_64 /usr/bin/gs
 chmod 755 /usr/bin/gs
