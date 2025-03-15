4585486c8119f3ab6a484644c47005a071e5d6a0
Dirk Lemstra <dirk@lemstra.org>
2025-03-15 17:41:41 +0100

    Added unit tests for the QuantumScalerFactory.

---
+++ tests/Magick.NET.Core.Tests/Scaler/QuantumScalerFactory/TheCreateMethod.cs
@@ -0,0 +1,44 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using ImageMagick;
+using Xunit;
+
+namespace Magick.NET.Core.Tests;
+
+public partial class QuantumScalerFactoryTests
+{
+    public class TheCreateMethod
+    {
+        [Fact]
+        public void ShouldThrowExceptionWhenTypeIsNotSupported()
+        {
+            Assert.Throws<NotSupportedException>(() =>
+            {
+                var factory = QuantumScalerFactory.Create<int>();
+            });
+        }
+
+        [Fact]
+        public void ShouldReturnByteQuantumScalerWhenTypeIsByte()
+        {
+            var factory = QuantumScalerFactory.Create<byte>();
+            Assert.IsType<ByteQuantumScaler>(factory);
+        }
+
+        [Fact]
+        public void ShouldReturnUnsignedShortQuantumScalerWhenTypeIsUShort()
+        {
+            var factory = QuantumScalerFactory.Create<ushort>();
+            Assert.IsType<UnsignedShortQuantumScaler>(factory);
+        }
+
+        [Fact]
+        public void ShouldReturnFloatQuantumScalerWhenTypeIsFloat()
+        {
+            var factory = QuantumScalerFactory.Create<float>();
+            Assert.IsType<FloatQuantumScaler>(factory);
+        }
+    }
+}
