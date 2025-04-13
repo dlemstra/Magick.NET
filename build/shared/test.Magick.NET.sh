0d6ed80a2f3d6dc2af85abb6600984b56f879dc9
Dirk Lemstra <dirk@lemstra.org>
2025-04-13 09:47:34 +0200

    Also run Avalonia unit tests on the non-windows platforms.

--- build/shared/test.Magick.NET.sh
+++ build/shared/test.Magick.NET.sh
@@ -11,6 +11,7 @@ testMagickNET() {
 
     ./tests/Magick.NET.Core.Tests/bin/$config/AnyCPU/net8.0/Magick.NET.Core.Tests
     ./tests/Magick.NET.Tests/bin/$config/AnyCPU/net8.0/Magick.NET.Tests
+    ./tests/Magick.NET.AvaloniaMediaImaging.Tests/bin/$config/AnyCPU/net8.0/Magick.NET.AvaloniaMediaImaging.Tests
 }
 
 export FONTCONFIG_PATH=/etc/fonts
