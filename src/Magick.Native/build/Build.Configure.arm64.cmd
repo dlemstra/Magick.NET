a1696e7b4b1cc6d0defdbd5cacdb33e7823c6688
Dirk Lemstra <dirk@lemstra.org>
2025-04-21 12:12:15 +0200

    Added pause at the end of the new batch files.

--- src/Magick.Native/build/Build.Configure.arm64.cmd
+++ src/Magick.Native/build/Build.Configure.arm64.cmd
@@ -2,3 +2,5 @@
 
 cd ..\..\..\..\Magick.Native\build\windows\
 call build.configure.cmd arm64
+
+pause
--- src/Magick.Native/build/Build.Configure.x64.cmd
+++ src/Magick.Native/build/Build.Configure.x64.cmd
@@ -2,3 +2,5 @@
 
 cd ..\..\..\..\Magick.Native\build\windows\
 call build.configure.cmd x64
+
+pause
