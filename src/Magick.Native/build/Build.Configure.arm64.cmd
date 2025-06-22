d38eca8bdcde623a0ec244361dbcc27ede1c1adf
Dirk Lemstra <dirk@lemstra.org>
2025-06-22 15:41:31 +0200

    Remove building of configure because it is now always a pre-build binary.

--- src/Magick.Native/build/Build.Configure.arm64.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cd ..\..\..\..\Magick.Native\build\windows\
-call build.configure.cmd arm64
-
-pause
--- src/Magick.Native/build/Build.Configure.x64.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cd ..\..\..\..\Magick.Native\build\windows\
-call build.configure.cmd x64
-
-pause
