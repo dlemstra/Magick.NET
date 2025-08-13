28ee123a615652f500f8c4bc0a9b384918c9aadd
Dirk Lemstra <dirk@lemstra.org>
2025-08-13 07:25:22 +0200

    Published Magick.NET 14.8.0

--- publish/update.version.cmd
+++ publish/update.version.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-powershell .\update.version.ps1 -library "Magick.NET" -version "14.7.0"
-powershell .\update.version.ps1 -library "Magick.NET.Core" -version "14.7.0"
-powershell .\update.version.ps1 -library "Magick.NET.AvaloniaMediaImaging" -version "1.1.2"
-powershell .\update.version.ps1 -library "Magick.NET.SystemDrawing" -version "8.0.7"
-powershell .\update.version.ps1 -library "Magick.NET.SystemWindowsMedia" -version "8.0.7"
+powershell .\update.version.ps1 -library "Magick.NET" -version "14.8.0"
+powershell .\update.version.ps1 -library "Magick.NET.Core" -version "14.8.0"
+powershell .\update.version.ps1 -library "Magick.NET.AvaloniaMediaImaging" -version "1.1.3"
+powershell .\update.version.ps1 -library "Magick.NET.SystemDrawing" -version "8.0.8"
+powershell .\update.version.ps1 -library "Magick.NET.SystemWindowsMedia" -version "8.0.8"
 powershell .\update.version.ps1
