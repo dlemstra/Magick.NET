2165d26285fe3bbd476494285e3cbe46810f1723
Dirk Lemstra <dirk@lemstra.org>
2025-08-16 09:34:16 +0200

    Added extra unit test for the constructor of the DrawableAffine class.

---
+++ tests/Magick.NET.Tests/Drawing/DrawableAffineTests/TheConstructor.cs
@@ -0,0 +1,25 @@
+﻿// Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
+// Licensed under the Apache License, Version 2.0.
+
+using ImageMagick.Drawing;
+using Xunit;
+
+namespace Magick.NET.Tests;
+
+public partial class DrawableAffineTests
+{
+    public class TheConstructor
+    {
+        [Fact]
+        public void ShouldSetTheCorrectProperties()
+        {
+            var affine = new DrawableAffine(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+            Assert.Equal(1.0, affine.ScaleX);
+            Assert.Equal(2.0, affine.ScaleY);
+            Assert.Equal(3.0, affine.ShearX);
+            Assert.Equal(4.0, affine.ShearY);
+            Assert.Equal(5.0, affine.TranslateX);
+            Assert.Equal(6.0, affine.TranslateY);
+        }
+    }
+}
