789611349bb98d797f338857faddfdf6b5c2a89d
Dirk Lemstra <dirk@lemstra.org>
2025-08-16 09:39:34 +0200

    Removed old unit test.

--- tests/Magick.NET.Tests/Drawing/DrawableAffineTests/TheProperties.cs
+++ tests/Magick.NET.Tests/Drawing/DrawableAffineTests/TheProperties.cs
@@ -21,18 +21,5 @@ public partial class DrawableAffineTests
             Assert.Equal(0.0, affine.TranslateX);
             Assert.Equal(0.0, affine.TranslateY);
         }
-
-        [Fact]
-        public void ShouldSetTheCorrectValue()
-        {
-            var affine = new DrawableAffine(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
-
-            Assert.Equal(1.0, affine.ScaleX);
-            Assert.Equal(2.0, affine.ScaleY);
-            Assert.Equal(3.0, affine.ShearX);
-            Assert.Equal(4.0, affine.ShearY);
-            Assert.Equal(5.0, affine.TranslateX);
-            Assert.Equal(6.0, affine.TranslateY);
-        }
     }
 }
