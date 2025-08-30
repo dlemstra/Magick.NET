612bc6deacf8c662a25ca33ae26d5950b1049f61
Dirk Lemstra <dirk@lemstra.org>
2025-08-30 13:05:44 +0200

    Use the binaries from the new repository.

--- build/linux-x64/install.dependencies.sh
+++ build/linux-x64/install.dependencies.sh
@@ -11,7 +11,7 @@ cd msttcorefonts
 
 fc-cache
 
-wget https://github.com/ImageMagick/ImageMagick-Windows/releases/download/20200615/ghostscript-10.0.0-linux-x86_64.tgz
+wget https://github.com/dlemstra/Magick.NET.BuildDependencies/releases/download/build-binaries-2025-08-30/ghostscript-10.0.0-linux-x86_64.tgz
 tar zxvf ghostscript-10.0.0-linux-x86_64.tgz
 cp ghostscript-10.0.0-linux-x86_64/gs-1000-linux-x86_64 /usr/bin/gs
 chmod 755 /usr/bin/gs
--- build/windows/install.dependencies.ps1
+++ build/windows/install.dependencies.ps1
@@ -7,12 +7,12 @@ $targetNugetExe = "$PSScriptRoot\..\..\tools\windows\nuget.exe"
 Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
 
 Write-Host "Downloading Ghostscript 10.00.0"
-$sourceGhostscriptExe = "https://github.com/ImageMagick/ImageMagick-Windows/releases/download/20200615/gs1000w32.exe"
+$sourceGhostscriptExe = "https://github.com/dlemstra/Magick.NET.BuildDependencies/releases/download/build-binaries-2025-08-30/gs1000w32.exe"
 $targetGhostscriptExe = "$PSScriptRoot\..\..\tools\windows\gs1000w32.exe"
 Invoke-WebRequest $sourceGhostscriptExe -OutFile $targetGhostscriptExe
 
 Write-Host "Downloading FFmpeg 4.2.3"
-$sourceFFmpegExe = "https://github.com/ImageMagick/ImageMagick-Windows/releases/download/20200615/ffmpeg-4.2.3-win64.exe"
+$sourceFFmpegExe = "https://github.com/dlemstra/Magick.NET.BuildDependencies/releases/download/build-binaries-2025-08-30/ffmpeg-4.2.3-win64.exe"
 $targetFFmpegExe = "c:\vcpkg\ffmpeg.exe"
 Invoke-WebRequest $sourceFFmpegExe -OutFile $targetFFmpegExe
 
