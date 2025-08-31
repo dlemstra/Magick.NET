4048e9a8851056b1444f011a309f734ff9747a87
Dirk Lemstra <dirk@lemstra.org>
2025-08-31 17:40:30 +0200

    Specify platform instead.

--- build/shared/build.Magick.NET.sh
+++ build/shared/build.Magick.NET.sh
@@ -10,8 +10,8 @@ buildMagickNET() {
         config=$config-OpenMP
     fi
 
-    dotnet build tests/Magick.NET.Core.Tests/Magick.NET.Core.Tests.csproj -f net8.0 -c $config --arch $architecture
-    dotnet build tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f net8.0 -c $config --arch $architecture
+    dotnet build tests/Magick.NET.Core.Tests/Magick.NET.Core.Tests.csproj -f net8.0 -c $config -p:Platform=$architecture
+    dotnet build tests/Magick.NET.Tests/Magick.NET.Tests.csproj -f net8.0 -c $config -p:Platform=$architecture
 }
 
 buildMagickNET "Q8"
