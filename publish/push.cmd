39f302ca5289392cdcc93aa6691eee84b68f1592
Dirk Lemstra <dirk@lemstra.org>
2025-12-09 19:24:25 +0100

    Added step to push the NuGet packages when they need to be published.

--- publish/push.cmd
+++ publish/push.cmd
@@ -1,16 +1,11 @@
 @echo off
 
-echo Are you sure?
-pause
+if "%NUGET_API_KEY%"=="" (
+    echo Error: NUGET_API_KEY environment variable is not set
+    exit /b 1
+)
 
-set /p ApiKey=<../keys/nuget.txt
-if not "%ApiKey%"=="" goto push
+echo Waiting 5 minutes before pushing packages...
+timeout /t 300 /nobreak
 
-echo Unable to find nuget.txt in the keys folder.
-goto done
-
-:push
-for /r %%i in (*.nupkg) do ..\tools\windows\nuget.exe push %%i %ApiKey% -Source nuget.org
-
-:done
-pause
+for /r %%i in (*.nupkg) do ..\tools\windows\nuget.exe push %%i -ApiKey %NUGET_API_KEY% -Source "https://api.nuget.org/v3/index.json
