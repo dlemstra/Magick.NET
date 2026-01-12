3bf312366afb0ce9fdbd8a07f3bb33e17a0791c4
Dirk Lemstra <dirk@lemstra.org>
2026-01-12 21:11:33 +0100

    Added missing quote.

--- publish/push.cmd
+++ publish/push.cmd
@@ -8,4 +8,4 @@ if "%NUGET_API_KEY%"=="" (
 echo Waiting 5 minutes before pushing packages...
 ping -n 301 127.0.0.1 > nul
 
-for /r %%i in (*.nupkg) do ..\tools\windows\nuget.exe push %%i -ApiKey %NUGET_API_KEY% -Source "https://api.nuget.org/v3/index.json
+for /r %%i in (*.nupkg) do ..\tools\windows\nuget.exe push %%i -ApiKey %NUGET_API_KEY% -Source "https://api.nuget.org/v3/index.json"
