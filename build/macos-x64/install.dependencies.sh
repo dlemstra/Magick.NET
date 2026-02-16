037462f5766746c9d9eff5f1130e378bfb12b046
Dirk Lemstra <dirk@lemstra.org>
2026-02-16 21:48:35 +0100

    Install ffmpeg with brew instead on MacOS.

--- build/macos-x64/install.dependencies.sh
+++ build/macos-x64/install.dependencies.sh
@@ -5,6 +5,4 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 
 fc-list
 
-downloadUrl=$(curl 'https://evermeet.cx/ffmpeg/info/ffmpeg/6.0' -fsS| jq -rc '.download.zip.url') 
-curl -f -L -# --compressed -A 'https://github.com/eugeneware/ffmpeg-static binaries download script' -o "ffmpeg-darwin-x64.zip" "$downloadUrl"
-unzip -o -d /usr/local/bin -j ffmpeg-darwin-x64.zip ffmpeg
+brew install ffmpeg
