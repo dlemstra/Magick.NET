8e15a1cb001434cfb1e23b560188dded6737b65a
Dirk Lemstra <dirk@lemstra.org>
2026-04-13 19:17:53 +0200

    Corrected version.

--- publish/update.version.cmd
+++ publish/update.version.cmd
@@ -2,7 +2,7 @@
 
 powershell .\update.version.ps1 -library "Magick.NET" -version "14.12.0"
 powershell .\update.version.ps1 -library "Magick.NET.Core" -version "14.12.0"
-powershell .\update.version.ps1 -library "Magick.NET.AvaloniaMediaImaging" -version "1.1.7"
+powershell .\update.version.ps1 -library "Magick.NET.AvaloniaMediaImaging" -version "1.1.9"
 powershell .\update.version.ps1 -library "Magick.NET.SystemDrawing" -version "8.0.20"
 powershell .\update.version.ps1 -library "Magick.NET.SystemWindowsMedia" -version "8.0.20"
 powershell .\update.version.ps1
