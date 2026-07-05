b84d753a8957fe93635eff5d168ec8e80c9fac7b
Dirk Lemstra <dirk@lemstra.org>
2026-07-05 17:16:36 +0200

    Remove old scripts.

--- src/Magick.Native/create-nuget-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-cp nuget.config.template nuget.config
-dotnet nuget update source "github" --source "https://nuget.pkg.github.com/dlemstra/index.json" --username "$1" --password "$2" --store-password-in-clear-text
--- src/Magick.Native/install.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-powershell .\install.ps1
-if %errorlevel% neq 0 exit /b %errorlevel%
